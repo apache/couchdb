9807d8d7d0efd6c257a2642ceeba9a631e00aa68
Jan Lehnardt <jan@apache.org>
2008-07-03 09:19:40 +0000

    Improve error message. If the couch_erl_driver.so is no good for some reason (linking e.g.) you get something along the lines of {"init terminating in do_boot","Driver is an inappropriate Mach-O file"} instead of {"init terminating in do_boot",{error,{open_error,-12}}} Patch by Dale Johnson <dale.johnson@gmail.com>
    git-svn-id: https://svn.apache.org/repos/asf/incubator/couchdb/trunk@673634 13f79535-47bb-0310-9956-ffa450edef68

--- src/couchdb/couch_util.erl
+++ src/couchdb/couch_util.erl
@@ -28,7 +28,7 @@ start_driver(LibDir) ->
     case erl_ddll:load_driver(LibDir, "couch_erl_driver") of
     ok -> ok;
     {error, already_loaded} -> ok;
-    Error -> exit(Error)
+    {error, Error} -> exit(erl_ddll:format_error(Error))
     end.
     
     
