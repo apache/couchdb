863bd56a3dcdf4d28637fd02de2f79bb25a51906
Noah Slater <nslater@apache.org>
2008-10-23 15:04:36 +0000

    moved erlang compilation out of check_PROGRAMS
    git-svn-id: https://svn.apache.org/repos/asf/incubator/couchdb/trunk@707385 13f79535-47bb-0310-9956-ffa450edef68

--- test/runner.sh
+++ test/runner.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+erlc runner.erl
+
 erl -noshell -pa ../src/couchdb -pa ../src/mochiweb -eval "runner:run()"
 
 cat ../share/www/script/couch.js ../share/www/script/couch_tests.js test.js  | ../src/couchdb/couchjs -
