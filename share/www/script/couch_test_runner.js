e5d58ede60c633e6d0da484eb513726465a3eed6
Christopher Lenz <cmlenz@apache.org>
2009-01-15 09:00:39 +0000

    Oops, removed debugging leftovers from r734633.
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@734636 13f79535-47bb-0310-9956-ffa450edef68

--- share/www/script/couch_test_runner.js
+++ share/www/script/couch_test_runner.js
@@ -137,9 +137,7 @@ function updateTestsListing() {
 
 function updateTestsFooter() {
   var tests = $("#tests tbody.content tr td.status");
-  console.log(tests);
   var testsRun = tests.filter(".success, .error, .failure");
-  console.log(testsRun)
   var testsFailed = testsRun.not(".success");
   $("#tests tbody.footer td").text(testsRun.length + " of " + tests.length +
     " test(s) run, " + testsFailed.length + " failures");
