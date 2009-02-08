0784c9173ae7a577f795bc73cbd7f0994451dcd4
John Christopher Anderson <jchris@apache.org>
2009-02-08 20:29:51 +0000

    extract method couch_httpd:send_redirect(Req, Path)
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@742158 13f79535-47bb-0310-9956-ffa450edef68

--- couch_httpd_misc_handlers.erl
+++ couch_httpd_misc_handlers.erl
@@ -50,8 +50,7 @@ handle_utils_dir_req(#httpd{method='GET'}=Req, DocumentRoot) ->
         couch_httpd:serve_file(Req, RelativePath, DocumentRoot);
     {_ActionKey, "", _RelativePath} ->
         % GET /_utils
-        Headers = [{"Location", couch_httpd:absolute_uri(Req, "/_utils/")}],
-        couch_httpd:send_response(Req, 301, Headers, <<>>)
+        couch_httpd:send_redirect(Req, "/_utils/")
     end;
 handle_utils_dir_req(Req, _) ->
     send_method_not_allowed(Req, "GET,HEAD").
