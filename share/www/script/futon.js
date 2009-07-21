faa5479e472356770a8cda116defd002d88b20d6
Christopher Lenz <cmlenz@apache.org>
2009-07-21 12:15:36 +0000

    Avoid wrapping of long database names in the Futon sidebar.
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@796269 13f79535-47bb-0310-9956-ffa450edef68

--- share/www/script/futon.js
+++ share/www/script/futon.js
@@ -65,9 +65,9 @@
       recentDbs.sort();
       $.each(recentDbs, function(idx, name) {
         if (name) {
-          $("#dbs").append("<li><a href='database.html?" +
-            encodeURIComponent(name) + "'>" + name +
+          $("#dbs").append("<li>" +
             "<button class='remove' title='Remove from list' value='" + name + "'></button>" +
+            "<a href='database.html?" + encodeURIComponent(name) + "'>" + name +
             "</a></li>");
         }
       });
