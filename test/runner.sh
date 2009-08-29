422d815b3baf1a0152429d02257aeaeaf6fa471c
Jan Lehnardt <jan@apache.org>
2009-08-29 16:49:22 +0000

    remove legacy erlang tests, etap covers these
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@809158 13f79535-47bb-0310-9956-ffa450edef68

--- test/runner.sh
+++ test/runner.sh
@@ -12,10 +12,6 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-erlc runner.erl
-
-erl -noshell -pa ../src/couchdb -pa ../src/mochiweb -eval "runner:run()"
-
 cat ../share/www/script/couch.js \
     ../share/www/script/couch_test_runner.js \
     ../share/www/script/couch_tests.js \
