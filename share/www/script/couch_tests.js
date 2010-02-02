a4da98647be6e14eab5299c462ed47c2fda990c1
Benoit Chesneau <benoitc@apache.org>
2010-02-02 21:05:51 +0000

    proxy authentification handler.  This handler allows creation of a userCtx object from a user authenticated remotly.
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@905797 13f79535-47bb-0310-9956-ffa450edef68

--- share/www/script/couch_tests.js
+++ share/www/script/couch_tests.js
@@ -62,6 +62,7 @@ loadTest("multiple_rows.js");
 loadScript("script/oauth.js");
 loadScript("script/sha1.js");
 loadTest("oauth.js");
+loadTest("proxyauth.js");
 loadTest("purge.js");
 loadTest("reader_acl.js");
 loadTest("recreate_doc.js");
