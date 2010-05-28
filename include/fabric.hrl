2c2a4192531e6af514c21c127065dba3af518561
Brad Anderson <brad@cloudant.com>
2010-05-28 16:50:35 -0400

    begin move of dynomite to membership

--- include/fabric.hrl
+++ include/fabric.hrl
@@ -1,3 +1,11 @@
+-define(FABRIC, true).
+
+-ifndef(COUCH).
 -include("../../couch/src/couch_db.hrl").
+-endif.
+
+-ifndef(MEMBERSHIP).
 -include("../../dynomite/include/membership.hrl").
+-endif.
+
 -include_lib("eunit/include/eunit.hrl").
