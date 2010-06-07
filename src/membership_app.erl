ec6a2999c98332ff90e2d0907eefc8bf5671c71d
Brad Anderson <brad@cloudant.com>
2010-06-07 16:18:44 -0400

    dbs database creation moved to membership app

--- src/membership_app.erl
+++ src/membership_app.erl
@@ -10,10 +10,11 @@
 
 %% @doc start required apps, join cluster, start supervisor
 start(_Type, _StartArgs) ->
-    % start dynomite supervisor
+    couch_api:create_db(<<"dbs">>, []), % all nodes have local 'dbs' db
+    % start membership supervisor
     membership_sup:start_link().
 
 stop({_, Sup}) ->
-    ?LOG_ALERT("dynomite application stopped", []),
+    ?LOG_ALERT("membership application stopped", []),
     exit(Sup, normal),
     ok.
