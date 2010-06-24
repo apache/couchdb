b3da633985e6ea41fda2a7f302913bcf8e1ea780
Benoit Chesneau <benoitc@apache.org>
2010-06-24 08:59:24 +0000

    allows client to retrieve the revision of document updated via _update, by providing it in headers. Header is named "X-Couch-Update-NewRev.
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/branches/0.11.x@957459 13f79535-47bb-0310-9956-ffa450edef68

--- src/couchdb/couch_httpd_show.erl
+++ src/couchdb/couch_httpd_show.erl
@@ -125,22 +125,27 @@ handle_doc_update_req(Req, _Db, _DDoc) ->
 send_doc_update_response(Req, Db, DDoc, UpdateName, Doc, DocId) ->
     JsonReq = couch_httpd_external:json_req_obj(Req, Db, DocId),
     JsonDoc = couch_query_servers:json_doc(Doc),
-    case couch_query_servers:ddoc_prompt(DDoc, [<<"updates">>, UpdateName], [JsonDoc, JsonReq]) of
-        [<<"up">>, {NewJsonDoc}, JsonResp] ->
-            Options = case couch_httpd:header_value(Req, "X-Couch-Full-Commit", "false") of
+    {Code, JsonResp1} = case couch_query_servers:ddoc_prompt(DDoc, 
+                [<<"updates">>, UpdateName], [JsonDoc, JsonReq]) of
+        [<<"up">>, {NewJsonDoc}, {JsonResp}] ->
+            Options = case couch_httpd:header_value(Req, "X-Couch-Full-Commit", 
+                "false") of
             "true" ->
                 [full_commit];
             _ ->
                 []
             end,
             NewDoc = couch_doc:from_json_obj({NewJsonDoc}),
-            Code = 201,
-            {ok, _NewRev} = couch_db:update_doc(Db, NewDoc, Options);
+            {ok, NewRev} = couch_db:update_doc(Db, NewDoc, Options),
+            NewRevStr = couch_doc:rev_to_str(NewRev),
+            JsonRespWithResv =  {[{<<"headers">>, 
+                {[{<<"X-Couch-Update-NewRev">>, NewRevStr}]}} | JsonResp]},
+            {201, JsonRespWithResv};
         [<<"up">>, _Other, JsonResp] ->
-            Code = 200,
-            ok
+            {200, JsonResp}
     end,
-    JsonResp2 = json_apply_field({<<"code">>, Code}, JsonResp),
+    
+    JsonResp2 = json_apply_field({<<"code">>, Code}, JsonResp1),
     % todo set location field
     couch_httpd_external:send_external_response(Req, JsonResp2).
 
