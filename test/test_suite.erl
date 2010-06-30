ebfbbb43cddaf592687b53caa0ed42cd58b2d36d
Adam Kocoloski <adam@cloudant.com>
2010-06-30 16:43:49 -0400

    update test_suite

--- test/test_suite.erl
+++ test/test_suite.erl
@@ -3,8 +3,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 all_test_() ->
-  [{module, mem_utils_test},
-   {module, membership2_test},
-   {module, partitions_test},
-   {module, replication_test}
-  ].
+    [
+        {module, mem3_test},
+        {module, partitions_test}
+    ].
