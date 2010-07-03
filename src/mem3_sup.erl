f3c820d21573a93bc8b792ef555a578d8db2f0b3
Adam Kocoloski <adam@cloudant.com>
2010-07-03 09:37:51 -0400

    forgot to register the mem3 top-level supervisor

--- src/mem3_sup.erl
+++ src/mem3_sup.erl
@@ -3,7 +3,7 @@
 -export([start_link/0, init/1]).
 
 start_link() ->
-    supervisor:start_link(?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init(_Args) ->
     Children = [
