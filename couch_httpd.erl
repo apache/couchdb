d63c864e4e2d8ebe45c83380b96c05896d568f60
John Christopher Anderson <jchris@apache.org>
2010-07-06 18:36:27 +0000

    dont redirect endlessly when require_valid_user=true
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@960975 13f79535-47bb-0310-9956-ffa450edef68

--- couch_httpd.erl
+++ couch_httpd.erl
@@ -748,15 +748,20 @@ error_headers(#httpd{mochi_req=MochiReq}=Req, Code, ErrorStr, ReasonStr) ->
                     case couch_config:get("couch_httpd_auth", "authentication_redirect", nil) of
                     nil -> {Code, []};
                     AuthRedirect ->
-                        % if the accept header matches html, then do the redirect. else proceed as usual.
-                        case re:run(MochiReq:get_header_value("Accept"), "html", [{capture, none}]) of
-                        nomatch ->
+                        case couch_config:get("couch_httpd_auth", "require_valid_user", "false") of
+                        "true" ->
                             {Code, []};
-                        match ->
-                            AuthRedirectBin = ?l2b(AuthRedirect),
-                            UrlReturn = ?l2b(couch_util:url_encode(MochiReq:get(path))),
-                            UrlReason = ?l2b(couch_util:url_encode(ReasonStr)),
-                            {302, [{"Location", couch_httpd:absolute_uri(Req, <<AuthRedirectBin/binary,"?return=",UrlReturn/binary,"&reason=",UrlReason/binary>>)}]}
+                        _False ->
+                            % if the accept header matches html, then do the redirect. else proceed as usual.
+                            case re:run(MochiReq:get_header_value("Accept"), "html", [{capture, none}]) of
+                            nomatch ->
+                                {Code, []};
+                            match ->
+                                AuthRedirectBin = ?l2b(AuthRedirect),
+                                UrlReturn = ?l2b(couch_util:url_encode(MochiReq:get(path))),
+                                UrlReason = ?l2b(couch_util:url_encode(ReasonStr)),
+                                {302, [{"Location", couch_httpd:absolute_uri(Req, <<AuthRedirectBin/binary,"?return=",UrlReturn/binary,"&reason=",UrlReason/binary>>)}]}
+                            end
                         end
                     end;
                 _Else ->
