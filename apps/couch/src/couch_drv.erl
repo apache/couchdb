75a44a40776917073c87a2e2e44c9fef516364b8
Brad Anderson <brad@cloudant.com>
2010-08-19 19:30:32 -0400

    make dist back in working order, and dbcore starts successfully

--- apps/couch/src/couch_drv.erl
+++ apps/couch/src/couch_drv.erl
@@ -1,6 +1,6 @@
 -module(couch_drv).
 -behaviour(gen_server).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, 
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
     code_change/3]).
 
 -export([start_link/0]).
@@ -11,7 +11,7 @@ start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-    case erl_ddll:load(code:priv_dir(couch), "couch_erl_driver") of
+    case erl_ddll:load(code:priv_dir(couch), "couch_icu_driver") of
     ok ->
         {ok, nil};
     {error, already_loaded} ->
