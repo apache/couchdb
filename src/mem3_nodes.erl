716fc9d7704e717b63909ba91502ea79139231c1
Adam Kocoloski <adam@cloudant.com>
2010-08-27 15:43:44 -0400

    Apache 2 license, Cloudant copyright when appropriate

--- src/mem3_nodes.erl
+++ src/mem3_nodes.erl
@@ -1,3 +1,17 @@
+% Copyright 2010 Cloudant
+% 
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
 -module(mem3_nodes).
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, 
