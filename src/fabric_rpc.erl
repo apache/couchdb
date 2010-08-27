5dd29eb9852ae087ed2e6c4f54420bf09568e681
Adam Kocoloski <adam@cloudant.com>
2010-08-27 15:43:44 -0400

    Apache 2 license, Cloudant copyright when appropriate

--- src/fabric_rpc.erl
+++ src/fabric_rpc.erl
@@ -1,3 +1,17 @@
+% Copyright 2010 Cloudant
+% 
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
 -module(fabric_rpc).
 
 -export([get_db_info/1, get_doc_count/1, get_update_seq/1]).
