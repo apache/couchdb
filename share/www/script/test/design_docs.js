da6167eb1d0c893925b98116fe2c17cd20e36760
John Christopher Anderson <jchris@apache.org>
2010-09-12 09:03:38 +0000

    commonjs require no longer creates circular references
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/branches/1.0.x@996269 13f79535-47bb-0310-9956-ffa450edef68

--- share/www/script/test/design_docs.js
+++ share/www/script/test/design_docs.js
@@ -41,8 +41,8 @@ function() {
     whatever : {
       stringzone : "exports.string = 'plankton';",
       commonjs : {
-        whynot : "exports.test = require('../stringzone')",
-        upper : "exports.testing = require('./whynot').test.string.toUpperCase()+module.id"
+        whynot : "exports.test = require('../stringzone'); exports.foo = require('whatever/stringzone');",
+        upper : "exports.testing = require('./whynot').test.string.toUpperCase()+module.id+require('./whynot').foo.string"
       }
     },
     views: {
@@ -86,7 +86,7 @@ function() {
   // test commonjs require
   var xhr = CouchDB.request("GET", "/test_suite_db/_design/test/_show/requirey");
   T(xhr.status == 200);
-  TEquals("PLANKTONwhatever/commonjs/upper", xhr.responseText);
+  TEquals("PLANKTONwhatever/commonjs/upperplankton", xhr.responseText);
 
   // test that we get design doc info back
   var dinfo = db.designInfo("_design/test");
