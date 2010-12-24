cd0fbea23da9bb596a9bbbebb90bc0a6444a3510
Adam Kocoloski <adam@cloudant.com>
2010-12-24 15:46:40 -0500

    Return the raw open_doc response in open_db_doc/1

--- src/mem3_util.erl
+++ src/mem3_util.erl
@@ -71,12 +71,7 @@ attach_nodes([S | Rest], Acc, [Node | Nodes], UsedNodes) ->
 open_db_doc(DocId) ->
     DbName = ?l2b(couch_config:get("mem3", "shard_db", "dbs")),
     {ok, Db} = couch_db:open(DbName, []),
-    try couch_db:open_doc(Db, DocId, []) of
-    {ok, Doc} ->
-        Doc
-    after
-        couch_db:close(Db)
-    end.
+    try couch_db:open_doc(Db, DocId, []) after couch_db:close(Db) end.
 
 write_db_doc(Doc) ->
     DbName = ?l2b(couch_config:get("mem3", "shard_db", "dbs")),
