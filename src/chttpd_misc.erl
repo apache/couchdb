4b7394d5100fef20934209abedeed11703c44415
Robert Dionne <bob@cloudant.com>
2011-01-25 07:38:33 -0500

    BugzID: 11719 all_dbs no longer pulls from partition map.
    The partition map is updated from a changes listener. When new dbs are created an immediate all_dbs call may not reflect the latest state. all_dbs now pulls directly from the shard_db db.

--- src/chttpd_misc.erl
+++ src/chttpd_misc.erl
@@ -88,11 +88,34 @@ handle_sleep_req(Req) ->
     send_method_not_allowed(Req, "GET,HEAD").
 
 handle_all_dbs_req(#httpd{method='GET'}=Req) ->
-    {ok, DbNames} = fabric:all_dbs(),
-    send_json(Req, DbNames);
+    ShardDbName = couch_config:get("mem3", "shard_db", "dbs"),
+    %% shard_db is not sharded but mem3:shards treats it as an edge case
+    %% so it can be pushed thru fabric
+    {ok, Info} = fabric:get_db_info(ShardDbName),
+    Etag = couch_httpd:make_etag({Info}),
+    chttpd:etag_respond(Req, Etag, fun() ->
+        {ok, Resp} = chttpd:start_json_response(Req, 200, [{"Etag",Etag}]),
+        fabric:all_docs(ShardDbName, fun all_dbs_callback/2,
+            {nil, Resp}, #view_query_args{})
+    end);
 handle_all_dbs_req(Req) ->
     send_method_not_allowed(Req, "GET,HEAD").
 
+all_dbs_callback({total_and_offset, _Total, _Offset}, {_, Resp}) ->
+    send_chunk(Resp, "["),
+    {ok, {"", Resp}};
+all_dbs_callback({row, {Row}}, {Prepend, Resp}) ->
+    case couch_util:get_value(id, Row) of <<"_design", _/binary>> ->
+        {ok, {Prepend, Resp}};
+    DbName ->
+        send_chunk(Resp, [Prepend, ?JSON_ENCODE(DbName)]),
+        {ok, {",", Resp}}
+    end;
+all_dbs_callback(complete, {_, Resp}) ->
+    send_chunk(Resp, "]"),
+    end_json_response(Resp);
+all_dbs_callback({error, Reason}, {_, Resp}) ->
+    chttpd:send_chunked_error(Resp, {error, Reason}).
 
 handle_task_status_req(#httpd{method='GET'}=Req) ->
     {Replies, _BadNodes} = gen_server:multi_call(couch_task_status, all),
