112abf7efa8c8bd5b7e306dbc0519298dde234d0
Filipe David Borba Manana <fdmanana@apache.org>
2011-05-13 12:45:27 +0000

    Avoid ocassional replication.js test failures due to timing issues
    This happens mostly on slower machines.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@1102700 13f79535-47bb-0310-9956-ffa450edef68

--- share/www/script/test/replication.js
+++ share/www/script/test/replication.js
@@ -1244,6 +1244,7 @@ couchTests.replication = function(debug) {
 
   var tasksAfter = JSON.parse(xhr.responseText);
   TEquals(tasks.length, tasksAfter.length);
+  waitForSeq(sourceDb, targetDb);
   T(sourceDb.open("30") !== null);
 
   // cancel replication
