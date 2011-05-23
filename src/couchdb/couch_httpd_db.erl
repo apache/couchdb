8520662745b108f9ea78952bb4c45d0e3dd4b883
Filipe David Borba Manana <fdmanana@apache.org>
2011-05-23 10:53:37 +0000

    Fix timing issues in the doc PUT multipart/related API
    Two issues were present:
    
    1) the handler replied to the request before the multipart parser consumed
       all the request's data, causing a subsequent request in the same connection
       to consume the remaining data from the multipart/related request;
    2) the data function passed to the multipart parser could consume, and
       discard, all or part of the data from a subsequent request in the same
       connection.
    
    This closes COUCHDB-1174.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@1126426 13f79535-47bb-0310-9956-ffa450edef68

--- src/couchdb/couch_httpd_db.erl
+++ src/couchdb/couch_httpd_db.erl
@@ -691,10 +691,12 @@ db_doc_req(#httpd{method='PUT'}=Req, Db, DocId) ->
     RespHeaders = [{"Location", Loc}],
     case couch_util:to_list(couch_httpd:header_value(Req, "Content-Type")) of
     ("multipart/related;" ++ _) = ContentType ->
-        {ok, Doc0} = couch_doc:doc_from_multi_part_stream(ContentType,
-                fun() -> receive_request_data(Req) end),
+        {ok, Doc0, WaitFun} = couch_doc:doc_from_multi_part_stream(
+            ContentType, fun() -> receive_request_data(Req) end),
         Doc = couch_doc_from_req(Req, DocId, Doc0),
-        update_doc(Req, Db, DocId, Doc, RespHeaders, UpdateType);
+        Result = update_doc(Req, Db, DocId, Doc, RespHeaders, UpdateType),
+        WaitFun(),
+        Result;
     _Else ->
         case couch_httpd:qs_value(Req, "batch") of
         "ok" ->
@@ -829,7 +831,14 @@ send_ranges_multipart(Req, ContentType, Len, Att, Ranges) ->
     {ok, Resp}.
 
 receive_request_data(Req) ->
-    {couch_httpd:recv(Req, 0), fun() -> receive_request_data(Req) end}.
+    receive_request_data(Req, couch_httpd:body_length(Req)).
+
+receive_request_data(Req, LenLeft) when LenLeft > 0 ->
+    Len = erlang:min(4096, LenLeft),
+    Data = couch_httpd:recv(Req, Len),
+    {Data, fun() -> receive_request_data(Req, LenLeft - iolist_size(Data)) end};
+receive_request_data(_Req, _) ->
+    throw(<<"expected more data">>).
     
 make_content_range(From, To, Len) ->
     ?l2b(io_lib:format("bytes ~B-~B/~B", [From, To, Len])).
