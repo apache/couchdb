7f6ad63f4ba238ec0b81085d04b151cf5f145ec5
Robert Newson <rnewson@apache.org>
2011-05-26 22:27:51 +0000

    COUCHDB-1178 - fix make_first_doc_on_disk function_clause error
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/branches/1.0.x@1128112 13f79535-47bb-0310-9956-ffa450edef68

--- share/www/script/couch_tests.js
+++ share/www/script/couch_tests.js
@@ -73,6 +73,7 @@ loadTest("reduce.js");
 loadTest("reduce_builtin.js");
 loadTest("reduce_false.js");
 loadTest("reduce_false_temp.js");
+loadTest("regression.js");
 loadTest("replication.js");
 loadTest("rev_stemming.js");
 loadTest("rewrite.js");
