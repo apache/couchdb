72c90cd279c68cd219e94d89621a72f7cc5d8347
Robert Newson <rnewson@apache.org>
2011-05-28 17:05:54 +0000

    move COUCHDB-1178 test to conflicts.js
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@1128709 13f79535-47bb-0310-9956-ffa450edef68

--- share/www/script/test/conflicts.js
+++ share/www/script/test/conflicts.js
@@ -61,4 +61,29 @@ couchTests.conflicts = function(debug) {
 
   T(db.save(doc2).ok);  // we can save a new document over a deletion without
                         // knowing the deletion rev.
+
+  // Verify COUCHDB-1178
+  var r1 = {"_id":"doc","foo":"bar"};
+  var r2 = {"_id":"doc","foo":"baz","_rev":"1-4c6114c65e295552ab1019e2b046b10e"};
+  var r3 = {"_id":"doc","foo":"bam","_rev":"2-cfcd6781f13994bde69a1c3320bfdadb"};
+  var r4 = {"_id":"doc","foo":"bat","_rev":"3-cc2f3210d779aef595cd4738be0ef8ff"};
+
+  T(db.save({"_id":"_design/couchdb-1178","validate_doc_update":"function(){}"}).ok);
+  T(db.save(r1).ok);
+  T(db.save(r2).ok);
+  T(db.save(r3).ok);
+
+  T(db.compact().ok);
+  while (db.info().compact_running) {};
+
+  TEquals({"_id":"doc",
+        "_rev":"3-cc2f3210d779aef595cd4738be0ef8ff",
+        "foo":"bam",
+        "_revisions":{"start":3,
+          "ids":["cc2f3210d779aef595cd4738be0ef8ff",
+                 "cfcd6781f13994bde69a1c3320bfdadb",
+                                      "4c6114c65e295552ab1019e2b046b10e"]}},
+    db.open("doc", {"revs": true}));
+  TEquals([], db.bulkSave([r4, r3, r2], {"new_edits":false}), "no failures");
+
 };
