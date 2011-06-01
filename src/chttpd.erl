0d01e99d8200614e5cd7b701b9ae956094406658
Robert Newson <robert.newson@cloudant.com>
2011-06-01 17:19:58 +0100

    add empty jsonstack to send_method_not_allowed

--- src/chttpd.erl
+++ src/chttpd.erl
@@ -559,7 +559,7 @@ send_response(#httpd{mochi_req=MochiReq}=Req, Code, Headers, Body) ->
 
 send_method_not_allowed(Req, Methods) ->
     send_error(Req, 405, [{"Allow", Methods}], <<"method_not_allowed">>,
-        ?l2b("Only " ++ Methods ++ " allowed")).
+        ?l2b("Only " ++ Methods ++ " allowed"), []).
 
 send_json(Req, Value) ->
     send_json(Req, 200, Value).
