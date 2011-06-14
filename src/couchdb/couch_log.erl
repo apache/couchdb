f0ffa445b99fd11ebd8770163de7761718c4c187
Paul Joseph Davis <davisp@apache.org>
2011-06-14 00:29:08 +0000

    COUCHDB-1158 - Allow Unicode in in log messages.
    This is a hand crafted backport of 1101896 from trunk
    because the logger has had many other changes.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/branches/1.0.x@1135349 13f79535-47bb-0310-9956-ffa450edef68

--- src/couchdb/couch_log.erl
+++ src/couchdb/couch_log.erl
@@ -131,11 +131,11 @@ terminate(_Arg, {Fd, _LoggingLevel, _Sasl}) ->
     file:close(Fd).
 
 log(Fd, Pid, Level, Format, Args) ->
-    Msg = io_lib:format(Format, Args),
-    ok = io:format("[~s] [~p] ~s~n", [Level, Pid, Msg]), % dump to console too
-    Msg2 = re:replace(lists:flatten(Msg),"\\r\\n|\\r|\\n", "\r\n",
-        [global, {return, list}]),
-    ok = io:format(Fd, "[~s] [~s] [~p] ~s\r~n\r~n", [httpd_util:rfc1123_date(), Level, Pid, Msg2]).
+    ConsoleMsg = unicode:characters_to_binary(io_lib:format(
+            "[~s] [~p] " ++ Format ++ "~n", [Level, Pid | Args])),
+    FileMsg = ["[", httpd_util:rfc1123_date(), "] ", ConsoleMsg],
+    ok = io:put_chars(ConsoleMsg),
+    ok = io:put_chars(Fd, iolist_to_binary(FileMsg)).
 
 read(Bytes, Offset) ->
     LogFileName = couch_config:get("log", "file"),
