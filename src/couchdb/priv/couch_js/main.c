7b0f330627c9f3ef1ccb9e3ffe1e909e3a27f1bf
Randall Leeds <randall@apache.org>
2011-06-19 23:09:42 +0000

    Fix COUCHDB-1078 - Port couchjs to newest libmozjs
    As usually, please test and report any build system problems as I
    cannot test every configuration.
    
    Thanks to Chris Coulson for the original patch.
    
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@1137464 13f79535-47bb-0310-9956-ffa450edef68

--- src/couchdb/priv/couch_js/main.c
+++ src/couchdb/priv/couch_js/main.c
@@ -18,6 +18,7 @@
 
 #include "utf8.h"
 #include "http.h"
+#include "jscompat.h"
 
 int gExitCode = 0;
 
@@ -33,9 +34,25 @@ int gExitCode = 0;
 #define FINISH_REQUEST(cx)
 #endif
 
+
+static JSClass global_class = {
+    "GlobalClass",
+    JSCLASS_GLOBAL_FLAGS,
+    JS_PropertyStub,
+    JS_PropertyStub,
+    JS_PropertyStub,
+    JS_SETPROPERTY_PROPERTY_STUB,
+    JS_EnumerateStub,
+    JS_ResolveStub,
+    JS_ConvertStub,
+    JS_FinalizeStub,
+    JSCLASS_NO_OPTIONAL_MEMBERS
+};
+
 static JSBool
-evalcx(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+evalcx(JSContext* cx, uintN argc, jsval* vp)
 {
+    jsval* argv = JS_ARGV(cx, vp);
     JSString *str;
     JSObject *sandbox;
     JSContext *subcx;
@@ -43,6 +60,9 @@ evalcx(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
     size_t srclen;
     JSBool ret = JS_FALSE;
     jsval v;
+#ifdef HAVE_COMPARTMENTS
+    JSCrossCompartmentCall *call = NULL;
+#endif
 
     sandbox = NULL;
     if(!JS_ConvertArguments(cx, argc, argv, "S / o", &str, &sandbox))
@@ -59,42 +79,69 @@ evalcx(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
     SETUP_REQUEST(subcx);
 
+#ifdef HAVE_JS_GET_STRING_CHARS_AND_LENGTH
+    src = JS_GetStringCharsAndLength(cx, str, &srclen);
+#else
     src = JS_GetStringChars(str);
     srclen = JS_GetStringLength(str);
+#endif
+
+#ifdef HAVE_COMPARTMENTS
+    // Re-use the compartment associated with the main context,
+    // rather than creating a new compartment */
+    JSObject *global = JS_GetGlobalObject(cx);
+    if(!global)
+    {
+       goto done;
+    }
+    call = JS_EnterCrossCompartmentCall(subcx, global);
+#endif
 
     if(!sandbox)
     {
+#ifdef HAVE_JS_NEW_GLOBAL_OBJECT
+        sandbox = JS_NewGlobalObject(subcx, &global_class);
+#else
         sandbox = JS_NewObject(subcx, NULL, NULL, NULL);
+#endif
         if(!sandbox || !JS_InitStandardClasses(subcx, sandbox)) goto done;
     }
 
     if(srclen == 0)
     {
-        *rval = OBJECT_TO_JSVAL(sandbox);
+        JS_SET_RVAL(cx, vp, OBJECT_TO_JSVAL(sandbox));
     }
     else
     {
-        JS_EvaluateUCScript(subcx, sandbox, src, srclen, NULL, 0, rval);
+        JS_EvaluateUCScript(subcx, sandbox, src, srclen, NULL, 0, &JS_RVAL(cx, vp));
     }
     
     ret = JS_TRUE;
 
 done:
+#ifdef HAVE_COMPARTMENTS
+    if(call)
+    {
+        JS_LeaveCrossCompartmentCall(call);
+    }
+#endif
     FINISH_REQUEST(subcx);
     JS_DestroyContext(subcx);
     return ret;
 }
 
 static JSBool
-gc(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+gc(JSContext* cx, uintN argc, jsval* vp)
 {
     JS_GC(cx);
+    JS_SET_RVAL(cx, vp, JSVAL_VOID);
     return JS_TRUE;
 }
 
 static JSBool
-print(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+print(JSContext *cx, uintN argc, jsval *vp)
 {
+    jsval *argv = JS_ARGV(cx, vp);
     uintN i;
     char *bytes;
 
@@ -109,14 +156,45 @@ print(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
     fputc('\n', stdout);
     fflush(stdout);
+    JS_SET_RVAL(cx, vp, JSVAL_VOID);
     return JS_TRUE;
 }
 
 static JSBool
-quit(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+quit(JSContext *cx, uintN argc, jsval *vp)
 {
+    jsval *argv = JS_ARGV(cx, vp);
     JS_ConvertArguments(cx, argc, argv, "/ i", &gExitCode);
     return JS_FALSE;
+}
+
+static int
+couchjs_fgets(char *buf, int size, FILE *file)
+{
+    int n, i, c;
+    JSBool crflag;
+
+    n = size - 1;
+    if (n < 0)
+        return -1;
+
+    crflag = JS_FALSE;
+    for (i = 0; i < n && (c = getc(file)) != EOF; i++)
+    {
+        buf[i] = c;
+        if (c == '\n') {        /* any \n ends a line */
+            i++;                /* keep the \n; we know there is room for \0 */
+            break;
+        }
+        if (crflag) {           /* \r not followed by \n ends line at the \r */
+            ungetc(c, file);
+            break;              /* and overwrite c in buf with \0 */
+        }
+        crflag = (c == '\r');
+    }
+
+    buf[i] = '\0';
+    return i;
 }
 
 static char*
@@ -131,7 +209,7 @@ readfp(JSContext* cx, FILE* fp, size_t* buflen)
     bytes = JS_malloc(cx, byteslen);
     if(bytes == NULL) return NULL;
     
-    while((readlen = js_fgets(bytes+used, byteslen-used, stdin)) > 0)
+    while((readlen = couchjs_fgets(bytes+used, byteslen-used, stdin)) > 0)
     {
         used += readlen;
 
@@ -157,7 +235,7 @@ readfp(JSContext* cx, FILE* fp, size_t* buflen)
 }
 
 static JSBool
-readline(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+readline(JSContext *cx, uintN argc, jsval *vp) {
     jschar *chars;
     JSString *str;
     char* bytes;
@@ -173,7 +251,7 @@ readline(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
     /* Treat the empty string specially */
     if(byteslen == 0)
     {
-        *rval = JS_GetEmptyStringValue(cx);
+        JS_SET_RVAL(cx, vp, JS_GetEmptyStringValue(cx));
         JS_free(cx, bytes);
         return JS_TRUE;
     }
@@ -192,27 +270,37 @@ readline(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
 
     if(!str) return JS_FALSE;
 
-    *rval = STRING_TO_JSVAL(str);
+    JS_SET_RVAL(cx, vp, STRING_TO_JSVAL(str));
 
     return JS_TRUE;
 }
 
 static JSBool
-seal(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval) {
+seal(JSContext *cx, uintN argc, jsval *vp) {
+    jsval *argv = JS_ARGV(cx, vp);
     JSObject *target;
     JSBool deep = JS_FALSE;
+    JSBool ret;
 
     if (!JS_ConvertArguments(cx, argc, argv, "o/b", &target, &deep))
         return JS_FALSE;
     if (!target)
+    {
+        JS_SET_RVAL(cx, vp, JSVAL_VOID);
         return JS_TRUE;
-    return JS_SealObject(cx, target, deep);
+    }
+
+    COUCHJS_SEAL_OBJECT(ret, cx, target, deep);
+    if (ret == JS_TRUE)
+        JS_SET_RVAL(cx, vp, JSVAL_VOID);
+
+    return ret;
 }
 
 static void
 execute_script(JSContext *cx, JSObject *obj, const char *filename) {
     FILE *file;
-    JSScript *script;
+    COUCHJS_SCRIPT *script;
     jsval result;
 
     if(!filename || strcmp(filename, "-") == 0)
@@ -234,7 +322,7 @@ execute_script(JSContext *cx, JSObject *obj, const char *filename) {
     if(script)
     {
         JS_ExecuteScript(cx, obj, script, &result);
-        JS_DestroyScript(cx, script);
+        COUCHJS_DESTROY_SCRIPT(cx, script);
     }
 }
 
@@ -248,27 +336,13 @@ printerror(JSContext *cx, const char *mesg, JSErrorReport *report)
 }
 
 static JSFunctionSpec global_functions[] = {
-    {"evalcx", evalcx, 0, 0, 0},
-    {"gc", gc, 0, 0, 0},
-    {"print", print, 0, 0, 0},
-    {"quit", quit, 0, 0, 0},
-    {"readline", readline, 0, 0, 0},
-    {"seal", seal, 0, 0, 0},
-    {0, 0, 0, 0, 0}
-};
-
-static JSClass global_class = {
-    "GlobalClass",
-    JSCLASS_GLOBAL_FLAGS,
-    JS_PropertyStub,
-    JS_PropertyStub,
-    JS_PropertyStub,
-    JS_PropertyStub,
-    JS_EnumerateStub,
-    JS_ResolveStub,
-    JS_ConvertStub,
-    JS_FinalizeStub,
-    JSCLASS_NO_OPTIONAL_MEMBERS
+    JS_FS("evalcx", COUCHJS_NATIVE_FUNC(evalcx), 0, JSFUN_FAST_NATIVE),
+    JS_FS("gc", COUCHJS_NATIVE_FUNC(gc), 0, JSFUN_FAST_NATIVE),
+    JS_FS("print", COUCHJS_NATIVE_FUNC(print), 0, JSFUN_FAST_NATIVE),
+    JS_FS("quit", COUCHJS_NATIVE_FUNC(quit), 0, JSFUN_FAST_NATIVE),
+    JS_FS("readline", COUCHJS_NATIVE_FUNC(readline), 0, JSFUN_FAST_NATIVE),
+    JS_FS("seal", COUCHJS_NATIVE_FUNC(seal), 0, JSFUN_FAST_NATIVE),
+    JS_FS_END
 };
 
 int
@@ -284,6 +358,9 @@ main(int argc, const char * argv[])
     JSRuntime* rt = NULL;
     JSContext* cx = NULL;
     JSObject* global = NULL;
+#ifdef HAVE_COMPARTMENTS
+    JSCrossCompartmentCall *call = NULL;
+#endif
     JSFunctionSpec* sp = NULL;
     const char* script_name = NULL;
     int use_http = 0;
@@ -325,8 +402,18 @@ main(int argc, const char * argv[])
     
     SETUP_REQUEST(cx);
 
+#ifdef HAVE_COMPARTMENTS
+    global = JS_NewCompartmentAndGlobalObject(cx, &global_class, NULL);
+    if (!global) return 1;
+    call = JS_EnterCrossCompartmentCall(cx, global);
+#elif HAVE_JS_NEW_GLOBAL_OBJECT
+    global = JS_NewGlobalObject(cx, &global_class);
+    if (!global) return 1;
+#else
     global = JS_NewObject(cx, &global_class, NULL, NULL);
     if (!global) return 1;
+    JS_SetGlobalObject(cx, global);
+#endif
     if (!JS_InitStandardClasses(cx, global)) return 1;
     
     for(sp = global_functions; sp->name != NULL; sp++)
@@ -344,9 +431,11 @@ main(int argc, const char * argv[])
         return 2;
     }
 
-    JS_SetGlobalObject(cx, global);
     execute_script(cx, global, script_name);
 
+#ifdef HAVE_COMPARTMENTS
+    JS_LeaveCrossCompartmentCall(call);
+#endif
     FINISH_REQUEST(cx);
 
     JS_DestroyContext(cx);
