8d53b7d926c9169c914f6421e681842529d9ac3f
Filipe David Borba Manana <fdmanana@apache.org>
2011-07-06 12:27:15 +0000

    Merge revision 1143375 from trunk
        Redefine logging macros
    
        With these macro definitions we don't evaluate the arguments
        if the corresponding log level is not enabled.
        This behaviour was accidently removed by the patch from
        COUCHDB-1054.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/branches/1.1.x@1143376 13f79535-47bb-0310-9956-ffa450edef68

--- src/couchdb/couch_db.hrl
+++ src/couchdb/couch_db.hrl
@@ -25,8 +25,20 @@
 
 -define(DEFAULT_ATTACHMENT_CONTENT_TYPE, <<"application/octet-stream">>).
 
--define(LOG_DEBUG(Format, Args), couch_log:debug(Format, Args)).
--define(LOG_INFO(Format, Args), couch_log:info(Format, Args)).
+-define(LOG_DEBUG(Format, Args),
+    case couch_log:debug_on() of
+        true ->
+            couch_log:debug(Format, Args);
+        false -> ok
+    end).
+
+-define(LOG_INFO(Format, Args),
+    case couch_log:info_on() of
+        true ->
+            couch_log:info(Format, Args);
+        false -> ok
+    end).
+
 -define(LOG_ERROR(Format, Args), couch_log:error(Format, Args)).
 
 -record(rev_info,
