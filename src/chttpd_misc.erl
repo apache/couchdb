b856e58595d373b647e78ff0cb216b17e60e479b
Robert Dionne <bob@cloudant.com>
2011-07-28 08:04:07 -0400

    Remove unused imports

--- src/chttpd_misc.erl
+++ src/chttpd_misc.erl
@@ -22,9 +22,9 @@
 -include_lib("couch/include/couch_db.hrl").
 
 -import(chttpd,
-    [send_json/2,send_json/3,send_json/4,send_method_not_allowed/2,
-    start_json_response/2,send_chunk/2,end_json_response/1,
-    start_chunked_response/3, send_error/4]).
+    [send_json/2,send_json/3,send_method_not_allowed/2,
+    send_chunk/2,end_json_response/1,
+    start_chunked_response/3]).
 
 % httpd global handlers
 
