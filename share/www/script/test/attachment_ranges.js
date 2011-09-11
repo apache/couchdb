7b80fe585400a694631017e2537325e3cccfce01
Robert Newson <rnewson@apache.org>
2011-09-11 10:50:11 +0000

    Reject large Range requests
    Return a 200 response for any Range request that covers the entire entity
    or that contains more than 10 byte ranges.
    
    git-svn-id: https://svn.apache.org/repos/asf/couchdb/trunk@1168196 13f79535-47bb-0310-9956-ffa450edef68

--- share/www/script/test/attachment_ranges.js
+++ share/www/script/test/attachment_ranges.js
@@ -32,7 +32,7 @@ couchTests.attachment_ranges = function(debug) {
     T(save_response.ok);
 
     // Fetching the whole entity is a 206.
-    var xhr = CouchDB.request("GET", "/test_suite_db/bin_doc/foo.txt", {
+    var xhr = CouchDB.request("GET", "/test_suite_db/bin_doc/foo.txt?bar=0", {
         headers: {
             "Range": "bytes=0-28"
         }
@@ -43,7 +43,7 @@ couchTests.attachment_ranges = function(debug) {
     TEquals("29", xhr.getResponseHeader("Content-Length"));
 
     // Fetch the whole entity without an end offset is a 200.
-    var xhr = CouchDB.request("GET", "/test_suite_db/bin_doc/foo.txt", {
+    var xhr = CouchDB.request("GET", "/test_suite_db/bin_doc/foo.txt?bar=1", {
         headers: {
             "Range": "bytes=0-"
         }
@@ -52,6 +52,14 @@ couchTests.attachment_ranges = function(debug) {
     TEquals("This is a base64 encoded text", xhr.responseText);
     TEquals(null, xhr.getResponseHeader("Content-Range"));
     TEquals("29", xhr.getResponseHeader("Content-Length"));
+
+    // Even if you ask multiple times.
+    var xhr = CouchDB.request("GET", "/test_suite_db/bin_doc/foo.txt", {
+        headers: {
+            "Range": "bytes=0-,0-,0-"
+        }
+    });
+    TEquals(200, xhr.status, "multiple 0-'s");
 
     // Badly formed range header is a 200.
     var xhr = CouchDB.request("GET", "/test_suite_db/bin_doc/foo.txt", {
@@ -130,5 +138,13 @@ couchTests.attachment_ranges = function(debug) {
         }
     });
     TEquals(416, xhr.status, "fetch 300-310");
+
+    // We ignore a Range header with too many ranges
+    var xhr = CouchDB.request("GET", "/test_suite_db/bin_doc/foo.txt", {
+        headers: {
+            "Range": "bytes=0-1,0-1,0-1,0-1,0-1,0-1,0-1,0-1,0-1,0-1"
+        }
+    });
+    TEquals(200, xhr.status, "too many ranges");
 
 };
