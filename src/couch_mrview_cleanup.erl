7f1a4dd58787a86bc9f8603df9e43c9bade722b0
Randall Leeds <randall@apache.org>
2011-11-09 01:33:47 -0800

    couch_db:get_design_docs/1 -> #full_doc_info{}
    Rather than open every design doc at once with this call, just return
    the full doc info. It's the cheapest thing to do because it lets other
    code open the documents as needed.

--- src/couch_mrview_cleanup.erl
+++ src/couch_mrview_cleanup.erl
@@ -23,14 +23,15 @@ run(Db) ->
     RootDir = couch_index_util:root_dir(),
     DbName = couch_db:name(Db),
 
-    {ok, DesignDocs} = couch_db:get_design_docs(Db),
-    SigFiles = lists:foldl(fun(DDoc, SFAcc) ->
+    DesignDocs = couch_db:get_design_docs(Db),
+    SigFiles = lists:foldl(fun(DDocInfo, SFAcc) ->
+        {ok, DDoc} = couch_db:open_doc_int(Db, DDocInfo, [ejson_body]),
         {ok, InitState} = couch_mrview_util:ddoc_to_mrst(DbName, DDoc),
         Sig = InitState#mrst.sig,
         IFName = couch_mrview_util:index_file(DbName, Sig),
         CFName = couch_mrview_util:compaction_file(DbName, Sig),
         [IFName, CFName | SFAcc]
-    end, [], [DD || DD <- DesignDocs, DD#doc.deleted == false]),
+    end, [], [DD || DD <- DesignDocs, DD#full_doc_info.deleted == false]),
 
     IdxDir = couch_index_util:index_dir(mrview, DbName),
     DiskFiles = filelib:wildcard(filename:join(IdxDir, "*")),
