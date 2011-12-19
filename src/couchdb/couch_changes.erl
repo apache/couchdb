apache/couchdb

commit 6a04e333663596aba4a4d8889e63459b95ef99ff

Fix COUCHDB-1363 - race condition in couch_changes
It's necessary to re-open the #db after subscribing to notifications
so that updates are not lost. In practice, this is rarely problematic
because the next change will cause everything to catch up, but if a
quick burst of changes happens while replication is starting the
replication can go stale. Detected by intermittent replicator_db js
test failures.

src/couchdb/couch_changes.erl
@@ -20,23 +20,28 @@ handle_changes(#changes_args{style=Style}=Args1, Req, Db) ->
     #changes_args{feed = Feed} = Args = Args1#changes_args{
         filter = make_filter_fun(Args1#changes_args.filter, Style, Req, Db)
     },
-    StartSeq = case Args#changes_args.dir of
-    rev ->
-        couch_db:get_update_seq(Db);
-    fwd ->
-        Args#changes_args.since
+    Start = fun() ->
+        {ok, Db} = couch_db:reopen(Db0),
+        StartSeq = case Dir of
+        rev ->
+            couch_db:get_update_seq(Db);
+        fwd ->
+            Since
+        end,
+        {Db, StartSeq}
     end,
     if Feed == "continuous" orelse Feed == "longpoll" ->
         fun(CallbackAcc) ->
             {Callback, UserAcc} = get_callback_acc(CallbackAcc),
             Self = self(),
             {ok, Notify} = couch_db_update_notifier:start_link(
-                fun({_, DbName}) when DbName == Db#db.name ->
+                fun({_, DbName}) when  Db0#db.name == DbName ->
                     Self ! db_updated;
                 (_) ->
                     ok
                 end
             ),
+            {Db, StartSeq} = Start(),
             UserAcc2 = start_sending_changes(Callback, UserAcc, Feed),
             {Timeout, TimeoutFun} = get_changes_timeout(Args, Callback),
             try
@@ -58,6 +63,7 @@ handle_changes(#changes_args{style=Style}=Args1, Req, Db) ->
     true ->
         fun(CallbackAcc) ->
             {Callback, UserAcc} = get_callback_acc(CallbackAcc),
+            {Db, StartSeq} = Start(),
             UserAcc2 = start_sending_changes(Callback, UserAcc, Feed),
             {ok, {_, LastSeq, _Prepend, _, _, UserAcc3, _, _, _, _}} =
                 send_changes(

commit cd67a2c1ece49ccacd3eaa9d0fc1207e4d3e10d1

Fix build (couch_changes Erlang compilation errors)
Broken in revision 6a04e333663596aba4a4d8889e63459b95ef99ff.

src/couchdb/couch_changes.erl
@@ -16,17 +16,17 @@
 -export([handle_changes/3]).
 
 %% @type Req -> #httpd{} | {json_req, JsonObj()}
-handle_changes(#changes_args{style=Style}=Args1, Req, Db) ->
+handle_changes(#changes_args{style=Style}=Args1, Req, Db0) ->
     #changes_args{feed = Feed} = Args = Args1#changes_args{
-        filter = make_filter_fun(Args1#changes_args.filter, Style, Req, Db)
+        filter = make_filter_fun(Args1#changes_args.filter, Style, Req, Db0)
     },
     Start = fun() ->
         {ok, Db} = couch_db:reopen(Db0),
-        StartSeq = case Dir of
+        StartSeq = case Args#changes_args.dir of
         rev ->
             couch_db:get_update_seq(Db);
         fwd ->
-            Since
+            Args#changes_args.since
         end,
         {Db, StartSeq}
     end,