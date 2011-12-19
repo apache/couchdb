8c700fb1a7d0bf4a8a37883a1d4d32cd2fd2726b
Bob Dionne <bitdiddle@apache.org>
2011-12-19 06:24:07 -0500

    Remove unused records

--- src/couchdb/couch_db.hrl
+++ src/couchdb/couch_db.hrl
@@ -235,32 +235,6 @@
     headers = []
 }).
 
--record(group, {
-    sig=nil,
-    fd=nil,
-    name,
-    def_lang,
-    design_options=[],
-    views,
-    lib,
-    id_btree=nil,
-    current_seq=0,
-    purge_seq=0,
-    query_server=nil,
-    waiting_delayed_commit=nil
-    }).
-
--record(view,
-    {id_num,
-    update_seq=0,
-    purge_seq=0,
-    map_names=[],
-    def,
-    btree=nil,
-    reduce_funs=[],
-    options=[]
-    }).
-
 -record(index_header,
     {seq=0,
     purge_seq=0,
