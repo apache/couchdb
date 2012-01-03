32fb9f8e0416910a2431227b5fbdeef93ccd94bc
Randall Leeds <randall@apache.org>
2012-01-03 14:14:55 -0500

    Use ICU_CPPFLAGS instead of _CFLAGS
    This removes the need for c4f6ff9c6cf5512546d5799f05dd445a4da979b3
    which was made in response to -ansi coming from icu-config on my
    system. Instead, it's more sensible to use the preprocessor flags from
    icu-config which provides the -I header locations and -D definitions
    but doesn't add a bunch of excessive warning flags or ansi conformance.
    
    This reverts commit c4f6ff9c6cf5512546d5799f05dd445a4da979b3.

--- src/couchdb/priv/icu_driver/couch_icu_driver.c
+++ src/couchdb/priv/icu_driver/couch_icu_driver.c
@@ -30,10 +30,6 @@ specific language governing permissions and limitations under the License.
 #include <string.h> /* for memcpy */
 #endif
 
-#define DRIVER_NAME "couch_icu_driver"
-
-DRIVER_INIT(DRIVER_NAME); /* prototype for erl_driver entry point */
-
 typedef struct {
     ErlDrvPort port;
     UCollator* collNoCase;
@@ -159,7 +155,7 @@ ErlDrvEntry couch_driver_entry = {
         NULL,               /* F_PTR output, called when erlang has sent */
         NULL,               /* F_PTR ready_input, called when input descriptor ready */
         NULL,               /* F_PTR ready_output, called when output descriptor ready */
-        (char*)DRIVER_NAME, /* char *driver_name, the argument to open_port */
+        "couch_icu_driver", /* char *driver_name, the argument to open_port */
         NULL,               /* F_PTR finish, called when unloaded */
         NULL,               /* Not used */
         couch_drv_control,  /* F_PTR control, port_command callback */
@@ -177,7 +173,7 @@ ErlDrvEntry couch_driver_entry = {
         NULL,               /* F_PTR process_exit */
 };
 
-DRIVER_INIT(DRIVER_NAME) /* must match name in driver_entry */
+DRIVER_INIT(couch_icu_driver) /* must match name in driver_entry */
 {
         return &couch_driver_entry;
 }
