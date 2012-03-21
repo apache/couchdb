bb569905aa1781beaf3403c98f4079ea06339451
Randall Leeds <randall@apache.org>
2012-03-21 01:46:08 -0700

    Revert "Receive the correct error message to proceed upon ignoring a file-open error"
    Most of the errors here will be of the form {error, Reason} because
    they come from the file module. Instead of minting our own file_exists
    atom lets use {error, eexist}.
    
    This reverts commit 3e5ad086c59af9819f2e93d55f10d61f949002d3.

--- src/couchdb/couch_file.erl
+++ src/couchdb/couch_file.erl
@@ -53,14 +53,14 @@ open(Filepath, Options) ->
     ignore ->
         % get the error
         receive
-        {Ref, Pid, Reason} ->
+        {Ref, Pid, {error, Reason} = Error} ->
             case process_info(self(), trap_exit) of
             {trap_exit, true} -> receive {'EXIT', Pid, _} -> ok end;
             {trap_exit, false} -> ok
             end,
             ?LOG_ERROR("Error opening file ~s: ~s",
                 [Filepath, file:format_error(Reason)]),
-            Reason
+            Error
         end;
     Error ->
         % We can't say much here, because it could be any kind of error.
