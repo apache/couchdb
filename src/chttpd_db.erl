26bc7f72de206fd61ce5f93db910a784d49884b3
Robert Newson <robert.newson@cloudant.com>
2012-09-27 23:48:44 +0100

    Support placement param when creating db's
    Conflicts:
    	src/chttpd_db.erl

--- src/chttpd_db.erl
+++ src/chttpd_db.erl
@@ -171,8 +171,9 @@ create_db_req(#httpd{}=Req, DbName) ->
     couch_httpd:verify_is_server_admin(Req),
     N = couch_httpd:qs_value(Req, "n", couch_config:get("cluster", "n", "3")),
     Q = couch_httpd:qs_value(Req, "q", couch_config:get("cluster", "q", "8")),
+    P = couch_httpd:qs_value(Req, "placement", couch_config:get("cluster", "placement")),
     DocUrl = absolute_uri(Req, "/" ++ couch_util:url_encode(DbName)),
-    case fabric:create_db(DbName, [{n,N}, {q,Q}]) of
+    case fabric:create_db(DbName, [{n,N}, {q,Q}, {placement,P}]) of
     ok ->
         send_json(Req, 201, [{"Location", DocUrl}], {[{ok, true}]});
     accepted ->
