39fe998257a5f29b264c7632c75c2aafc6da5892
Robert Newson <rnewson@apache.org>
2012-11-14 09:59:50 +0000

    Use local UUID instead of local host:port

--- src/couch_replicator/src/couch_replicator.hrl
+++ src/couch_replicator/src/couch_replicator.hrl
@@ -10,7 +10,7 @@
 % License for the specific language governing permissions and limitations under
 % the License.
 
--define(REP_ID_VERSION, 2).
+-define(REP_ID_VERSION, 3).
 
 -record(rep, {
     id,
