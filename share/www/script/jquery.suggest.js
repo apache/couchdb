3734f5da1a1e3e2bc62bb5f50dce9343118e2a99
Noah Slater <nslater@apache.org>
2012-12-03 10:06:29 +0100

    Add docs and search links into Futon

--- share/www/script/jquery.suggest.js
+++ share/www/script/jquery.suggest.js
@@ -1,5 +1,3 @@
-// http://svn.apache.org/repos/asf/couchdb/trunk/share/www/script/jquery.suggest.js
-
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy of
 // the License at
