0a945c274af33c929ee102d8708ddace54edada0
Simon Metson <simon+github@cloudant.com>
2012-12-21 11:16:37 +0000

    move template, get_deps and gen_load_addons into tasks

--- src/fauxton/tasks/fauxton.js
+++ src/fauxton/tasks/fauxton.js
@@ -57,4 +57,59 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerMultiTask('template', 'generates an html file from a specified template', function(){
+    var data = this.data;
+    var _ = grunt.utils._;
+    var tmpl = _.template(grunt.file.read(data.src), null, data.variables);
+    grunt.file.write(data.dest, tmpl(data.variables));
+  });
+
+  grunt.registerMultiTask('get_deps', 'Fetch external dependencies', function() {
+    var child_process = require('child_process');
+    var async = require('async');
+    var path = require('path');
+    var done = this.async();
+    var data = this.data;
+    var target = data.target || "app/addons/";
+    var settingsFile = path.existsSync(data.src) ? data.src : "settings.json.default";
+    var settings = grunt.file.readJSON(settingsFile);
+    var _ = grunt.utils._;
+    var remoteDeps = _.filter(settings.deps, function(dep) { return !! dep.url; });
+
+    async.forEach(remoteDeps, function(dep, cb) {
+      var path = target + dep.name;
+      var command = "git clone " + dep.url + " " + path;
+      console.log("Cloning: " + dep.name + "(" + dep.url + ")");
+
+      child_process.exec(command, function(error, stdout, stderr) {
+        console.log(stderr);
+        console.log(stdout);
+
+        cb(error);
+      });
+    }, function(error) {
+      if (error) {
+        console.log("ERROR: " + error.message);
+        done(false);
+      } else {
+        done();
+      }
+    });
+  });
+
+  grunt.registerMultiTask('gen_load_addons', 'Generate the load_addons.js file', function() {
+    var path = require('path');
+    var data = this.data;
+    var _ = grunt.utils._;
+    var settingsFile = path.existsSync(data.src) ? data.src : "settings.json.default";
+    var settings = grunt.file.readJSON(settingsFile);
+    var template = "app/load_addons.js.underscore";
+    var dest = "app/load_addons.js";
+    var deps = _.map(settings.deps, function(dep) {
+      return "addons/" + dep.name + "/base";
+    });
+    var tmpl = _.template(grunt.file.read(template));
+    grunt.file.write(dest, tmpl({deps: deps}));
+  });
+
 };
