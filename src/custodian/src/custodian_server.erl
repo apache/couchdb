5104e45f9e06b835c6de972c26528687a5dc947e
Robert Newson <robert.newson@cloudant.com>
2013-01-21 01:14:24 -0500

    Move fold code to _util

--- src/custodian/src/custodian_server.erl
+++ src/custodian/src/custodian_server.erl
@@ -2,8 +2,6 @@
 
 -module(custodian_server).
 -behaviour(gen_server).
--include_lib("mem3/include/mem3.hrl").
--include_lib("couch/include/couch_db.hrl").
 
 % public api.
 -export([start_link/0]).
@@ -69,64 +67,19 @@ update_event_handler(_) ->
     ok.
 
 send_alerts() ->
-    Live = [node() | nodes()],
-    N = list_to_integer(couch_config:get("cluster", "n", "3")),
-    Acc0 = {Live, N, 0, 0},
-    {ok, Db} = custodian_util:ensure_dbs_exists(),
-    try
-        {ok, _, {_, _, Unavailable, Impaired}} = couch_db:enum_docs(Db, fun fold_dbs/3, Acc0, []),
-        case {Unavailable, Impaired} of
-            {0, 0} ->
-                send_all_available_alert();
-            {Unavailable, 0} ->
-                send_unavailable_alert(Unavailable),
-                twig:log(crit, "~B unavailable shards in this cluster", [Unavailable]);
-            {0, Impaired} ->
-                send_impaired_alert(Impaired),
-                twig:log(crit, "~B impaired shards in this cluster", [Impaired]);
-            {Unavailable, Impaired} ->
-                send_unavailable_alert(Unavailable),
-                send_impaired_alert(Impaired),
-                twig:log(crit, "~B unavailable and ~B impaired shards in this cluster", [Unavailable, Impaired])
-        end
-    after
-        couch_db:close(Db)
-    end.
-
-fold_dbs(#full_doc_info{id = <<"_design/", _/binary>>}, _, Acc) ->
-    {ok, Acc};
-fold_dbs(#full_doc_info{deleted=true}, _, Acc) ->
-    {ok, Acc};
-fold_dbs(#full_doc_info{id = Id}, _, Acc) ->
-    Shards = mem3:shards(Id),
-    Rs = [R || #shard{range=R} <- lists:ukeysort(#shard.range, Shards)],
-    ActualN = [{R1, [N || #shard{node=N,range=R2} <- Shards, R1 == R2]} ||  R1 <- Rs],
-    fold_dbs(Id, ActualN, Acc);
-fold_dbs(_Id, [], Acc) ->
-    {ok, Acc};
-fold_dbs(Id, [{Range, Nodes}|Rest], {Live, N, Unavailable, Impaired}) ->
-    Nodes1 = maybe_redirect(Nodes),
-    Nodes2 = [Node || Node <- Nodes1, lists:member(Node, Live)],
-    case length(Nodes2) of
-        0 ->
-            fold_dbs(Id, Rest, {Live, N, Unavailable + 1, Impaired});
-        N1 when N1 < N ->
-            fold_dbs(Id, Rest, {Live, N, Unavailable, Impaired + 1});
-        _ ->
-            fold_dbs(Id, Rest, {Live, N, Unavailable, Impaired})
-    end.
-
-maybe_redirect(Nodes) ->
-    maybe_redirect(Nodes, []).
-
-maybe_redirect([], Acc) ->
-    Acc;
-maybe_redirect([Node|Rest], Acc) ->
-    case couch_config:get("mem3.redirects", atom_to_list(Node)) of
-        undefined ->
-            maybe_redirect(Rest, [Node|Acc]);
-        Redirect ->
-            maybe_redirect(Rest, [list_to_atom(Redirect)|Acc])
+    case custodian_util:summary() of
+        {0, 0} ->
+            send_all_available_alert();
+        {Unavailable, 0} ->
+            send_unavailable_alert(Unavailable),
+            twig:log(crit, "~B unavailable shards in this cluster", [Unavailable]);
+        {0, Impaired} ->
+            send_impaired_alert(Impaired),
+            twig:log(crit, "~B impaired shards in this cluster", [Impaired]);
+        {Unavailable, Impaired} ->
+            send_unavailable_alert(Unavailable),
+            send_impaired_alert(Impaired),
+            twig:log(crit, "~B unavailable and ~B impaired shards in this cluster", [Unavailable, Impaired])
     end.
 
 send_unavailable_alert(Count) when is_integer(Count) ->
--- src/custodian/src/custodian_util.erl
+++ src/custodian/src/custodian_util.erl
@@ -3,9 +3,33 @@
 -module(custodian_util).
 -include("custodian.hrl").
 
--export([ensure_dbs_exists/0]).
+-include_lib("mem3/include/mem3.hrl").
+-include_lib("couch/include/couch_db.hrl").
 
-% public functions.
+-export([summary/0]).
+
+%% public functions.
+
+summary() ->
+    Fun = fun(_Id, _Range,  unavailable, {Unavailable, Impaired}) ->
+                  {Unavailable + 1, Impaired};
+             (_Id, _Range,  {impaired, _N}, {Unavailable, Impaired}) ->
+                  {Unavailable, Impaired + 1}
+          end,
+    fold_dbs({0, 0}, Fun).
+
+%% private functions.
+
+fold_dbs(Acc0, Fun) ->
+    Live = [node() | nodes()],
+    N = list_to_integer(couch_config:get("cluster", "n", "3")),
+    {ok, Db} = ensure_dbs_exists(),
+    try
+        {ok, _, {_, _, _, Acc1}} = couch_db:enum_docs(Db, fun fold_dbs/3, {Live, N, Fun, Acc0}, []),
+        Acc1
+    after
+        couch_db:close(Db)
+    end.
 
 ensure_dbs_exists() ->
     DbName = couch_config:get("mem3", "shards_db", "dbs"),
@@ -13,7 +37,42 @@ ensure_dbs_exists() ->
     ensure_custodian_ddoc_exists(Db),
     {ok, Db}.
 
-% private functions.
+fold_dbs(#full_doc_info{id = <<"_design/", _/binary>>}, _, Acc) ->
+    {ok, Acc};
+fold_dbs(#full_doc_info{deleted=true}, _, Acc) ->
+    {ok, Acc};
+fold_dbs(#full_doc_info{id = Id}, _, Acc) ->
+    Shards = mem3:shards(Id),
+    Rs = [R || #shard{range=R} <- lists:ukeysort(#shard.range, Shards)],
+    ActualN = [{R1, [N || #shard{node=N,range=R2} <- Shards, R1 == R2]} ||  R1 <- Rs],
+    fold_dbs(Id, ActualN, Acc);
+fold_dbs(_Id, [], Acc) ->
+    {ok, Acc};
+fold_dbs(Id, [{Range, Nodes}|Rest], {Live, N, Fun, Acc0}) ->
+    Nodes1 = maybe_redirect(Nodes),
+    Nodes2 = [Node || Node <- Nodes1, lists:member(Node, Live)],
+    Acc1 = case length(Nodes2) of
+        0 ->
+            Fun(Id, Range, unavailable, Acc0);
+        N1 when N1 < N ->
+            Fun(Id, Range, {impaired, N1}, Acc0);
+        _ ->
+            Acc0
+    end,
+    fold_dbs(Id, Rest, {Live, N, Fun, Acc1}).
+
+maybe_redirect(Nodes) ->
+    maybe_redirect(Nodes, []).
+
+maybe_redirect([], Acc) ->
+    Acc;
+maybe_redirect([Node|Rest], Acc) ->
+    case couch_config:get("mem3.redirects", atom_to_list(Node)) of
+        undefined ->
+            maybe_redirect(Rest, [Node|Acc]);
+        Redirect ->
+            maybe_redirect(Rest, [list_to_atom(Redirect)|Acc])
+    end.
 
 ensure_custodian_ddoc_exists(Db) ->
     case couch_db:open_doc(Db, ?CUSTODIAN_ID) of
