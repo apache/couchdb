9d04753f8a0b33ab372aa280e484617475153677
Simon Metson <simon+github@cloudant.com>
2013-02-12 11:00:52 +0000

    add contribute addon

---
+++ src/fauxton/app/addons/contribute/base.js
@@ -0,0 +1,17 @@
+define([
+  // Libraries.
+  "jquery",
+  "lodash"
+],
+function($, _){
+  $.contribute = function(message, file){
+    var JST = window.JST = window.JST || {};
+    var template = JST['app/addons/contribute/templates/modal.html'];
+    console.log(template);
+    var compiled = template({message: message, file: file});
+    console.log('contribute!contribute!monorail!contribute!');
+    console.log(compiled);
+    var elem = $(compiled);
+    elem.modal('show');
+  };
+});
