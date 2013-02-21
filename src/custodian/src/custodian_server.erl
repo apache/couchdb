dc92e668c30c2ed7410ce20e1925e78358fb6d45
Paul J. Davis <paul.joseph.davis@gmail.com>
2013-02-21 12:16:25 -0600

    Rescan shards if asked to during a scan

--- src/custodian/src/custodian_server.erl
+++ src/custodian/src/custodian_server.erl
@@ -19,7 +19,8 @@
 % private records.
 -record(state, {
     update_notifier,
-    shard_checker
+    shard_checker,
+    rescan=false
 }).
 
 % public functions.
@@ -32,34 +33,35 @@ init(_) ->
     process_flag(trap_exit, true),
     net_kernel:monitor_nodes(true),
     {ok, UNPid} = start_update_notifier(),
-    {ok, SCPid} = start_shard_checker(),
-    {ok, #state{
-        update_notifier=UNPid,
-        shard_checker=SCPid
-    }}.
+    {ok, start_shard_checker(#state{
+        update_notifier=UNPid
+    })}.
 
 handle_call(_Msg, _From, State) ->
     {noreply, State}.
 
 handle_cast(refresh, State) ->
-    NewState = start_shard_checker(State),
-    {noreply, NewState}.
+    {noreply, start_shard_checker(State)}.
 
 handle_info({nodeup, _}, State) ->
-    NewState = start_shard_checker(State),
-    {noreply, NewState};
+    {noreply, start_shard_checker(State)};
 
 handle_info({nodedown, _}, State) ->
-    NewState = start_shard_checker(State),
-    {noreply, NewState};
+    {noreply, start_shard_checker(State)};
 
 handle_info({'EXIT', Pid, normal}, #state{shard_checker=Pid}=State) ->
-    {noreply, State#state{shard_checker=undefined}};
+    NewState = State#state{shard_checker=undefined},
+    case State#state.rescan of
+        true ->
+            {noreply, start_shard_checker(NewState)};
+        false ->
+            {noreply, NewState}
+    end;
 
 handle_info({'EXIT', Pid, Reason}, #state{shard_checker=Pid}=State) ->
     twig:log(notice, "custodian shard checker died ~p", [Reason]),
-    {ok, SCPid} = start_shard_checker(),
-    {noreply, State#state{shard_checker=SCPid}};
+    NewState = State#state{shard_checker=undefined},
+    {noreply, start_shard_checker(NewState)};
 
 handle_info({'EXIT', Pid, Reason}, #state{update_notifier=Pid}=State) ->
     twig:log(notice, "custodian update notifier died ~p", [Reason]),
@@ -72,21 +74,24 @@ terminate(_Reason, State) ->
     ok.
 
 code_change(_OldVsn, {state, Pid}, _Extra) ->
-    {ok, #state{update_notifier=Pid}};
+    {ok, #state{
+        update_notifier=Pid,
+        shard_checker=undefined,
+        rescan=false
+    }};
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 % private functions
 
-start_shard_checker() ->
-    {ok, spawn_link(fun ?MODULE:check_shards/0)}.
-
 
 start_shard_checker(#state{shard_checker=undefined}=State) ->
-    {ok, Pid} = start_shard_checker(),
-    State#state{shard_checker=Pid};
+    State#state{
+        shard_checker=spawn_link(fun ?MODULE:check_shards/0),
+        rescan=false
+    };
 start_shard_checker(#state{shard_checker=Pid}=State) when is_pid(Pid) ->
-    State.
+    State#state{rescan=true}.
 
 
 start_update_notifier() ->
