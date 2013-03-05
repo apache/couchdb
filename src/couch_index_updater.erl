d83c669fbe4ea4051938f57bf72be7345099e2dc
Paul J. Davis <paul.joseph.davis@gmail.com>
2013-03-05 13:51:39 -0600

    Refactor couch source tree

--- src/couch_index_updater.erl
+++ src/couch_index_updater.erl
@@ -21,7 +21,7 @@
 -export([init/1, terminate/2, code_change/3]).
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 
--include("couch_db.hrl").
+-include_lib("couch/include/couch_db.hrl").
 
 -record(st, {
     idx,
