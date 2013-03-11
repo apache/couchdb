9667be958948cad2e5d410ca4bc403664d694679
Russell Branca <chewbranca@gmail.com>
2013-03-11 16:33:53 -0700

    Temporarily disable contribute addon and add gitignore

--- src/fauxton/app/addons/contribute/base.js
+++ src/fauxton/app/addons/contribute/base.js
@@ -5,13 +5,17 @@ define([
 ],
 function($, _){
   $.contribute = function(message, file){
+    /*
     var JST = window.JST = window.JST || {};
     var template = JST['app/addons/contribute/templates/modal.html'];
     console.log(template);
     var compiled = template({message: message, file: file});
+    */
     console.log('contribute!contribute!monorail!contribute!');
+    /*
     console.log(compiled);
     var elem = $(compiled);
     elem.modal('show');
+    */
   };
 });
