28c8973bf19cd141ab19803f79e8df4821a0aaf7
Russell Branca <chewbranca@gmail.com>
2013-03-19 16:10:17 -0700

    Fauxton license updates for make distcheck

--- src/fauxton/Gruntfile.js
+++ src/fauxton/Gruntfile.js
@@ -1,7 +1,19 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+
 // This is the main application configuration file.  It is a Grunt
 // configuration file, which you can learn more about here:
 // https://github.com/cowboy/grunt/blob/master/docs/configuring.md
-
 
 module.exports = function(grunt) {
   var helper = require('./tasks/helper').init(grunt),
