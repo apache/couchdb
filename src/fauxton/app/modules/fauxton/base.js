c38ea8ada39116d05f4f2bea7b5dd1c4e93a2b5e
suelockwood <deathbearbrown@gmail.com>
2013-04-08 09:53:25 -0400

    Added the option to pass in a data.  Data is serialized and will be accessible to the template.

--- src/fauxton/app/modules/fauxton/base.js
+++ src/fauxton/app/modules/fauxton/base.js
@@ -99,11 +99,13 @@ function(app, Backbone) {
       this.selector = options.selector;
       this.fade = options.fade === undefined ? true : options.fade;
       this.clear = options.clear;
+      this.data = options.data || "";
       this.template = options.template || "templates/fauxton/notification";
     },
 
     serialize: function() {
       return {
+        data: this.data,
         msg: this.msg,
         type: this.type
       };
