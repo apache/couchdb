b0cbe9811735ba17323fdde892b038010601259b
suelockwood <deathbearbrown@gmail.com>
2013-08-06 10:52:19 -0400

    fixes for windowResize

--- src/fauxton/app/windowResize.js
+++ src/fauxton/app/windowResize.js
@@ -6,14 +6,16 @@ function() {
   var Resize = function(options){
     this.options = options;
     this.options.selectorElements = options.selectorElements || "#dashboard-content";
-    this.initialize();
   };
 
   Resize.prototype = {
+    getPrimaryNavWidth: function(){
+      var primaryNavWidth  = $('body').hasClass('closeMenu')? 66:220;
+      return primaryNavWidth;
+    },
     getPanelWidth: function(){
-      var sidebarWidth = $('#sidebar-content').length > 0?$('#sidebar-content').width(): 40,
-          primeNavWidth = parseInt($('#dashboard').css('left').replace('px',''),10);
-      return (primeNavWidth +sidebarWidth); 
+      var sidebarWidth = $('#sidebar-content').length > 0 ? $('#sidebar-content').width(): 0;
+      return (this.getPrimaryNavWidth() + sidebarWidth); 
     },
     initialize: function(){
       $(window).off('resize');
@@ -23,13 +25,32 @@ function() {
       $(window).on('resize', this.lazyLayout);
       this.onResizeHandler();
     },
+    updateOptions:function(options){
+      this.options = {};
+      this.options = options;
+    },
+    turnOff:function(){
+      $(window).off('resize');
+    },
     onResizeHandler: function (){
       //if there is an override, do that instead
+      console.log("onResizeHandler");
       if (this.options.onResizeHandler){
         this.options.onResizeHandler();
       } else {
-        var panelWidth = (window.innerWidth - this.getPanelWidth() < 1100) ? window.innerWidth - this.getPanelWidth() :1100;
-        $(this.options.selectorElements).width(panelWidth);
+        var combinedWidth = window.innerWidth - this.getPanelWidth(),
+        smallWidthConstraint = ($('#sidebar-content').length > 0)? 470:800,
+        panelWidth; 
+
+        if( combinedWidth > smallWidthConstraint  && combinedWidth < 1400){
+          panelWidth = window.innerWidth - this.getPanelWidth();
+        } else if (combinedWidth < smallWidthConstraint){
+          panelWidth = smallWidthConstraint;
+        } else if(combinedWidth > 1400){
+          panelWidth = 1400;
+        }
+
+        $(this.options.selectorElements).innerWidth(panelWidth);
       }
       //if there is a callback, run that
       if(this.options.callback) {
