65451cd76c08c1add029bcb5a476f7d9f8fbbc4f
Garren Smith <garren.smith@gmail.com>
2013-08-07 18:27:28 +0200

    Fauxton: Spin.js improvements and Active Task clean We now have a spinner for the route object that spins until a view is ready to show a spinner on its section.
    Removed the event listenOnce and added showData to the beforeRender

--- src/fauxton/app/addons/activetasks/views.js
+++ src/fauxton/app/addons/activetasks/views.js
@@ -11,159 +11,163 @@
 // the License.
 
 define([
-  "app",
-  "api",
-  "addons/activetasks/resources"
+       "app",
+       "api",
+       "addons/activetasks/resources"
 ],
 
 function (app, FauxtonAPI, activetasks) {
 
   var Views = {},
-			Events = {},
-			pollingInfo ={
-				rate: "5",
-				intervalId: null
-			};
+      Events = {},
+      pollingInfo ={
+        rate: "5",
+        intervalId: null
+      };
 
 
-	_.extend(Events, Backbone.Events);
+  _.extend(Events, Backbone.Events);
 
-	Views.TabMenu = FauxtonAPI.View.extend({
-		template: "addons/activetasks/templates/tabs",
-		events: {
-			"click .task-tabs li": "requestByType",
-			"change #pollingRange": "changePollInterval"
-		},
-		establish: function(){
-			return [this.model.fetch({reset: true})];
-		},
-		serialize: function(){
-			return {
-				filters: this.model.alltypes
-			};
-		},
-		afterRender: function(){
-			$('.task-tabs').find('li').eq(0).addClass('active');
-		},
-		changePollInterval: function(e){
-			var range = $(e.currentTarget).val();
-			$('label[for="pollingRange"] span').text(range);
-			pollingInfo.rate = range;
-			clearInterval(pollingInfo.intervalId);
-			Events.trigger('update:poll');
-		},
-		requestByType: function(e){
-			var currentTarget = e.currentTarget;
-					datatype = $(currentTarget).attr("data-type");
+  Views.TabMenu = FauxtonAPI.View.extend({
+    template: "addons/activetasks/templates/tabs",
+    events: {
+      "click .task-tabs li": "requestByType",
+      "change #pollingRange": "changePollInterval"
+    },
+    establish: function(){
+      return [this.model.fetch({reset: true})];
+    },
+    serialize: function(){
+      return {
+        filters: this.model.alltypes
+      };
+    },
+    afterRender: function(){
+      $('.task-tabs').find('li').eq(0).addClass('active');
+    },
+    changePollInterval: function(e){
+      var range = $(e.currentTarget).val();
+      $('label[for="pollingRange"] span').text(range);
+      pollingInfo.rate = range;
+      clearInterval(pollingInfo.intervalId);
+      Events.trigger('update:poll');
+    },
+    requestByType: function(e){
+      var currentTarget = e.currentTarget;
+      datatype = $(currentTarget).attr("data-type");
 
-			$('.task-tabs').find('li').removeClass('active');
-			$(currentTarget).addClass('active');
-			this.model.changeView(datatype);
-		}
-	});
+      $('.task-tabs').find('li').removeClass('active');
+      $(currentTarget).addClass('active');
+      this.model.changeView(datatype);
+    }
+  });
 
-	Views.DataSection = FauxtonAPI.View.extend({
-		initialize: function(){
-			this.listenToOnce(this.model, "change", this.showData); //check why I needed to do this
-		},
-		showData: function(){
-			var that = this,
-					currentData = this.model.getCurrentViewData();
-			//remove the old stuff in a nice clean way
-			if (this.dataView) {
-				this.dataView.remove();
-			}
+  Views.DataSection = FauxtonAPI.View.extend({
+    showData: function(){
+      var that = this,
+      currentData = this.model.getCurrentViewData();
+      //remove the old stuff in a nice clean way
+      if (this.dataView) {
+        this.dataView.remove();
+      }
 
-				//add the new stuff
-			this.dataView = that.insertView( new Views.tableData({ 
-				collection: currentData,
-				currentView: this.model.get('currentView').replace('_',' ')
-			}));
+      //add the new stuff
+      this.dataView = that.insertView( new Views.TableData({ 
+        collection: currentData,
+        currentView: this.model.get('currentView').replace('_',' ')
+      }));
 
-			this.dataView.render();
-		},
-		establish: function(){
-			return [this.model.fetch()];
-		},
-		setPolling: function(){
-			var that = this;
-			clearInterval(pollingInfo.intervalId);
-			pollingInfo.intervalId = setInterval(function() {
-				that.establish();
-			}, pollingInfo.rate*1000);
-		},
-		cleanup: function(){
-			clearInterval(pollingInfo.intervalId);
-		},
-		afterRender: function(){
-			this.listenTo(this.model, "change", this.showData);
-			Events.bind('update:poll', this.setPolling, this);
-			this.setPolling();
-		}
-	});
+    },
+    showDataAndRender: function () {
+      this.showData();
+      this.render();
+    },
 
-	Views.tableData = FauxtonAPI.View.extend({
-		tagName: "table",
-		className: "table table-bordered table-striped active-tasks",
-		template: "addons/activetasks/templates/table",
-		events: {
-			"click th": "sortByType"
-		},
-		initialize: function(){
-			currentView = this.options.currentView;
-		},
-		sortByType:  function(e){
-			var currentTarget = e.currentTarget;
-					datatype = $(currentTarget).attr("data-type");
-			this.collection.sortByColumn(datatype);
-			this.render();
-		},
-		serialize: function(){
-			return {
-				currentView: currentView,
-				collection: this.collection
-			};
-		},
-		beforeRender: function(){
-			//iterate over the collection to add each
-			this.collection.forEach(function(item) {
-				this.insertView("#tasks_go_here", new Views.TableDetail({ 
-					model: item
-				}));
-			}, this);
-		}
-	});
+    beforeRender: function () {
+      this.showData();
+    },
+    establish: function(){
+      return [this.model.fetch()];
+    },
+    setPolling: function(){
+      var that = this;
+      clearInterval(pollingInfo.intervalId);
+      pollingInfo.intervalId = setInterval(function() {
+        that.establish();
+      }, pollingInfo.rate*1000);
+    },
+    cleanup: function(){
+      clearInterval(pollingInfo.intervalId);
+    },
+    afterRender: function(){
+      this.listenTo(this.model, "change", this.showDataAndRender);
+      Events.bind('update:poll', this.setPolling, this);
+      this.setPolling();
+    }
+  });
 
-	Views.TableDetail = FauxtonAPI.View.extend({
-		tagName: 'tr',
-		template: "addons/activetasks/templates/tabledetail",
-		initialize: function(){
-			this.type = this.model.get('type');
-		},
-		getObject: function(){
-			var objectField = this.model.get('database');
-			if (this.type === "replication"){
-				objectField = this.model.get('source') + " to " + this.model.get('target');
-			}
-			return objectField;
-		},
-		getProgress:  function(){
-			var progress = "";
-			if (this.type === "indexer"){
-				progress = "Processed " +this.model.get('changes_done')+ " of "+this.model.get('total_changes')+ ' changes';
-			}
-			return progress;
-		},
-		serialize: function(){
-			return {
-				model: this.model,
-				objectField: this.getObject(),
-				progress: this.getProgress()
-			};
-		}
-	});
+  Views.TableData = FauxtonAPI.View.extend({
+    tagName: "table",
+    className: "table table-bordered table-striped active-tasks",
+    template: "addons/activetasks/templates/table",
+    events: {
+      "click th": "sortByType"
+    },
+    initialize: function(){
+      currentView = this.options.currentView;
+    },
+    sortByType:  function(e){
+      var currentTarget = e.currentTarget;
+      datatype = $(currentTarget).attr("data-type");
+      this.collection.sortByColumn(datatype);
+      this.render();
+    },
+    serialize: function(){
+      return {
+        currentView: currentView,
+        collection: this.collection
+      };
+    },
+    beforeRender: function(){
+      //iterate over the collection to add each
+      this.collection.forEach(function(item) {
+        this.insertView("#tasks_go_here", new Views.TableDetail({ 
+          model: item
+        }));
+      }, this);
+    }
+  });
+
+  Views.TableDetail = FauxtonAPI.View.extend({
+    tagName: 'tr',
+    template: "addons/activetasks/templates/tabledetail",
+    initialize: function(){
+      this.type = this.model.get('type');
+    },
+    getObject: function(){
+      var objectField = this.model.get('database');
+      if (this.type === "replication"){
+        objectField = this.model.get('source') + " to " + this.model.get('target');
+      }
+      return objectField;
+    },
+    getProgress:  function(){
+      var progress = "";
+      if (this.type === "indexer"){
+        progress = "Processed " +this.model.get('changes_done')+ " of "+this.model.get('total_changes')+ ' changes';
+      }
+      return progress;
+    },
+    serialize: function(){
+      return {
+        model: this.model,
+        objectField: this.getObject(),
+        progress: this.getProgress()
+      };
+    }
+  });
 
 
- 
+
   return Views;
 });
