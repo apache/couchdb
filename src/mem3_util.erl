19c3ee2cd9ea4941505db7f54fbf257e740586ae
Adam Kocoloski <adam@cloudant.com>
2013-10-22 15:29:00 -0400

    Ensure that the owner of a doc is also a host
    BugzID: 24395

--- src/mem3_util.erl
+++ src/mem3_util.erl
@@ -224,7 +224,7 @@ ensure_exists(DbName) ->
 
 
 owner(DbName, DocId) ->
-    Nodes = lists:sort([node()|nodes()]),
+    Nodes = lists:sort([N || #shard{node=N} <- mem3:shards(DbName, DocId)]),
     node() =:= hd(rotate_list({DbName, DocId}, Nodes)).
 
 is_deleted(Change) ->
