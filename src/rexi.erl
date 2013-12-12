f9380551e3bedf4e32842eaca77435206424b1be
Paul J. Davis <paul.joseph.davis@gmail.com>
2013-12-12 11:36:05 -0600

    Revert "Tag all replies to the coordinator"
    Not all rexi communication happens through `rexi_utils:recv/6`. We'll
    need to audit all of our apps to see anywhere that doesn't to update to
    the new message format.
    
    This reverts commit 2f967906730eedf5851752be06155fc49edd28a1.

--- src/rexi.erl
+++ src/rexi.erl
@@ -98,7 +98,7 @@ async_server_call(Server, Caller, Request) ->
 -spec reply(any()) -> any().
 reply(Reply) ->
     {Caller, Ref} = get(rexi_from),
-    erlang:send(Caller, {rexi, Ref, Reply}).
+    erlang:send(Caller, {Ref,Reply}).
 
 %% @equiv sync_reply(Reply, 300000)
 sync_reply(Reply) ->
@@ -111,7 +111,7 @@ sync_reply(Reply) ->
 sync_reply(Reply, Timeout) ->
     {Caller, Ref} = get(rexi_from),
     Tag = make_ref(),
-    erlang:send(Caller, {rexi, Ref, {self(),Tag}, Reply}),
+    erlang:send(Caller, {Ref, {self(),Tag}, Reply}),
     receive {Tag, Response} ->
         Response
     after Timeout ->
@@ -179,7 +179,7 @@ stream(Msg, Limit, Timeout) ->
         {ok, Count} ->
             put(rexi_unacked, Count+1),
             {Caller, Ref} = get(rexi_from),
-            erlang:send(Caller, {rexi, Ref, self(), Msg}),
+            erlang:send(Caller, {Ref, self(), Msg}),
             ok
     catch throw:timeout ->
         exit(timeout)
