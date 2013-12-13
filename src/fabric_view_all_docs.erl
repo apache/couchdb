a01d2395fd6970c7802b668898aa7eb1c3ea73a4
Paul J. Davis <paul.joseph.davis@gmail.com>
2013-12-13 13:56:44 -0600

    Fix logging of errors in _all_docs open_doc calls
    Our exception handling accidentally captured the successful return
    value. Using try/of/catch we can exit from the of clause without
    catching.
    
    BugzId: 26162

--- src/fabric_view_all_docs.erl
+++ src/fabric_view_all_docs.erl
@@ -206,8 +206,9 @@ doc_receive_loop(Keys, Pids, SpawnFun, MaxJobs, Callback, AccIn) ->
 
 
 open_doc(DbName, Options, Id, IncludeDocs) ->
-    try
-        open_doc_int(DbName, Options, Id, IncludeDocs)
+    try open_doc_int(DbName, Options, Id, IncludeDocs) of
+        #view_row{} = Row ->
+            exit(Row)
     catch Type:Reason ->
         Stack = erlang:get_stacktrace(),
         couch_log:error("_all_docs open error: ~s ~s :: ~w ~w", [
@@ -231,7 +232,7 @@ open_doc_int(DbName, Options, Id, IncludeDocs) ->
         Value = {[{rev,couch_doc:rev_to_str({RevPos, RevId})}]},
         #view_row{key=Id, id=Id, value=Value}
     end,
-    exit(if IncludeDocs -> Row#view_row{doc=Doc}; true -> Row end).
+    if IncludeDocs -> Row#view_row{doc=Doc}; true -> Row end.
 
 cancel_read_pids(Pids) ->
     case queue:out(Pids) of
