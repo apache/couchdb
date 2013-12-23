5d7c38ee6e9ba56f169d271eae085a57a91b6479
Russell Branca <chewbranca@gmail.com>
2013-12-23 14:34:07 -0800

    Add dev cluster scripts and HAProxy config

---
+++ rel/boot_dev_cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Make log directory
+mkdir -p ./rel/logs/
+
+# Start each node
+./rel/dev1/bin/couchdb > ./rel/logs/couchdb1.log 2>&1 &
+DB1_PID=$!
+
+./rel/dev2/bin/couchdb > ./rel/logs/couchdb2.log 2>&1 &
+DB2_PID=$!
+
+./rel/dev3/bin/couchdb > ./rel/logs/couchdb3.log 2>&1 &
+DB3_PID=$!
+
+/usr/local/sbin/haproxy -f rel/haproxy.cfg > ./rel/logs/haproxy.log 2>&1 &
+HP_PID=$!
+
+sleep 2
+
+# Connect the cluster
+curl localhost:15986/nodes/dev2@127.0.0.1 -X PUT -d '{}'
+curl localhost:15986/nodes/dev3@127.0.0.1 -X PUT -d '{}'
+
+trap "kill $DB1_PID $DB2_PID $DB3_PID $HP_PID" SIGINT SIGTERM SIGHUP
+
+wait
