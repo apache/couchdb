6efb035b7f181e3024b3cd87a1875c78f0bf2274
Benjamin Anderson <b@banjiewen.net>
2014-02-12 09:16:09 -0800

    Log unknown messages instead of crashing

--- src/couch_stats_process_tracker.erl
+++ src/couch_stats_process_tracker.erl
@@ -35,21 +35,24 @@ init([]) ->
     {ok, #st{tracked = dict:new()}}.
 
 handle_call(Msg, _From, State) ->
-    {stop, {unknown_call, Msg}, error, State}.
+    twig:log(notice, "~p received unknown call ~p", [?MODULE, Msg]),
+    {noreply, State}.
 
 handle_cast({track, Pid, Name}, #st{tracked=Tracked}=State) ->
     couch_stats:increment_counter(Name),
     Ref = erlang:monitor(process, Pid),
     {noreply, State#st{tracked=dict:store(Ref, Name, Tracked)}};
 handle_cast(Msg, State) ->
-    {stop, {unknown_cast, Msg}, State}.
+    twig:log(notice, "~p received unknown cast ~p", [?MODULE, Msg]),
+    {noreply, State}.
 
 handle_info({'DOWN', Ref, _, _, _}, #st{tracked=Tracked}=State) ->
     Name = dict:fetch(Ref, Tracked),
     couch_stats:decrement_counter(Name),
     {noreply, State#st{tracked=dict:erase(Ref, Tracked)}};
 handle_info(Msg, State) ->
-    {stop, {unknown_info, Msg}, State}.
+    twig:log(notice, "~p received unknown message ~p", [?MODULE, Msg]),
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
     ok.
