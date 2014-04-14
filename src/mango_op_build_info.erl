cc28ace2c742f2c7e1538979fe64fd79b845df8f
tonysun83 <root@debian.tonylinux>
2014-04-14 14:07:18 -0700

    Mango Build Info Operation
    Add support to return build info specified by Mongodb.
    Returns DBcore version as build info. Need to properly figure
    out how to convert version number such as 2.8.10 to a format that
    gets coverted to an actual array on the client end. Passing in
    [2,8,10] causes conversion error. Some values are just placeholders.
    
    BugzId: 30109

--- src/mango_op_build_info.erl
+++ src/mango_op_build_info.erl
@@ -9,4 +9,37 @@
 
 
 run(_Props, _Ctx) ->
-    ok.
+    Version = get_version(),
+    {_, SysInfo} = os:type(),
+    VerArray = <<"[2,8,10]">>,
+    Reply = {[
+        {<<"version">>, Version},
+        {<<"gitVersion">>, <<"N/A">>},
+        {<<"sysInfo">>, atom_to_binary(SysInfo, latin1)},
+        {<<"loaderFlags">>, <<"loaderFlags">>},
+        {<<"compilerFlags">>, <<"compilerFlags">>},
+        {<<"allocator">>, <<"allocator">>},
+        {<<"versionArray">>, VerArray},
+        {<<"javascriptingEngine">>, <<"V8">>},
+        {<<"bits">>, 1},
+        {<<"debug">>, false},
+        {<<"maxBsonObjectSize">>, 16777216},
+        {<<"ok">>, 1}
+    ]},
+    {ok, Reply, _Ctx}.
+
+get_version() ->
+    Releases = release_handler:which_releases(),
+    Version = case [V || {"dbcore", V, _, current} <- Releases] of
+    [] ->
+        case [V || {"dbcore", V, _, permanent} <- Releases] of
+        [] ->
+            "dev";
+        [Permanent] ->
+            Permanent
+        end;
+    [Current] ->
+        Current
+    end,
+    list_to_binary(Version).
+
