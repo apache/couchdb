1a53458c879fe78f84406ced0051862b709383d7
Paul J. Davis <paul.joseph.davis@gmail.com>
2014-04-23 17:24:37 -0500

    WIP on the HTTP API

--- src/mango_httpd.erl
+++ src/mango_httpd.erl
@@ -5,10 +5,32 @@
 ]).
 
 
-handle_query_req(#httpd{method='POST'}=Req, #db{name=DbName}) ->
-    send_json(Req, 200, {[{ok, true}]});
+-include_lib("couch/include/couch_db.hrl").
+
+
+handle_query_req(#httpd{method='POST'}=Req, #db{name=_DbName}) ->
+    couch_httpd:validate_ctype(Req, "application/json"),
+    
+    chttpd:send_json(Req, 200, {[{ok, true}]});
 handle_query_req(Req, _Db) ->
-    chttpd:send_method_not_allowed(Req, 'POST').
+    chttpd:send_method_not_allowed(Req, "POST").
+
+
+get_body(Req) ->
+    case chttpd:json_body(Req) of
+        {_} = Body0 ->
+            [Body0];
+        Body0 when is_list(Body0) ->
+            IsObj = fun({_}) -> true; (_) -> false end,
+            case lists:all(IsObj, Body0) of
+                true ->
+                    Body0;
+                false ->
+                    throw({bad_request, invalid_body})
+            end;
+        _ ->
+            throw({bad_request, invalid_body})
+    end.
 
 
 
