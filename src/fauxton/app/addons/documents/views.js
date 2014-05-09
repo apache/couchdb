apache/couchdb

commit ebade0f2f82d4da60e386f4aeaf24ecb98181e81

Fauxton: use .listenTo and not .on
There was no `.off` call on destroy, so these two Eventhandlers
where leaking memory

src/fauxton/app/addons/documents/views.js
@@ -928,7 +928,9 @@ function(app, FauxtonAPI, Components, Documents, Databases, pouchdb,
     },
 
     afterRender: function() {
-      var saveDoc = this.saveDoc;
+      var saveDoc = this.saveDoc,
+          editor,
+          model;
 
       this.editor = new Components.Editor({
         editorId: "editor-container",
@@ -943,12 +945,9 @@ function(app, FauxtonAPI, Components, Documents, Databases, pouchdb,
         }]
       });
       this.editor.render();
-      this.model.on("sync", this.updateValues, this);
 
-      var editor = this.editor,
-          model = this.model;
-
-      editor.editor.on("change", function (event) {
+      this.listenTo(this.model, "sync", this.updateValues);
+      this.listenTo(editor.editor, "change", function (event) {
         var changedDoc;
         try {
           changedDoc = JSON.parse(editor.getValue());
@@ -974,6 +973,9 @@ function(app, FauxtonAPI, Components, Documents, Databases, pouchdb,
           clear:  true
         });
       });
+
+      editor = this.editor;
+      model = this.model;
     },
 
     cleanup: function () {

commit 9bdbea82a940a5f5fa2f51aea865166d7fddf23c

Fauxton: fix regression for document editor
Introduced with ebade0f2f82d4da60e386f4aeaf24ecb98181e81

src/fauxton/app/addons/documents/views.js
@@ -946,6 +946,9 @@ function(app, FauxtonAPI, Components, Documents, Databases, pouchdb,
       });
       this.editor.render();
 
+      editor = this.editor;
+      model = this.model;
+
       this.listenTo(this.model, "sync", this.updateValues);
       this.listenTo(editor.editor, "change", function (event) {
         var changedDoc;
@@ -973,9 +976,6 @@ function(app, FauxtonAPI, Components, Documents, Databases, pouchdb,
           clear:  true
         });
       });
-
-      editor = this.editor;
-      model = this.model;
     },
 
     cleanup: function () {