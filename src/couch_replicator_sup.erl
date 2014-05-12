b38025c2db4322f5715c0ad54d1754c847cf9e9e
Robert Newson <rnewson@apache.org>
2014-05-12 13:09:33 +0100

    remove Cloudant refs

--- src/couch_replicator_sup.erl
+++ src/couch_replicator_sup.erl
@@ -1,4 +1,3 @@
-% Copyright 2010 Cloudant
 %
 % Licensed under the Apache License, Version 2.0 (the "License"); you may not
 % use this file except in compliance with the License. You may obtain a copy of
