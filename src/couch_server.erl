f1c4c23a61868e80d8567e4a489e4ee4ae16b806
Robert Newson <rnewson@apache.org>
2014-05-22 16:39:40 +0100

    Apply security hooks to all {_users,_replicator} databases

--- src/couch_server.erl
+++ src/couch_server.erl
@@ -115,34 +115,31 @@ delete(DbName, Options) ->
 maybe_add_sys_db_callbacks(DbName, Options) when is_binary(DbName) ->
     maybe_add_sys_db_callbacks(?b2l(DbName), Options);
 maybe_add_sys_db_callbacks(DbName, Options) ->
-    ReplicatorDbName = config:get("replicator", "db", "_replicator"),
-    ReplicatorDbOptions = [
-        {before_doc_update, fun couch_replicator_manager:before_doc_update/2},
-        {after_doc_read, fun couch_replicator_manager:after_doc_read/2},
-        sys_db
-    ] ++ Options,
-    UsersDbName = config:get("couch_httpd_auth", "authentication_db", "_users"),
-    UsersDbOptions = [
-        {before_doc_update, fun couch_users_db:before_doc_update/2},
-        {after_doc_read, fun couch_users_db:after_doc_read/2},
-        sys_db
-    ] ++ Options,
     DbsDbName = config:get("mem3", "shard_db", "dbs"),
-    DbsDbOptions = [sys_db | Options],
-    NodesDbName = config:get("mem3", "node_db", "nodes"),
-    NodesDbOptions = [sys_db | Options],
-    KnownSysDbs = [
-        {ReplicatorDbName, ReplicatorDbOptions},
-        {UsersDbName, UsersDbOptions},
-        {DbsDbName, DbsDbOptions},
-        {NodesDbName, NodesDbOptions}
-    ],
-    case lists:keyfind(DbName, 1, KnownSysDbs) of
-        {DbName, SysOptions} ->
-            SysOptions;
-        false ->
-            Options
+    NodesDbName = config:get("mem3", "shard_db", "nodes"),
+    IsReplicatorDb = DbName == config:get("replicator", "db", "_replicator") orelse
+	path_ends_with(DbName, <<"_replicator">>),
+    IsUsersDb = DbName ==config:get("couch_httpd_auth", "authentication_db", "_users") orelse
+	path_ends_with(DbName, <<"_users">>),
+    if
+	DbName == DbsDbName ->
+	    [sys_db | Options];
+	DbName == NodesDbName ->
+	    [sys_db | Options];
+	IsReplicatorDb ->
+	    [{before_doc_update, fun couch_replicator_manager:before_doc_update/2},
+	     {after_doc_read, fun couch_replicator_manager:after_doc_read/2},
+	     sys_db | Options];
+	IsUsersDb ->
+	    [{before_doc_update, fun couch_users_db:before_doc_update/2},
+	     {after_doc_read, fun couch_users_db:after_doc_read/2},
+	     sys_db | Options];
+	true ->
+	    Options
     end.
+
+path_ends_with(Path, Suffix) ->
+    Suffix == lists:last(binary:split(mem3:dbname(Path), <<"/">>, [global])).
 
 check_dbname(#server{dbname_regexp=RegExp}, DbName) ->
     case re:run(DbName, RegExp, [{capture, none}]) of
