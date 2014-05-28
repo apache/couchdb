31c7ce756645041c67221a267cc901ea6eea15ce
Garren Smith <garren.smith@gmail.com>
2014-05-28 12:46:32 +0200

    Fauxton: Query Parse url

--- src/fauxton/app/addons/documents/resources.js
+++ src/fauxton/app/addons/documents/resources.js
@@ -414,6 +414,7 @@ function(app, FauxtonAPI, PagingCollection) {
 
     urlRef: function(context, params) {
       var query = "";
+      
       if (params) {
         if (!_.isEmpty(params)) {
           query = "?" + $.param(params);
@@ -421,7 +422,8 @@ function(app, FauxtonAPI, PagingCollection) {
           query = '';
         }
       } else if (this.params) {
-        query = "?" + $.param(this.params);
+        var parsedParam = Documents.QueryParams.stringify(this.params);
+        query = "?" + $.param(parsedParam);
       }
 
       var startOfUrl = app.host;
