b59a4b28a43e68371165ab7cda7268c622750d89
ILYA Khlopotov <iilyak@ca.ibm.com>
2015-02-04 09:18:50 -0800

    Enable couchdb_http_proxy_tests
    COUCHDB-2547

--- test/couchdb_http_proxy_tests.erl
+++ test/couchdb_http_proxy_tests.erl
@@ -27,8 +27,6 @@
 -define(TIMEOUT, 5000).
 
 
--ifdef(run_broken_tests).
-
 start() ->
     % we have to write any config changes to temp ini file to not loose them
     % when supervisor will kill all children due to reaching restart threshold
@@ -454,6 +452,3 @@ recv_body(ReqId, Acc) ->
     after ?TIMEOUT ->
         throw({error, timeout})
     end.
-
--endif.
-
