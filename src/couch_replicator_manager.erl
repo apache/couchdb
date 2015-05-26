80708a99e2bc82868c26de89b7a4d86c0c4167af
Robert Newson <rnewson@apache.org>
2015-05-26 16:04:24 +0100

    Infinity timeout, just like all the others :(
    This is a cherry-pick of:
    
    https://github.com/cloudant/couch_replicator/commit/e947b392db1eb2de22aac4a4fa12da118fe114b3

--- src/couch_replicator_manager.erl
+++ src/couch_replicator_manager.erl
@@ -134,7 +134,7 @@ replication_error(#rep{id = {BaseId, _} = RepId}, Error) ->
 continue(#rep{doc_id = null}) ->
     {true, no_owner};
 continue(#rep{id = RepId}) ->
-    Owner = gen_server:call(?MODULE, {owner, RepId}),
+    Owner = gen_server:call(?MODULE, {owner, RepId}, infinity),
     {node() == Owner, Owner}.
 
 
