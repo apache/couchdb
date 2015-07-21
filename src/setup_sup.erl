5c0e927c11df8f6b45b9a60f0c8eddaccbf3debe
Robert Newson <rnewson@apache.org>
2015-07-21 14:15:23 +0100

    Use dynamic handlers

--- src/setup_sup.erl
+++ src/setup_sup.erl
@@ -35,5 +35,6 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+    {ok, { {one_for_one, 5, 10}, [
+        chttpd_handlers:provider(setup, setup_httpd_handlers)
+    ]} }.
