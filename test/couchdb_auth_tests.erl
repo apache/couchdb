b387a0b70596e5610ef6ab13ef92c71afb84f4f4
ILYA Khlopotov <iilyak@ca.ibm.com>
2015-08-05 13:45:35 -0700

    Tests for 'authentication_handlers' and 'authenticated' on '_session'

--- test/couchdb_auth_tests.erl
+++ test/couchdb_auth_tests.erl
@@ -15,32 +15,40 @@
 -include_lib("couch/include/couch_eunit.hrl").
 
 
-setup() ->
+setup(PortType) ->
+    ok = config:set("admins", "rocko", "artischocko", false),
     Addr = config:get("httpd", "bind_address", "127.0.0.1"),
-    Port = integer_to_list(mochiweb_socket_server:get(couch_httpd, port)),
-    lists:concat(["http://", Addr, ":", Port, "/_session"]).
+    lists:concat(["http://", Addr, ":", port(PortType), "/_session"]).
 
-teardown(_) ->
+teardown(_, _) ->
     ok.
 
 
 auth_test_() ->
+    Tests = [
+        fun should_not_return_username_on_post_to_session/2,
+        fun should_return_authenticated_field/2,
+        fun should_return_list_of_handlers/2
+    ],
     {
         "Auth tests",
         {
             setup,
-            fun test_util:start_couch/0, fun test_util:stop_couch/1,
-            {
-                foreach,
-                fun setup/0, fun teardown/1,
-                [
-                    fun should_not_return_username_on_post_to_session/1
-                ]
-            }
+            fun() -> test_util:start_couch([chttpd]) end, fun test_util:stop_couch/1,
+            [
+                make_test_cases(clustered, Tests),
+                make_test_cases(backdoor, Tests)
+            ]
         }
     }.
 
-should_not_return_username_on_post_to_session(Url) ->
+make_test_cases(Mod, Funs) ->
+    {
+        lists:flatten(io_lib:format("~s", [Mod])),
+        {foreachx, fun setup/1, fun teardown/2, [{Mod, Fun} || Fun <- Funs]}
+    }.
+
+should_not_return_username_on_post_to_session(_PortType, Url) ->
     ?_assertEqual(<<"rocko">>,
         begin
             ok = config:set("admins", "rocko", "artischocko", false),
@@ -49,3 +57,38 @@ should_not_return_username_on_post_to_session(Url) ->
             {Json} = jiffy:decode(Body),
             proplists:get_value(<<"name">>, Json)
         end).
+
+should_return_authenticated_field(_PortType, Url) ->
+    ?_assertEqual(<<"local">>,
+        begin
+            couch_util:get_nested_json_value(session(Url), [
+                <<"info">>, <<"authenticated">>])
+        end).
+
+should_return_list_of_handlers(backdoor, Url) ->
+    ?_assertEqual([<<"oauth">>,<<"cookie">>,<<"default">>, <<"local">>],
+        begin
+            couch_util:get_nested_json_value(session(Url), [
+                <<"info">>, <<"authentication_handlers">>])
+        end);
+should_return_list_of_handlers(clustered, Url) ->
+    ?_assertEqual([<<"cookie">>,<<"default">>,<<"local">>],
+        begin
+            couch_util:get_nested_json_value(session(Url), [
+                <<"info">>, <<"authentication_handlers">>])
+        end).
+
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+session(Url) ->
+    {ok, _, _, Body} = test_request:get(Url, [{"Content-Type", "application/json"}],
+        "{\"name\":\"rocko\", \"password\":\"artischocko\"}"),
+    jiffy:decode(Body).
+
+port(clustered) ->
+    integer_to_list(mochiweb_socket_server:get(chttpd, port));
+port(backdoor) ->
+    integer_to_list(mochiweb_socket_server:get(couch_httpd, port)).
