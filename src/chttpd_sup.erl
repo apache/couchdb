96c46e93ee2e668727643b033999eb1538c09397
ILYA Khlopotov <iilyak@ca.ibm.com>
2015-09-29 13:04:19 -0700

    Pass supervisor's children to couch_epi

--- src/chttpd_sup.erl
+++ src/chttpd_sup.erl
@@ -18,21 +18,23 @@
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 100, Type, [I]}).
--define(NAMED_CHILD(I, M, Type), {I, {M, start_link, []}, permanent, 100, Type, [M]}).
 
 start_link(Args) ->
     supervisor:start_link({local,?MODULE}, ?MODULE, Args).
 
 init([]) ->
     chttpd_config_listener:subscribe(),
-    ServiceProcesses = couch_epi:register_service(chttpd_epi),
 
-    {ok, {{one_for_one, 3, 10}, add_auth_cache_if_missing(ServiceProcesses) ++ [
+    Children = [
         ?CHILD(chttpd, worker),
+        ?CHILD(chttpd_auth_cache, worker),
         {chttpd_auth_cache_lru,
 	 {ets_lru, start_link, [chttpd_auth_cache_lru, lru_opts()]},
 	 permanent, 5000, worker, [ets_lru]}
-    ]}}.
+    ],
+
+    {ok, {{one_for_one, 3, 10},
+        couch_epi:register_service(chttpd_epi, Children)}}.
 
 lru_opts() ->
     case config:get("chttpd_auth_cache", "max_objects") of
@@ -53,9 +55,3 @@ lru_opts() ->
         _ ->
             []
     end.
-
-add_auth_cache_if_missing(Children) ->
-    case lists:keymember(auth_cache, 1, Children) of
-        true -> Children;
-        false -> [?NAMED_CHILD(auth_cache, chttpd_auth_cache, worker)]
-     end.
