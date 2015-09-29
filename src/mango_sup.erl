46b58a3c53bc07db8b841fd55dc9fb32c1b6bb89
ILYA Khlopotov <iilyak@ca.ibm.com>
2015-09-29 13:05:25 -0700

    Pass supervisor's children to couch_epi

--- src/mango_sup.erl
+++ src/mango_sup.erl
@@ -21,4 +21,4 @@ start_link(Args) ->
     supervisor:start_link({local,?MODULE}, ?MODULE, Args).
 
 init([]) ->
-    {ok, {{one_for_one, 3, 10}, couch_epi:register_service(mango_epi)}}.
+    {ok, {{one_for_one, 3, 10}, couch_epi:register_service(mango_epi, [])}}.
