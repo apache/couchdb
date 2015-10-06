f089832ea7f759b4e655051025cc48522fee9e1b
ILYA Khlopotov <iilyak@ca.ibm.com>
2015-10-06 08:08:17 -0700

    Use couch_server:delete_file on view cleanup

--- src/couch_mrview_cleanup.erl
+++ src/couch_mrview_cleanup.erl
@@ -41,7 +41,71 @@ run(Db) ->
 
     lists:foreach(fun(FN) ->
         couch_log:debug("Deleting stale view file: ~s", [FN]),
-        couch_file:delete(RootDir, FN, false)
+        delete_view_file(RootDir, FN)
     end, ToDelete),
 
     ok.
+
+delete_view_file(RootDir, FullFilePath) ->
+    couch_server:delete_file(RootDir, FullFilePath, [sync]).
+
+-ifdef(TEST).
+-include_lib("couch/include/couch_eunit.hrl").
+
+setup(rename) ->
+    setup(true);
+setup(delete) ->
+    setup(false);
+setup(RenameOnDelete) ->
+    meck:new(config, [passthrough]),
+    meck:expect(config, get_boolean, fun
+        ("couchdb", "rename_on_delete", _Default) -> RenameOnDelete
+    end),
+    ViewFile = ?tempfile() ++ ".view",
+    RootDir = filename:dirname(ViewFile),
+    ok = couch_file:init_delete_dir(RootDir),
+    ok = file:write_file(ViewFile, <<>>),
+    {RootDir, ViewFile}.
+
+teardown(_, {_, ViewFile}) ->
+    (catch meck:unload(config)),
+    (catch file:delete(ViewFile)).
+
+delete_view_file_test_() ->
+    Funs = [
+        fun should_rename_on_delete/2,
+        fun should_delete/2
+    ],
+    [
+        make_test_case(rename, [fun should_rename_on_delete/2]),
+        make_test_case(delete, [fun should_delete/2])
+    ].
+
+make_test_case(Mod, Funs) ->
+    {
+        lists:flatten(io_lib:format("~s", [Mod])),
+        {foreachx, fun setup/1, fun teardown/2, [{Mod, Fun} || Fun <- Funs]}
+    }.
+
+should_rename_on_delete(_, {RootDir, ViewFile}) ->
+    ?_test(begin
+        ?assert(filelib:is_regular(ViewFile)),
+        Result = delete_view_file(RootDir, ViewFile),
+        ?assertNot(filelib:is_regular(ViewFile)),
+        ?assertMatch({ok, {renamed, _}}, Result),
+        {ok, {renamed, RenamedViewFile}} = Result,
+        ?assert(filelib:is_regular(RenamedViewFile))
+    end).
+
+should_delete(_, {RootDir, ViewFile}) ->
+    ?_test(begin
+        ?assert(filelib:is_regular(ViewFile)),
+        delete_view_file(RootDir, ViewFile),
+        ?assertNot(filelib:is_regular(ViewFile)),
+        ?assertMatch([], deleted_files(ViewFile))
+    end).
+
+deleted_files(ViewFile) ->
+    filelib:wildcard(filename:rootname(ViewFile) ++ "*.deleted.*").
+
+-endif.
