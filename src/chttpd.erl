apache/couchdb

commit 895715a6adf317a7b2f25dc54b0ce7eb1191c8ff

Escape database name on POST /_replicate
COUCHDB-2666

src/chttpd.erl
@@ -381,7 +381,7 @@ fix_uri(Req, Props, Type) ->
         true ->
             Props;
         false ->
-            Uri = make_uri(Req,replication_uri(Type, Props)),
+            Uri = make_uri(Req, Uri0),
             [{Type,Uri}|proplists:delete(Type,Props)]
         end
     end.
@@ -391,7 +391,7 @@ replication_uri(Type, PostProps) ->
     {Props} ->
         couch_util:get_value(<<"url">>, Props);
     Else ->
-        Else
+        quote(Else)
     end.
 
 is_http(<<"http://", _/binary>>) ->

test/chttpd_handlers_tests.erl
@@ -1,0 +1,87 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-module(chttpd_handlers_tests).
+
+-include_lib("couch/include/couch_eunit.hrl").
+-include_lib("couch/include/couch_db.hrl").
+
+
+setup() ->
+    Addr = config:get("chttpd", "bind_address", "127.0.0.1"),
+    Port = mochiweb_socket_server:get(chttpd, port),
+    BaseUrl = lists:concat(["http://", Addr, ":", Port]),
+    BaseUrl.
+
+teardown(_Url) ->
+    ok.
+
+
+replicate_test_() ->
+    {
+        "_replicate",
+        {
+            setup,
+            fun chttpd_test_util:start_couch/0,
+            fun chttpd_test_util:stop_couch/1,
+            {
+                foreach,
+                fun setup/0, fun teardown/1,
+                [
+                    fun should_escape_dbname_on_replicate/1
+                ]
+            }
+        }
+    }.
+
+
+should_escape_dbname_on_replicate(Url) ->
+    ?_test(
+        begin
+            Request = couch_util:json_encode({[
+                {<<"source">>, <<"foo/bar">>},
+                {<<"target">>, <<"bar/baz">>},
+                {<<"create_target">>, true}
+            ]}),
+            {ok, 200, _, Body} = request_replicate(Url ++ "/_replicate", Request),
+            JSON = couch_util:json_decode(Body),
+
+            Source = json_value(JSON, [<<"source">>, <<"url">>]),
+            Target = json_value(JSON, [<<"target">>, <<"url">>]),
+            UrlBin = ?l2b(Url),
+            ?assertEqual(<<UrlBin/binary, "/foo%2Fbar">>, Source),
+            ?assertEqual(<<UrlBin/binary, "/bar%2Fbaz">>, Target)
+        end).
+
+
+json_value(JSON, Keys) ->
+    couch_util:get_nested_json_value(JSON, Keys).
+
+request_replicate(Url, Body) ->
+    Headers = [{"Content-Type", "application/json"}],
+    Handler = {chttpd_misc, handle_replicate_req},
+    request(post, Url, Headers, Body, Handler, fun(Req) ->
+        chttpd:send_json(Req, 200, get(post_body))
+    end).
+
+request(Method, Url, Headers, Body, {M, F}, MockFun) ->
+    meck:new(M, [passthrough, non_strict]),
+    try
+        meck:expect(M, F, MockFun),
+        Result = test_request:Method(Url, Headers, Body),
+        ?assert(meck:validate(M)),
+        Result
+    catch Kind:Reason ->
+        {Kind, Reason}
+    after
+        meck:unload(M)
+    end.

commit 038a933a09ad90c814e39b0327463aca1b194982

Fix replication when source/target are URLs
Commit 895715a6 introduced a bug when we quote all the values
while we only need to escape values that references to local
databases.

src/chttpd.erl
@@ -381,7 +381,7 @@ fix_uri(Req, Props, Type) ->
         true ->
             Props;
         false ->
-            Uri = make_uri(Req, Uri0),
+            Uri = make_uri(Req, quote(Uri0)),
             [{Type,Uri}|proplists:delete(Type,Props)]
         end
     end.
@@ -391,7 +391,7 @@ replication_uri(Type, PostProps) ->
     {Props} ->
         couch_util:get_value(<<"url">>, Props);
     Else ->
-        quote(Else)
+        Else
     end.
 
 is_http(<<"http://", _/binary>>) ->

test/chttpd_handlers_tests.erl
@@ -47,17 +47,17 @@ replicate_test_() ->
 should_escape_dbname_on_replicate(Url) ->
     ?_test(
         begin
+            UrlBin = ?l2b(Url),
             Request = couch_util:json_encode({[
-                {<<"source">>, <<"foo/bar">>},
+                {<<"source">>, <<UrlBin/binary, "/foo%2Fbar">>},
                 {<<"target">>, <<"bar/baz">>},
                 {<<"create_target">>, true}
             ]}),
             {ok, 200, _, Body} = request_replicate(Url ++ "/_replicate", Request),
             JSON = couch_util:json_decode(Body),
 
-            Source = json_value(JSON, [<<"source">>, <<"url">>]),
+            Source = json_value(JSON, [<<"source">>]),
             Target = json_value(JSON, [<<"target">>, <<"url">>]),
-            UrlBin = ?l2b(Url),
             ?assertEqual(<<UrlBin/binary, "/foo%2Fbar">>, Source),
             ?assertEqual(<<UrlBin/binary, "/bar%2Fbaz">>, Target)
         end).