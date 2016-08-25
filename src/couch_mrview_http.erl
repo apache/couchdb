82f7913d95a1e418725408cbbc1f822d20dfa77d
Robert Newson <rnewson@apache.org>
2016-08-25 12:47:49 +0100

    Wrap line
    whitespace-only change
    
    COUCHDB-3060

--- src/couch_mrview_http.erl
+++ src/couch_mrview_http.erl
@@ -382,7 +382,8 @@ view_cb(complete, #vacc{resp=Resp, buffer=Buf, threshold=Max}=Acc) ->
             {ok, Resp2} = chttpd:end_delayed_json_response(Resp1),
             {ok, Acc#vacc{resp=Resp2}};
         _ ->
-            {ok, Acc#vacc{resp=Resp1, prepend=",\r\n", buffer=[], bufsize=0}}
+            {ok, Acc#vacc{resp=Resp1,
+                prepend=",\r\n", buffer=[], bufsize=0}}
     end;
 view_cb({error, Reason}, #vacc{resp=undefined}=Acc) ->
     {ok, Resp} = chttpd:send_error(Acc#vacc.req, Reason),
