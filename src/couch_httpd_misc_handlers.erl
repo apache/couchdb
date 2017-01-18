8fd7d52093dceb87ac6535dae21a2b006d86018c
Eric Avdey <eiri@eiri.ca>
2017-01-18 09:55:19 -0400

    Return "Bad Request" when count in /_uuids exceeds max

--- src/couch_httpd_misc_handlers.erl
+++ src/couch_httpd_misc_handlers.erl
@@ -129,7 +129,7 @@ handle_uuids_req(#httpd{method='GET'}=Req) ->
     Max = list_to_integer(config:get("uuids","max_count","1000")),
     Count = try list_to_integer(couch_httpd:qs_value(Req, "count", "1")) of
         N when N > Max ->
-            throw({forbidden, <<"count parameter too large">>});
+            throw({bad_request, <<"count parameter too large">>});
         N when N < 0 ->
             throw({bad_request, <<"count must be a positive integer">>});
         N -> N
