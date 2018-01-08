92a9e7cb314258f91dd08cf42a178e3dce762b53
Will Holley <willholley@gmail.com>
2018-01-08 11:07:35 +0000

    fallback to "selector" on empty "partial_filter_selector"
    Mango text indexes historically supported partial indexes
    defined via a "selector" field. This was renamed to
    "partial_filter_selector" in b98de40 but the fallback
    code did not correctly handle the case where
    a "selector" existed alongside a "partial_filter_selector".
    
    This situation can occur when a the _index endpoint is
    used to create a text index with a "selector". The resulting
    design document contains an empty "partial_filter_selector"
    field *and* the "selector" field that was passed in. The
    previous implementation of the fallback would detect the
    presence of "partial_filter_selector" and use the empty
    value (match all docs) instead of faling back to the
    "selector" field.
    
    This commit changes the behaviour so that a "selector"
    will be used even if an empty "partial_filter_selector"
    is present. A secondary fix would be to change the index
    creation so that we never use "selector" in the underlying
    index design document, even if it is passed to the _index
    API.

--- src/mango/test/mango.py
+++ src/mango/test/mango.py
@@ -145,7 +145,7 @@ class Database(object):
         return created
 
     def create_text_index(self, analyzer=None, idx_type="text",
-        partial_filter_selector=None, default_field=None, fields=None, 
+        partial_filter_selector=None, selector=None, default_field=None, fields=None, 
         name=None, ddoc=None,index_array_lengths=None):
         body = {
             "index": {
@@ -161,6 +161,8 @@ class Database(object):
             body["index"]["default_field"] = default_field
         if index_array_lengths is not None:
             body["index"]["index_array_lengths"] = index_array_lengths
+        if selector is not None:
+            body["index"]["selector"] = selector
         if partial_filter_selector is not None:
             body["index"]["partial_filter_selector"] = partial_filter_selector
         if fields is not None:
