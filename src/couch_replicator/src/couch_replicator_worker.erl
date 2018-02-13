eedb540854d2425720e74d39503b7cf27f1a4b30
Nick Vatamaniuc <vatamane@apache.org>
2018-02-13 11:54:52 -0500

    Implement pluggable authentication and session support for replicator
    Previously replicator used only basic authentication. It was simple and
    straightforward. However, with PBKDF2 hashing becoming the default it would be
    nice not to do all the password verification work with every single request,
    and instead take advantage of session (cookie) based authentication.
    
    This commit implements session based authentication via a plugin mechanism.
    The list of available replicator auth modules is configurable. For example:
    
    ```
    [replicator]
    auth_plugins = couch_replicator_auth_session,couch_replicator_auth_basic
    ```
    
    The plugins will be tried in order. The first one to successfully initialize
    will end up being used for that endpoint (source or target).
    
    During the initialization callback, a plugin could decide it cannot be used in
    the current context. In that case it signals to be "ignored". The plugin
    framework will then skip over it and try to initialize the next on in the list.
    
    `couch_replicator_auth_basic` effectively implements the old behavior. This
    plugin should normally be used as a default catch-all at the end of the plugin
    list. In some cases, it might be useful to enforce exclusive use of
    session-based auth and fail replication jobs if it is not available.
    
    `couch_replicator_auth_session` does most of the work of handling session based
    authentication. On initialization, it strips away basic auth credentials from
    headers and url to avoid basic auth being used on the server. Then it is in
    charge of periodically issuing POST requests to `_session`, updating the
    headers of each request with the latest cookie value, and possibly picking up
    new session cookie if the server can issue them along with reglar responses.
    
    Currently session based auth plugin is not enabled by default and is an opt-in
    feature. That is, users would have to explicitly add the session module to the
    list of auth_plugins. In a future, session might be used by default.
    
    As discussed in #1153 this work also removes OAuth 1.0 support. After
    server-side support was removed, it had stopped working anyway since the main
    oauth app was removed. However, with the plugin framework in place it would be
    possible for someone to implement it as a separate module not entangled with
    the rest of the replicator code.
    
    Fixes #1153

--- src/couch_replicator/src/couch_replicator_worker.erl
+++ src/couch_replicator/src/couch_replicator_worker.erl
@@ -22,7 +22,7 @@
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 
 -include_lib("couch/include/couch_db.hrl").
--include("couch_replicator_api_wrap.hrl").
+-include_lib("couch_replicator/include/couch_replicator_api_wrap.hrl").
 -include("couch_replicator.hrl").
 
 % TODO: maybe make both buffer max sizes configurable
