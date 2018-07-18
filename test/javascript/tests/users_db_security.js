c94cc4cea335f7ef747fa97fd3b7df8a1ae902cf
Joan Touzet <joant@atypical.net>
2018-07-18 12:23:36 -0400

    Revert "Make loginUser wait for successful authentication"
    This reverts commit 6ffe0421ac524138a6b48fd010f2540631b83d16.

--- test/javascript/tests/users_db_security.js
+++ test/javascript/tests/users_db_security.js
@@ -32,13 +32,7 @@ couchTests.users_db_security = function(debug) {
     // the actual tests
     var username1 = username.replace(/[0-9]$/, "");
     var password = pws[username];
-    waitForSuccess(function() {
-      var req = CouchDB.login(username1, pws[username]);
-      if (req.ok) {
-        return true
-      }
-      throw({});
-    }, 'loginUser');
+    T(CouchDB.login(username1, pws[username]).ok);
   };
 
   var open_as = function(db, docId, username) {
@@ -113,6 +107,7 @@ couchTests.users_db_security = function(debug) {
 
     // jan's gonna be admin as he's the first user
     TEquals(true, usersDb.save(userDoc).ok, "should save document");
+    wait(5000);
     userDoc = open_as(usersDb, "org.couchdb.user:jchris", "jchris");
     TEquals(undefined, userDoc.password, "password field should be null 1");
     TEquals(scheme, userDoc.password_scheme, "password_scheme should be " + scheme);
