4267e1ff805e6f5719f770c780874b3d8ba78d9a
ermouth <ermouth@gmail.com>
2018-09-21 20:52:30 +0300

    js rewrite send body
    Fixes #1612

--- src/chttpd/src/chttpd_rewrite.erl
+++ src/chttpd/src/chttpd_rewrite.erl
@@ -64,7 +64,14 @@ do_rewrite(#httpd{mochi_req=MochiReq}=Req, {Props}=Rewrite) when is_list(Props) 
                                                Path,
                                                MochiReq:get(version),
                                                Headers),
-            NewMochiReq:cleanup(),
+            Body = case couch_util:get_value(<<"body">>, Props) of
+                undefined -> erlang:get(mochiweb_request_body);
+                B -> B
+            end,
+            case Body of
+                undefined -> NewMochiReq:cleanup();
+                _ -> erlang:put(mochiweb_request_body, Body)
+            end,
             couch_log:debug("rewrite to ~p", [Path]),
             chttpd:handle_request_int(NewMochiReq);
         Code ->
