838c5ef6f3e7555014e95c0cc46336567d62caa6
Jan Lehnardt <jan@apache.org>
2018-11-09 11:23:04 +0100

    feat: change enaabling of chttpsd to `[ssl] enable = true`
    The previous configuration `[daemons] httpsd = {chttpd, start_link, [https]}`
    is also sill supported for backwards cmpatibility reasons.

--- src/couch/src/couch_secondary_sup.erl
+++ src/couch/src/couch_secondary_sup.erl
@@ -36,6 +36,11 @@ init([]) ->
         {compaction_daemon, {couch_compaction_daemon, start_link, []}}
     ],
 
+    MaybeHttps = case https_enabled() of
+        true -> [{httpsd, {chttpd, start_link, [https]}}];
+        _False -> []
+    end,
+
     Children = SecondarySupervisors ++ [
         begin
             {Module, Fun, Args} = Spec,
@@ -48,6 +53,15 @@ init([]) ->
                 [Module]}
         end
         || {Name, Spec}
-        <- Daemons, Spec /= ""],
+        <- Daemons ++ MaybeHttps, Spec /= ""],
     {ok, {{one_for_one, 50, 3600},
         couch_epi:register_service(couch_db_epi, Children)}}.
+
+https_enabled() ->
+    % 1. [ssl] enable = true | false
+    % 2. if [daemons] httpsd == {chttpd, start_link, [https]} -> pretend true as well
+    SSLEnabled = config:get_boolean("ssl", "enable", false),
+    LegacySSL = config:get("daemons", "httpsd"),
+    LegacySSLEnabled = LegacySSL =:= "{chttpd, start_link, [https]}",
+
+    SSLEnabled orelse LegacySSLEnabled.
