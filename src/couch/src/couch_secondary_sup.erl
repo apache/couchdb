0d316b27cf014dd5287acd4cd666e976c1effc2a
Jan Lehnardt <jan@apache.org>
2018-11-09 10:42:36 +0100

    feat: remove redundant string parsing

--- src/couch/src/couch_secondary_sup.erl
+++ src/couch/src/couch_secondary_sup.erl
@@ -27,27 +27,27 @@ init([]) ->
             dynamic}
     ],
     Daemons = [
-        {"index_server", "{couch_index_server, start_link, []}"},
-        {"query_servers", "{couch_proc_manager, start_link, []}"},
-        {"vhosts", "{couch_httpd_vhost, start_link, []}"},
-        {"httpd", "{couch_httpd, start_link, []}"},
-        {"uuids", "{couch_uuids, start, []}"},
-        {"auth_cache", "{couch_auth_cache, start_link, []}"},
-        {"compaction_daemon", "{couch_compaction_daemon, start_link, []}"}
+        {index_server, {couch_index_server, start_link, []}},
+        {query_servers, {couch_proc_manager, start_link, []}},
+        {vhosts, {couch_httpd_vhost, start_link, []}},
+        {httpd, {couch_httpd, start_link, []}},
+        {uuids, {couch_uuids, start, []}},
+        {auth_cache, {couch_auth_cache, start_link, []}},
+        {compaction_daemon, {couch_compaction_daemon, start_link, []}}
     ],
 
     Children = SecondarySupervisors ++ [
         begin
-            {ok, {Module, Fun, Args}} = couch_util:parse_term(SpecStr),
+            {Module, Fun, Args} = Spec,
 
-            {list_to_atom(Name),
+            {Name,
                 {Module, Fun, Args},
                 permanent,
                 brutal_kill,
                 worker,
                 [Module]}
         end
-        || {Name, SpecStr}
-        <- Daemons, SpecStr /= ""],
+        || {Name, Spec}
+        <- Daemons, Spec /= ""],
     {ok, {{one_for_one, 50, 3600},
         couch_epi:register_service(couch_db_epi, Children)}}.
