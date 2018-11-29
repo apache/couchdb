26fd0819e4cc90173e3269bc2b875380ae247fda
Alessio Biancalana <dottorblaster@gmail.com>
2018-11-29 18:07:15 +0100

    test: enforce max line length to 90 columns

--- test/elixir/.credo.exs
+++ test/elixir/.credo.exs
@@ -79,7 +79,7 @@
         {Credo.Check.Readability.AliasOrder, []},
         {Credo.Check.Readability.FunctionNames, []},
         {Credo.Check.Readability.LargeNumbers, []},
-        {Credo.Check.Readability.MaxLineLength, [priority: :low, max_length: 120]},
+        {Credo.Check.Readability.MaxLineLength, [priority: :normal, max_length: 90]},
         {Credo.Check.Readability.ModuleAttributeNames, []},
         {Credo.Check.Readability.ModuleDoc, []},
         {Credo.Check.Readability.ModuleNames, []},
