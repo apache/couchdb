4be3de9a11714418b8b737f6b170b2cd6dc34d8c
Juanjo Rodriguez <jjrodrig@gmail.com>
2018-12-16 10:07:50 -0800

    Add windows support for with(out) quorum tests

--- test/elixir/run.cmd
+++ test/elixir/run.cmd
@@ -4,4 +4,4 @@ cd %~dp0
 call mix local.hex --force
 call mix local.rebar --force
 call mix deps.get
-call mix test --trace --exclude without_quorum_test --exclude with_quorum_test
+call mix test --trace %*
