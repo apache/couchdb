e4944ac4ca1edfb6230cb50f3b90b5d727ae0372
Joan Touzet <wohali@users.noreply.github.com>
2019-01-18 15:16:19 -0500

    Add new /{db}/_sync_shards endpoint (admin-only) (#1811)
    This server admin-only endpoint forces an n-way sync of all shards
    across all nodes on which they are hosted.
    
    This can be useful for an administrator adding a new node to the
    cluster, after updating _dbs so that the new node hosts an existing db
    with content, to force the new node to sync all of that db's shards.
    
    Users may want to bump their `[mem3] sync_concurrency` value to a
    larger figure for the duration of the shards sync.
    
    Closes #1807

--- src/mem3/src/mem3_httpd_handlers.erl
+++ src/mem3/src/mem3_httpd_handlers.erl
@@ -18,6 +18,7 @@ url_handler(<<"_membership">>) -> fun mem3_httpd:handle_membership_req/1;
 url_handler(_) -> no_match.
 
 db_handler(<<"_shards">>) -> fun mem3_httpd:handle_shards_req/2;
+db_handler(<<"_sync_shards">>)   -> fun mem3_httpd:handle_sync_req/2;
 db_handler(_) -> no_match.
 
 design_handler(_) -> no_match.
