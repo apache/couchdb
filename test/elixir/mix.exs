a5ead1f0ef07cd29789f1622becd42c070f2d5bd
Garren Smith <garren.smith@gmail.com>
2019-01-29 15:04:38 +0200

    add elixir tests to travis
    Add the elixir test to `make check` and change the build language
    in travis from erlang to elixir.

--- test/elixir/mix.exs
+++ test/elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule Foo.Mixfile do
     [
       app: :foo,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
