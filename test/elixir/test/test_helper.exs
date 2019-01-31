d066b075b51c6312698e274c11fef94e1dade5f9
Garren Smith <garren.smith@gmail.com>
2019-01-31 15:22:30 +0200

    format

--- test/elixir/test/test_helper.exs
+++ test/elixir/test/test_helper.exs
@@ -1,3 +1,7 @@
-ExUnit.configure(exclude: [pending: true], formatters: [JUnitFormatter, ExUnit.CLIFormatter])
+ExUnit.configure(
+  exclude: [pending: true],
+  formatters: [JUnitFormatter, ExUnit.CLIFormatter]
+)
+
 ExUnit.start()
 Code.require_file("partition_helpers.exs", __DIR__)
