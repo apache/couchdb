d9d2375d203421c0e81b5186f9a95515af963529
garren smith <garren.smith@gmail.com>
2019-02-21 21:26:45 +0200

    Fix elixir tests on Jenkins (#1931)
    fix flaky  elixir tests on jenkins
    
    * Add retry_until to some flaky tests. 
    * Add skip_on_jenkins tag for tests that won't pass with retry_until but pass on Travis.
    * set jenkins timeout to 90 minutes

--- test/elixir/test/view_collation_test.exs
+++ test/elixir/test/view_collation_test.exs
@@ -70,28 +70,34 @@ defmodule ViewCollationTest do
   end
 
   test "ascending collation order", context do
-    resp = Couch.get(url(context))
-    pairs = Enum.zip(resp.body["rows"], @values)
+    retry_until(fn ->
+      resp = Couch.get(url(context))
+      pairs = Enum.zip(resp.body["rows"], @values)
 
-    Enum.each(pairs, fn {row, value} ->
-      assert row["key"] == convert(value)
+      Enum.each(pairs, fn {row, value} ->
+        assert row["key"] == convert(value)
+      end)
     end)
   end
 
   test "descending collation order", context do
-    resp = Couch.get(url(context), query: %{"descending" => "true"})
-    pairs = Enum.zip(resp.body["rows"], Enum.reverse(@values))
+    retry_until(fn ->
+      resp = Couch.get(url(context), query: %{"descending" => "true"})
+      pairs = Enum.zip(resp.body["rows"], Enum.reverse(@values))
 
-    Enum.each(pairs, fn {row, value} ->
-      assert row["key"] == convert(value)
+      Enum.each(pairs, fn {row, value} ->
+        assert row["key"] == convert(value)
+      end)
     end)
   end
 
   test "key query option", context do
     Enum.each(@values, fn value ->
-      resp = Couch.get(url(context), query: %{:key => :jiffy.encode(value)})
-      assert length(resp.body["rows"]) == 1
-      assert Enum.at(resp.body["rows"], 0)["key"] == convert(value)
+      retry_until(fn ->
+        resp = Couch.get(url(context), query: %{:key => :jiffy.encode(value)})
+        assert length(resp.body["rows"]) == 1
+        assert Enum.at(resp.body["rows"], 0)["key"] == convert(value)
+      end)
     end)
   end
 
