e75e68edd2f5377e2b5f9719ea19f44f23ccfb43
Paul J. Davis <paul.joseph.davis@gmail.com>
2019-03-08 17:15:49 -0600

    A simple test script for probing fdb progress
    Eventually when I get enough API covered this will be deleted and we'll
    use the normal JS/Elixir/EUnit suites. Those would blow up too much
    without a more significant portion of the API implemented.

---
+++ fdb-test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import requests
+
+S = requests.session()
+S.auth = ("adm", "pass")
+S.headers["Content-Type"] = "application/json"
+
+
+def main():
+    db_url = "http://127.0.0.1:15984/fdb-test"
+    S.delete(db_url)
+
+    r = S.put(db_url)
+    assert r.status_code == 201
+
+    r = S.get(db_url)
+    assert r.status_code == 200
+    info = r.json()
+    assert info["doc_count"] == 0
+    assert info["doc_del_count"] == 0
+    assert info["sizes"]["external"] == 2
+    assert info["update_seq"] == ("0" * 20)
+
+    r = S.delete(db_url)
+    assert r.status_code == 200
+    r = S.put(db_url)
+    assert r.status_code == 201
+
+    r = S.put(db_url + "/foo", data = "{}")
+    print r.text
+    assert r.status_code == 201
+    assert "rev" in r.json()
+
+
+if __name__ == "__main__":
+    main()
