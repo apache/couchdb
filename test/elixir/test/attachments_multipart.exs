c49b6ccac8665dd4031218d79130d1ad9d4d69eb
Juanjo Rodriguez <jjrodrig@gmail.com>
2019-04-05 00:09:48 +0200

    Attachmets multipart tests

--- test/elixir/test/attachments_multipart.exs
+++ test/elixir/test/attachments_multipart.exs
@@ -126,5 +126,168 @@ defmodule AttachmentMultipartTest do
     resp = Couch.get("/#{db_name}/multipart/baz.txt")
 
     assert resp.status_code == 404
+
+    resp =
+      Couch.get("/#{db_name}/multipart",
+        query: %{:attachments => true},
+        headers: [accept: "multipart/related,*/*;"]
+      )
+
+    assert resp.status_code == 200
+    assert resp.headers["Content-length"] == "790"
+    # parse out the multipart
+    sections = parse_multipart(resp)
+
+    assert length(sections) == 3
+    # The first section is the json doc. Check it's content-type.
+    # Each part carries their own meta data.
+
+    assert Enum.at(sections, 0).headers["Content-Type"] == "application/json"
+    assert Enum.at(sections, 1).headers["Content-Type"] == "application/test"
+    assert Enum.at(sections, 2).headers["Content-Type"] == "application/test"
+
+    assert Enum.at(sections, 1).headers["Content-Length"] == "21"
+    assert Enum.at(sections, 2).headers["Content-Length"] == "18"
+
+    assert Enum.at(sections, 1).headers["Content-Disposition"] ==
+             ~s(attachment; filename="foo.txt")
+
+    assert Enum.at(sections, 2).headers["Content-Disposition"] ==
+             ~s(attachment; filename="bar.txt")
+
+    doc = :jiffy.decode(Enum.at(sections, 0).body, [:return_maps])
+
+    assert doc["_attachments"]["foo.txt"]["follows"] == true
+    assert doc["_attachments"]["bar.txt"]["follows"] == true
+
+    assert Enum.at(sections, 1).body == "this is 21 chars long"
+    assert Enum.at(sections, 2).body == "this is 18 chars l"
+
+    # now get attachments incrementally (only the attachments changes since
+    # a certain rev).
+
+    resp =
+      Couch.get("/#{db_name}/multipart",
+        query: %{:atts_since => ~s(["#{first_rev}"])},
+        headers: [accept: "multipart/related,*/*;"]
+      )
+
+    assert resp.status_code == 200
+
+    sections = parse_multipart(resp)
+    assert length(sections) == 2
+
+    doc = :jiffy.decode(Enum.at(sections, 0).body, [:return_maps])
+
+    assert doc["_attachments"]["foo.txt"]["stub"] == true
+    assert doc["_attachments"]["bar.txt"]["follows"] == true
+    assert Enum.at(sections, 1).body == "this is 18 chars l"
+
+    # try the atts_since parameter together with the open_revs parameter
+    resp =
+      Couch.get("/#{db_name}/multipart",
+        query: %{
+          :open_revs => ~s(["#{doc["_rev"]}"]),
+          :atts_since => ~s(["#{first_rev}"])
+        },
+        headers: [accept: "multipart/related,*/*;"]
+      )
+
+    assert resp.status_code == 200
+    sections = parse_multipart(resp)
+
+    # 1 section, with a multipart/related Content-Type
+    assert length(sections) == 1
+
+    ctype_value = Enum.at(sections, 0).headers["Content-Type"]
+    assert String.starts_with?(ctype_value, "multipart/related;") == true
+
+    inner_sections = parse_multipart(Enum.at(sections, 0))
+    # 2 inner sections: a document body section plus an attachment data section
+    assert length(inner_sections) == 3
+    assert Enum.at(inner_sections, 0).headers["Content-Type"] == "application/json"
+
+    doc = :jiffy.decode(Enum.at(inner_sections, 0).body, [:return_maps])
+    assert doc["_attachments"]["foo.txt"]["follows"] == true
+    assert doc["_attachments"]["bar.txt"]["follows"] == true
+
+    assert Enum.at(inner_sections, 1).body == "this is 21 chars long"
+    assert Enum.at(inner_sections, 2).body == "this is 18 chars l"
+
+    # try it with a rev that doesn't exist (should get all attachments)
+
+    resp =
+      Couch.get("/#{db_name}/multipart",
+        query: %{
+          :atts_since => ~s(["1-2897589","#{first_rev}"])
+        },
+        headers: [accept: "multipart/related,*/*;"]
+      )
+
+    assert resp.status_code == 200
+    sections = parse_multipart(resp)
+
+    assert length(sections) == 2
+
+    doc = :jiffy.decode(Enum.at(sections, 0).body, [:return_maps])
+    assert doc["_attachments"]["foo.txt"]["stub"] == true
+    assert doc["_attachments"]["bar.txt"]["follows"] == true
+    assert Enum.at(sections, 1).body == "this is 18 chars l"
+
+    # check that with the document multipart/mixed API it's possible to receive
+    #  attachments in compressed form (if they're stored in compressed form)
+  end
+
+  def get_boundary(response) do
+    ctype = response.headers["Content-Type"]
+    ctype_args = String.split(ctype, "; ")
+    ctype_args = Enum.slice(ctype_args, 1, length(ctype_args))
+
+    boundary_arg =
+      Enum.find(
+        ctype_args,
+        fn arg -> String.starts_with?(arg, "boundary=") end
+      )
+
+    boundary = Enum.at(String.split(boundary_arg, "="), 1)
+
+    if String.starts_with?(boundary, ~s(")) do
+      :jiffy.decode(boundary)
+    else
+      boundary
+    end
+  end
+
+  def parse_multipart(response) do
+    boundary = get_boundary(response)
+
+    leading = "--#{boundary}\r\n"
+    last = "\r\n--#{boundary}--"
+    body = response.body
+    mimetext = Enum.at(String.split(body, leading, parts: 2), 1)
+    mimetext = Enum.at(String.split(mimetext, last, parts: 2), 0)
+
+    sections = String.split(mimetext, ~s(\r\n--#{boundary}))
+
+    Enum.map(sections, fn section ->
+      section_parts = String.split(section, "\r\n\r\n", parts: 2)
+      raw_headers = String.split(Enum.at(section_parts, 0), "\r\n")
+      body = Enum.at(section_parts, 1)
+
+      headers =
+        Enum.reduce(raw_headers, %{}, fn raw_header, acc ->
+          if raw_header != "" do
+            header_parts = String.split(raw_header, ": ")
+            Map.put(acc, Enum.at(header_parts, 0), Enum.at(header_parts, 1))
+          else
+            acc
+          end
+        end)
+
+      %{
+        :headers => headers,
+        :body => body
+      }
+    end)
   end
 end
