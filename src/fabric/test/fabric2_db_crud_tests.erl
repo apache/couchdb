4b722081b7d320189cbe89f52b022c3ff288b46c
Paul J. Davis <paul.joseph.davis@gmail.com>
2019-04-26 14:16:02 -0500

    Add more database CRUD tests

--- src/fabric/test/fabric2_db_crud_tests.erl
+++ src/fabric/test/fabric2_db_crud_tests.erl
@@ -28,7 +28,9 @@ crud_test_() ->
             fun() -> test_util:start_couch([fabric]) end,
             fun test_util:stop_couch/1,
             [
-                ?TDEF(create_db)
+                ?TDEF(create_db),
+                ?TDEF(open_db),
+                ?TDEF(delete_db)
             ]
         }
     }.
@@ -36,5 +38,34 @@ crud_test_() ->
 
 create_db() ->
     DbName = ?tempdb(),
-    ?assertMatch({ok, _}, fabric2_db:create(DbName, [])).
+    ?assertMatch({ok, _}, fabric2_db:create(DbName, [])),
+    ?assertEqual(true, ets:member(fabric2_server, DbName)),
+    ?assertEqual({error, file_exists}, fabric2_db:create(DbName, [])).
 
+
+open_db() ->
+    DbName = ?tempdb(),
+    ?assertError(database_does_not_exist, fabric2_db:open(DbName, [])),
+
+    ?assertMatch({ok, _}, fabric2_db:create(DbName, [])),
+    ?assertEqual(true, ets:member(fabric2_server, DbName)),
+
+    % Opening the cached version
+    ?assertMatch({ok, _}, fabric2_db:open(DbName, [])),
+
+    % Remove from cache and re-open
+    true = ets:delete(fabric2_server, DbName),
+    ?assertMatch({ok, _}, fabric2_db:open(DbName, [])).
+
+
+delete_db() ->
+    DbName = ?tempdb(),
+    ?assertError(database_does_not_exist, fabric2_db:delete(DbName, [])),
+
+    ?assertMatch({ok, _}, fabric2_db:create(DbName, [])),
+    ?assertEqual(true, ets:member(fabric2_server, DbName)),
+
+    ?assertEqual(ok, fabric2_db:delete(DbName, [])),
+    ?assertEqual(false, ets:member(fabric2_server, DbName)),
+
+    ?assertError(database_does_not_exist, fabric2_db:open(DbName, [])).
