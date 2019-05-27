d0ccfa2b463b7087c1c3a766ca3ae963c1d1738d
ILYA Khlopotov <iilyak@apache.org>
2019-05-27 19:17:16 +0000

    Add chained setups

---
+++ test/elixir/lib/setup/common.ex
@@ -0,0 +1,22 @@
+defmodule Couch.Test.Setup.Common do
+  @moduledoc """
+  A set of common setup pipelines for reuse
+
+  - httpd_with_admin - chttpd is started and new admin is created
+  - httpd_with_db - httpd_with_admin and new database is created
+  """
+  alias Couch.Test.Setup.Step
+
+  def httpd_with_admin(setup) do
+    setup
+      |> Step.Start.new(:start, extra_apps: [:chttpd])
+      |> Step.User.new(:admin, roles: [:server_admin])
+  end
+
+  def httpd_with_db(setup) do
+    setup
+      |> httpd_with_admin()
+      |> Step.Create.DB.new(:db)
+  end
+
+end
