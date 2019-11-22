8a5dee9df6a23cbc30ff179494202f244fb6a697
Eric Avdey <eiri@eiri.ca>
2019-11-22 15:15:45 -0400

    Change end-point /_up to check fdb connectivity

--- src/chttpd/src/chttpd_misc.erl
+++ src/chttpd/src/chttpd_misc.erl
@@ -498,12 +498,11 @@ handle_up_req(#httpd{method='GET'} = Req) ->
     "nolb" ->
         send_json(Req, 404, {[{status, nolb}]});
     _ ->
-        {ok, {Status}} = mem3_seeds:get_status(),
-        case couch_util:get_value(status, Status) of
-            ok ->
-                send_json(Req, 200, {Status});
-            seeding ->
-                send_json(Req, 404, {Status})
+        try
+            fabric2_db:list_dbs([{limit, 0}]),
+            send_json(Req, 200, {[{status, ok}]})
+        catch error:{timeout, _} ->
+            send_json(Req, 404, {[{status, backend_unavailable}]})
         end
     end;
 
