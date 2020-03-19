032934f3764c9e1ae2f8f359cf039349bf56cf86
Alexander Trauzzi <acj@trauzzi.me>
2020-03-19 05:43:47 -0500

    Feature - Add JWT support (#2648)
    Add JWT Authentication Handler
    
    Co-authored-by: Robert Newson <rnewson@apache.org>
    Co-authored-by: Joan Touzet <wohali@users.noreply.github.com>

---
+++ test/elixir/test/jwtauth_test.exs
@@ -0,0 +1,39 @@
+defmodule JwtAuthTest do
+  use CouchTestCase
+
+  @moduletag :authentication
+
+  test "jwt auth with secret", _context do
+
+    secret = "zxczxc12zxczxc12"
+
+    server_config = [
+      %{
+        :section => "jwt_auth",
+        :key => "secret",
+        :value => secret
+      }
+    ]
+
+    run_on_modified_server(server_config, fn ->
+      test_fun()
+    end)
+  end
+
+  def test_fun() do
+    resp = Couch.get("/_session",
+      headers: [authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjb3VjaEBhcGFjaGUub3JnIn0.KYHmGXWj0HNHzZCjfOfsIfZWdguEBSn31jUdDUA9118"]
+    )
+
+    assert resp.body["userCtx"]["name"] == "couch@apache.org"
+    assert resp.body["info"]["authenticated"] == "jwt"
+  end
+
+  test "jwt auth without secret", _context do
+
+    resp = Couch.get("/_session")
+
+    assert resp.body["userCtx"]["name"] == "adm"
+    assert resp.body["info"]["authenticated"] == "default"
+  end
+end
