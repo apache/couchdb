0508cfb27d9ba4e0881f66fe235edaaebd0e09ba
Juanjo Rodriguez <juanjo@apache.org>
2020-03-20 18:12:20 +0100

    Fix ported to elixir tag

--- test/javascript/tests/changes.js
+++ test/javascript/tests/changes.js
@@ -9,7 +9,8 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
-
+couchTests.elixir = true;
+ 
 function jsonp(obj) {
   T(jsonp_flag == 0);
   T(obj.results.length == 1 && obj.last_seq == 1, "jsonp");
