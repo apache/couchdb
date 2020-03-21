2ff7703eab302fe6e877d0c46d39c5fbde19221f
Alexander Trauzzi <acj@trauzzi.me>
2020-03-21 12:24:37 -0500

    Include roles in testing.

--- test/elixir/test/jwtauth_test.exs
+++ test/elixir/test/jwtauth_test.exs
@@ -110,6 +110,7 @@ defmodule JwtAuthTest do
     )
 
     assert resp.body["userCtx"]["name"] == "couch@apache.org"
+    assert resp.body["userCtx"]["roles"] == [<<"_admin">>]
     assert resp.body["info"]["authenticated"] == "jwt"
   end
 
