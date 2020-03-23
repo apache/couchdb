30893d25341deee192a7e443a751e3f839566ac5
Tony Sun <tony.sun427@gmail.com>
2020-03-23 10:21:37 -0700

    more whitespace

--- src/couch_views/test/couch_views_info_test.erl
+++ src/couch_views/test/couch_views_info_test.erl
@@ -12,12 +12,15 @@
 
 -module(couch_views_info_test).
 
+
 -include_lib("couch/include/couch_eunit.hrl").
 -include_lib("couch/include/couch_db.hrl").
 -include_lib("couch_mrview/include/couch_mrview.hrl").
 -include_lib("fabric/test/fabric2_test.hrl").
 
+
 -define(MAP_FUN1, <<"map_fun1">>).
+
 
 setup() ->
     Ctx = test_util:start_couch([
@@ -28,8 +31,10 @@ setup() ->
         ]),
     Ctx.
 
+
 cleanup(Ctx) ->
     test_util:stop_couch(Ctx).
+
 
 foreach_setup() ->
     {ok, Db} = fabric2_db:create(?tempdb(), [{user_ctx, ?ADMIN_USER}]),
@@ -43,9 +48,11 @@ foreach_setup() ->
     {ok, Info} = couch_views:get_info(Db, DDoc),
     {Db, Info}.
 
+
 foreach_teardown({Db, _}) ->
     meck:unload(),
     ok = fabric2_db:delete(fabric2_db:name(Db), []).
+
 
 views_info_test_() ->
     {
@@ -71,41 +78,54 @@ views_info_test_() ->
         }
     }.
 
+
 sig_is_binary({_, Info}) ->
     ?_assert(is_binary(prop(signature, Info))).
+
 
 language_is_js({_, Info}) ->
     ?_assertEqual(<<"javascript">>, prop(language, Info)).
 
+
 data_size_is_non_neg_int({_, Info}) ->
     ?_assert(check_non_neg_int(data_size, Info)).
+
 
 active_size_is_non_neg_int({_, Info}) ->
     ?_assert(check_non_neg_int([sizes, active], Info)).
 
+
 updater_running_is_boolean({_, Info}) ->
     ?_assert(is_boolean(prop(updater_running, Info))).
 
+
 update_seq_is_binary({_, Info}) ->
     ?_assert(is_binary(prop(update_seq, Info))).
+
 
 update_opts_is_bin_list({_, Info}) ->
     Opts = prop(update_options, Info),
     ?_assert(is_list(Opts) andalso
             (Opts == [] orelse lists:all([is_binary(B) || B <- Opts]))).
 
+
 check_non_neg_int(Key, Info) ->
     Size = prop(Key, Info),
     is_integer(Size) andalso Size >= 0.
 
+
 prop(Key, {Props}) when is_list(Props) ->
     prop(Key, Props);
+
 prop([Key], Info) ->
     prop(Key, Info);
+
 prop([Key | Rest], Info) ->
     prop(Rest, prop(Key, Info));
+
 prop(Key, Info) when is_atom(Key), is_list(Info) ->
     couch_util:get_value(Key, Info).
+
 
 create_ddoc() ->
     couch_doc:from_json_obj({[
@@ -117,18 +137,23 @@ create_ddoc() ->
         ]}}
     ]}).
 
+
 doc(Id, Val) ->
     couch_doc:from_json_obj({[
         {<<"_id">>, list_to_binary(integer_to_list(Id))},
         {<<"val">>, Val}
     ]}).
 
+
 fold_fun({meta, _Meta}, Acc) ->
     {ok, Acc};
+
 fold_fun({row, _} = Row, Acc) ->
     {ok, [Row | Acc]};
+
 fold_fun(complete, Acc) ->
     {ok, lists:reverse(Acc)}.
+
 
 run_query(#{} = Db, DDoc, <<_/binary>> = View) ->
     couch_views:query(Db, DDoc, View, fun fold_fun/2, [], #mrargs{}).
