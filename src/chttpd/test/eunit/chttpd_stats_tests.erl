a3fe87d2b8d98496555534ed30b07f982b136e03
Tony Sun <tony.sun427@gmail.com>
2020-04-15 13:16:18 -0700

    modify test

--- src/chttpd/test/eunit/chttpd_stats_tests.erl
+++ src/chttpd/test/eunit/chttpd_stats_tests.erl
@@ -1,14 +1,11 @@
 -module(chttpd_stats_tests).
 
--define(NODEBUG, ok).
-
 -include_lib("couch/include/couch_eunit.hrl").
 -include_lib("couch/include/couch_db.hrl").
 
 
 setup() ->
-    ok = meck:new(chttpd_stats, [unstick, passthrough]),
-    ok = meck:expect(chttpd_stats, report, fun(_) -> ok end).
+    ok = meck:new(chttpd_stats, [passthrough]).
 
 teardown(_) ->
     meck:unload(),
@@ -34,15 +31,18 @@ chttpd_stats_test_() ->
 
 
 test_reset(_) ->
-   config:set_integer("chttpd", "stats_reset_interval", 60),
-   chttpd_stats:init(undefined),
-   chttpd_stats:incr_rows(),
-   State = get(chttpd_stats),
-   ?assertEqual(1, element(4, State)),
-   % force a reset with 0 interval
-   chttpd_stats:update_interval(0),
-   % after this is called, the report should happen and rows should
-   % reset to 0
-   chttpd_stats:incr_rows(),
-   ResetState = get(chttpd_stats),
-   ?_assertEqual(0, element(4, ResetState)).
+    ?_test(begin
+        config:set_integer("chttpd", "stats_reporting_interval", 60),
+        ok = meck:expect(chttpd_stats, report, fun(_) -> true end),
+        chttpd_stats:init(undefined),
+        chttpd_stats:incr_rows(),
+        State = get(chttpd_stats),
+        ?assertEqual(1, element(4, State)),
+        % force a reset with 0 interval
+        chttpd_stats:update_interval(0),
+        % after this is called, the report should happen and rows should
+        % reset to 0
+        chttpd_stats:incr_rows(),
+        ResetState = get(chttpd_stats),
+        ?assertEqual(0, element(4, ResetState))
+    end).
