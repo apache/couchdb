baba64bfe47ab548231375f95b1e1a2a68d95bcc
Juanjo Rodriguez <juanjo@apache.org>
2020-04-26 19:57:01 +0200

    Port replicator db tests to elixir

--- test/javascript/tests/replicator_db_bad_rep_id.js
+++ test/javascript/tests/replicator_db_bad_rep_id.js
@@ -10,6 +10,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+couchTests.elixir = true;
 couchTests.replicator_db_bad_rep_id = function(debug) {
   //return console.log('TODO');
   if (debug) debugger;
--- test/javascript/tests/replicator_db_by_doc_id.js
+++ test/javascript/tests/replicator_db_by_doc_id.js
@@ -10,6 +10,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+couchTests.elixir = true;
 couchTests.replicator_db_by_doc_id = function(debug) {
   //return console.log('TODO');
 
