796d714c3fb5a5fc1cc0ddcd8184ff304084407d
Juanjo Rodriguez <juanjo@apache.org>
2020-07-23 08:26:36 +0200

    port rewrite and rewrite_js tests into elixir

--- test/javascript/tests/rewrite.js
+++ test/javascript/tests/rewrite.js
@@ -10,7 +10,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
  
- 
+couchTests.elixir = true;
  
 couchTests.rewrite = function(debug) {
   if (debug) debugger;
