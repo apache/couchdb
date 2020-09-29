87201b7baa9ab652a0ee723d769db6fd5903540a
Juanjo Rodriguez <juanjo@apache.org>
2020-09-29 11:24:39 +0200

    Complete the port of security_validation tests to Elixir

--- test/javascript/tests/security_validation.js
+++ test/javascript/tests/security_validation.js
@@ -9,7 +9,7 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
-
+couchTests.elixir = true;
 couchTests.security_validation = function(debug) {
 
   var db_name = get_random_db_name();
