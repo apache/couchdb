65fbc6cfdb29bab1072a42f4d6005da452a7fa65
ILYA Khlopotov <iilyak@apache.org>
2020-12-02 04:30:15 -0800

    Add ability to control which Elixir integration tests to run
    New `elixir-suite` Makefile target is added. It runs a predefined set of elixir
    integration tests.
    
    The feature is controlled by two files:
    - test/elixir/test/config/suite.elixir - contains list of all available tests
    - test/elixir/test/config/skip.elixir - contains list of tests to skip
    
    In order to update the `test/elixir/test/config/suite.elixir` when new tests
    are added. The one would need to run the following command:
    
    ```
    MIX_ENV=integration mix suite > test/elixir/test/config/suite.elixir
    ```

--- test/elixir/test/test_helper.exs
+++ test/elixir/test/test_helper.exs
@@ -1,18 +1,3 @@
-# If build number detected assume we running on Jenkins
-# and skip certain tests that fail on jenkins.
-exclude =
-  case System.get_env("BUILD_NUMBER") !== nil do
-    true -> [:pending, :skip_on_jenkins]
-    false -> [:pending]
-  end
-
-current_exclude = Keyword.get(ExUnit.configuration(), :exclude, [])
-
-ExUnit.configure(
-  exclude: Enum.uniq(exclude ++ current_exclude),
-  formatters: [JUnitFormatter, ExUnit.CLIFormatter]
-)
-
-ExUnit.start()
+Couch.Test.Suite.start()
 Code.require_file("partition_helpers.exs", __DIR__)
 Code.require_file("reshard_helpers.exs", __DIR__)
