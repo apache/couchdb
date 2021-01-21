875e67aa71d504599bd337ba001847b68be43ab3
Bessenyei Balázs Donát <bessbd@apache.org>
2021-01-21 17:49:24 +0100

    Fix PUT of multipart/related attachments support for Transfer-Encoding: chunked
    This fixes #1943 .

--- src/chttpd/src/chttpd_db.erl
+++ src/chttpd/src/chttpd_db.erl
@@ -1242,6 +1242,14 @@ bulk_get_multipart_boundary() ->
 receive_request_data(Req) ->
     receive_request_data(Req, chttpd:body_length(Req)).
 
+receive_request_data(Req, Len) when Len == chunked ->
+    ChunkFun = fun({_Length, Binary}, _State) ->
+        self() ! {chunk, Binary}
+    end,
+    couch_httpd:recv_chunked(Req, 4096, ChunkFun, ok),
+    GetChunk = fun() -> receive {chunk, Binary} -> Binary end end,
+    {GetChunk(), GetChunk};
+
 receive_request_data(Req, LenLeft) when LenLeft > 0 ->
     Len = erlang:min(4096, LenLeft),
     Data = chttpd:recv(Req, Len),
--- src/couch/src/couch_httpd.erl
+++ src/couch/src/couch_httpd.erl
@@ -589,7 +589,8 @@ recv_chunked(#httpd{mochi_req=MochiReq}, MaxChunkSize, ChunkFun, InitState) ->
     % Fun is called once with each chunk
     % Fun({Length, Binary}, State)
     % called with Length == 0 on the last time.
-    MochiReq:stream_body(MaxChunkSize, ChunkFun, InitState).
+    MochiReq:stream_body(MaxChunkSize, ChunkFun, InitState,
+        config:get_integer("httpd", "max_http_request_size", 4294967296)).
 
 body_length(#httpd{mochi_req=MochiReq}) ->
     MochiReq:get(body_length).
