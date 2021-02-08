3805762bc63b34f356920833f0236bfbeb54aac6
Janos Gyerman <Janos.Gyerman@ibm.com>
2021-02-08 14:12:25 +0100

    Extend error handling with an unhandled case

--- src/chttpd/src/chttpd.erl
+++ src/chttpd/src/chttpd.erl
@@ -1036,6 +1036,8 @@ error_info({service_unavailable, Reason}) ->
     {503, <<"service unavailable">>, Reason};
 error_info({timeout, _Reason}) ->
     error_info(timeout);
+error_info({'EXIT', {Error, Stack}}) ->
+    error_info({Error, nil, Stack});
 error_info({Error, null}) ->
     error_info(Error);
 error_info({_Error, _Reason} = Error) ->
