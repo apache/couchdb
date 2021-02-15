5724e8e3f35839f67e15dfca478a2b80103127b4
Robert Newson <rnewson@apache.org>
2021-02-15 13:24:13 +0000

    Add "couch_server" aggregate to _system output
    This helps ease transition from singleton couch_server to
    multiple. The "couch_server" message queue is simply the sum of the
    couch_server_X message queues.

--- src/chttpd/src/chttpd_node.erl
+++ src/chttpd/src/chttpd_node.erl
@@ -209,7 +209,8 @@ get_stats() ->
     {NumberOfGCs, WordsReclaimed, _} = statistics(garbage_collection),
     {{input, Input}, {output, Output}} = statistics(io),
     {CF, CDU} = db_pid_stats(),
-    MessageQueues0 = [{couch_file, {CF}}, {couch_db_updater, {CDU}}],
+    MessageQueues0 = [{couch_file, {CF}}, {couch_db_updater, {CDU}},
+        {couch_server, couch_server:aggregate_queue_len()}],
     MessageQueues = MessageQueues0 ++ message_queues(registered()),
     {SQ, DCQ} = run_queues(),
     [
