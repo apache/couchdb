apache/couchdb

commit acb43e12fd7fddc6f606246875909f7c7df27324

Improve retryable FDB error handling
After running the Elixir suite under the buggify" mode, we noticed
retryable errors were not handled well. In some cases we handled some
errors (1009) but not others (1007, 1031). Some other retryable errors
were not considered at all.

So here we use the newly defined constants and `ERLFDB_IS_RETRYABLE/2`
guard from erlfdb v1.3.1 to make handling of these errors a bit more
consistent.

src/couch_jobs/src/couch_jobs_type_monitor.erl
@@ -55,7 +55,10 @@ loop(#st{vs = VS, timeout = Timeout} = St) ->
     try
         erlfdb:wait(Watch, [{timeout, Timeout}])
     catch
-        error:{erlfdb_error, ?FUTURE_VERSION} ->
+        error:{erlfdb_error, ?ERLFDB_TRANSACTION_TIMED_OUT} ->
+            erlfdb:cancel(Watch, [flush]),
+            ok;
+        error:{erlfdb_error, Code} when ?ERLFDB_IS_RETRYABLE(Code) ->
             erlfdb:cancel(Watch, [flush]),
             ok;
         error:{timeout, _} ->

rebar.config.script
@@ -153,7 +153,7 @@ DepDescs = [
 %% Independent Apps
 {config,           "config",           {tag, "2.1.8"}},
 {b64url,           "b64url",           {tag, "1.0.2"}},
-{erlfdb,           "erlfdb",           {tag, "v1.3.0"}},
+{erlfdb,           "erlfdb",           {tag, "v1.3.1"}},
 {ets_lru,          "ets-lru",          {tag, "1.1.0"}},
 {khash,            "khash",            {tag, "1.1.0"}},
 {snappy,           "snappy",           {tag, "CouchDB-1.0.4"}},

src/couch_jobs/src/couch_jobs.erl
@@ -351,7 +351,9 @@ accept_loop(Type, NoSched, MaxSchedTime, Timeout) ->
     catch
         error:{timeout, _} ->
             retry;
-        error:{erlfdb_error, Err} when Err =:= 1020 orelse Err =:= 1031 ->
+        error:{erlfdb_error, ?ERLFDB_TRANSACTION_TIMED_OUT} ->
+            retry;
+        error:{erlfdb_error, Err} when ?ERLFDB_IS_RETRYABLE(Err) ->
             retry
     end,
     case AcceptResult of
@@ -387,7 +389,10 @@ wait_pending(PendingWatch, MaxSTime, UserTimeout, NoSched) ->
         erlfdb:wait(PendingWatch, [{timeout, Timeout}]),
         ok
     catch
-        error:{erlfdb_error, ?FUTURE_VERSION} ->
+        error:{erlfdb_error, ?ERLFDB_TRANSACTION_TIMED_OUT} ->
+            erlfdb:cancel(PendingWatch, [flush]),
+            retry;
+        error:{erlfdb_error, Error} when ?ERLFDB_IS_RETRYABLE(Error) ->
             erlfdb:cancel(PendingWatch, [flush]),
             retry;
         error:{timeout, _} ->

src/couch_jobs/src/couch_jobs_activity_monitor.erl
@@ -28,6 +28,10 @@
     code_change/3
 ]).
 
+
+-include("couch_jobs.hrl").
+
+
 -record(st, {
     jtx,
     type,
@@ -68,7 +72,8 @@ handle_info(check_activity, St) ->
     St1 = try
         check_activity(St)
     catch
-        error:{erlfdb_error, Err} when Err =:= 1020 orelse Err =:= 1031 ->
+        error:{erlfdb_error, Err} when ?ERLFDB_IS_RETRYABLE(Err) orelse
+                Err =:= ?ERLFDB_TRANSACTION_TIMED_OUT ->
             LogMsg = "~p : type:~p got ~p error, possibly from overload",
             couch_log:error(LogMsg, [?MODULE, St#st.type, Err]),
             St

src/couch_jobs/src/couch_jobs_notifier.erl
@@ -252,8 +252,12 @@ try_notify_subscribers(ActiveVS, #st{} = St) ->
     try
         notify_subscribers(ActiveVS, St)
     catch
-        error:{timeout, _} -> try_notify_subscribers(ActiveVS, St);
-        error:{erlfdb_error, 1031} -> try_notify_subscribers(ActiveVS, St)
+        error:{timeout, _} ->
+            try_notify_subscribers(ActiveVS, St);
+        error:{erlfdb_error, ?ERLFDB_TRANSACTION_TIMED_OUT} ->
+            try_notify_subscribers(ActiveVS, St);
+        error:{erlfdb_error, Code} when ?ERLFDB_IS_RETRYABLE(Code) ->
+            try_notify_subscribers(ActiveVS, St)
     end.
 
 

src/couch_views/src/couch_views_indexer.erl
@@ -43,10 +43,10 @@
 % These are all of the errors that we can fix by using
 % a smaller batch size.
 -define(IS_RECOVERABLE_ERROR(Code), (
-    (Code == 1004) % timed_out
-    orelse (Code == 1007) % transaction_too_old
-    orelse (Code == 1031) % transaction_timed_out
-    orelse (Code == 2101) % transaction_too_large
+    (Code == ?ERLFDB_TIMED_OUT) orelse
+    (Code == ?ERLFDB_TRANSACTION_TOO_OLD) orelse
+    (Code == ?ERLFDB_TRANSACTION_TIMED_OUT) orelse
+    (Code == ?ERLFDB_TRANSACTION_TOO_LARGE)
 )).
 
 
@@ -161,7 +161,7 @@ upgrade_data(Data) ->
 
 
 % Transaction limit exceeded don't retry
-should_retry(_, _, {erlfdb_error, 2101}) ->
+should_retry(_, _, {erlfdb_error, ?ERLFDB_TRANSACTION_TOO_LARGE}) ->
     false;
 
 should_retry(Retries, RetryLimit, _) when Retries < RetryLimit ->

src/fabric/include/fabric2.hrl
@@ -11,6 +11,9 @@
 % the License.
 
 
+-include_lib("erlfdb/include/erlfdb.hrl").
+
+
 -define(uint2bin(I), binary:encode_unsigned(I, little)).
 -define(bin2uint(I), binary:decode_unsigned(I, little)).
 -define(bin2int(V), binary_to_integer(V)).
@@ -76,13 +79,5 @@
 -define(PDICT_TX_ID_KEY, '$fabric_tx_id').
 -define(PDICT_TX_RES_KEY, '$fabric_tx_result').
 -define(PDICT_FOLD_ACC_STATE, '$fabric_fold_acc_state').
-
-% Let's keep these in ascending order
--define(TRANSACTION_TOO_OLD, 1007).
--define(FUTURE_VERSION, 1009).
--define(COMMIT_UNKNOWN_RESULT, 1021).
--define(TRANSACTION_CANCELLED, 1025).
--define(TRANSACTION_TOO_LARGE, 2101).
-
 
 -define(DEFAULT_BINARY_CHUNK_SIZE, 100000).

src/fabric/src/fabric2_fdb.erl
@@ -567,10 +567,10 @@ get_info_wait(#info_future{tx = Tx, retries = Retries} = Future) ->
     try
         get_info_wait_int(Future)
     catch
-        error:{erlfdb_error, ?TRANSACTION_CANCELLED} ->
+        error:{erlfdb_error, ?ERLFDB_TRANSACTION_CANCELLED} ->
             Future1 = get_info_future(Tx, Future#info_future.db_prefix),
             get_info_wait(Future1#info_future{retries = Retries + 1});
-        error:{erlfdb_error, ?TRANSACTION_TOO_OLD} ->
+        error:{erlfdb_error, Error} when ?ERLFDB_IS_RETRYABLE(Error) ->
             ok = erlfdb:reset(Tx),
             Future1 = get_info_future(Tx, Future#info_future.db_prefix),
             get_info_wait(Future1#info_future{retries = Retries + 1})
@@ -1161,7 +1161,8 @@ fold_range(Tx, FAcc) ->
             user_acc = FinalUserAcc
         } = erlfdb:fold_range(Tx, Start, End, Callback, FAcc, Opts),
         FinalUserAcc
-    catch error:{erlfdb_error, ?TRANSACTION_TOO_OLD} when DoRestart ->
+    catch error:{erlfdb_error, Error} when
+            ?ERLFDB_IS_RETRYABLE(Error) andalso DoRestart ->
         % Possibly handle cluster_version_changed and future_version as well to
         % continue iteration instead fallback to transactional and retrying
         % from the beginning which is bound to fail when streaming data out to a
@@ -1991,7 +1992,7 @@ clear_transaction() ->
 
 
 is_commit_unknown_result() ->
-    erlfdb:get_last_error() == ?COMMIT_UNKNOWN_RESULT.
+    erlfdb:get_last_error() == ?ERLFDB_COMMIT_UNKNOWN_RESULT.
 
 
 has_transaction_id() ->

commit ca355ebaf9d8fa8afbc2da56b0ccee2e29964f13

Retryable error fixes in couch_jobs_type_monitor
This continues improvements to retryable error handling started in
https://github.com/apache/couchdb/pull/3460. Here we add the same logic we
already have for the `erlfdb:wait/2` call in
https://github.com/apache/couchdb/blob/main/src/couch_jobs/src/couch_jobs_type_monitor.erl#L55-L57
to the `get_vs_and_watch/1` section.

couch_jobs_type_monitor is meant to be linked to and run in a continuous loop
as long as the parent process is alive. If FDB becomes unavailable the main
process which we linked to or other main component (the whole application)
should crash and fail as opposed to the type monitor itself. Still, to avoid
running in a tight loop we use the holdoff interval to sleep a bit before
recursing. The typical values of the holdoff is around 50-100 msec.

src/couch_jobs/src/couch_jobs_type_monitor.erl
@@ -81,7 +81,20 @@ notify(#st{} = St) ->
     St#st{timestamp = Now}.
 
 
-get_vs_and_watch(#st{jtx = JTx, type = Type}) ->
-    couch_jobs_fdb:tx(JTx, fun(JTx1) ->
-        couch_jobs_fdb:get_activity_vs_and_watch(JTx1, Type)
-    end).
+get_vs_and_watch(#st{} = St) ->
+    #st{jtx = JTx, type = Type, holdoff = HoldOff} = St,
+    try
+        couch_jobs_fdb:tx(JTx, fun(JTx1) ->
+            couch_jobs_fdb:get_activity_vs_and_watch(JTx1, Type)
+        end)
+    catch
+        error:{erlfdb_error, ?ERLFDB_TRANSACTION_TIMED_OUT} ->
+            timer:sleep(HoldOff),
+            get_vs_and_watch(St);
+        error:{erlfdb_error, Code} when ?ERLFDB_IS_RETRYABLE(Code) ->
+            timer:sleep(HoldOff),
+            get_vs_and_watch(St);
+        error:{timeout, _} ->
+            timer:sleep(HoldOff),
+            get_vs_and_watch(St)
+    end.