890c1ead085f1c252b84886805f63334081588cc
Bessenyei Balázs Donát <bessbd@apache.org>
2021-05-14 12:45:23 +0200

    Refactor couch_eval_error_tests

--- src/couch_eval/test/couch_eval_error_tests.erl
+++ src/couch_eval/test/couch_eval_error_tests.erl
@@ -2,7 +2,7 @@
 % use this file except in compliance with the License. You may obtain a copy of
 % the License at
 %
-%   http://www.apache.org/licenses/LICENSE-2.0
+%     http://www.apache.org/licenses/LICENSE-2.0
 %
 % Unless required by applicable law or agreed to in writing, software
 % distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -20,34 +20,35 @@
 
 
 setup() ->
-  meck:new(mock_language_server, [non_strict]),
-  Ctx = test_util:start_couch(),
-  config:set("couch_eval.languages", ?LANG_STRING,
-    atom_to_list(mock_language_server)),
-  Ctx.
+    meck:new(mock_language_server, [non_strict]),
+    Ctx = test_util:start_couch(),
+    config:set("couch_eval.languages", ?LANG_STRING,
+        atom_to_list(mock_language_server)),
+    Ctx.
 
 
 teardown(Ctx) ->
-  test_util:stop_couch(Ctx),
-  meck:unload().
+    test_util:stop_couch(Ctx),
+    meck:unload().
 
 
 error_test_() ->
-  {
-    "Error tests",
     {
-      setup,
-      fun setup/0, fun teardown/1,
-      [
-        fun acquire_map_context_error_handled/0
-      ]
-    }
-  }.
+        "Error tests",
+        {
+            setup,
+            fun setup/0, fun teardown/1,
+            [
+                fun acquire_map_context_error_handled/0
+            ]
+        }
+    }.
+
 
 acquire_map_context_error_handled() ->
-  meck:expect(mock_language_server, acquire_map_context,
-    fun(_) -> {error, foo_error} end),
-  Result = couch_eval:acquire_map_context(<<"foo">>, <<"bar">>, ?LANG_BINARY,
-    <<"baz">>, <<"quux">>, [<<"quuz">>]),
-  ?assertEqual({error, foo_error}, Result).
-
+    meck:expect(mock_language_server, acquire_map_context, fun(_) ->
+        {error, foo_error}
+    end),
+    Result = couch_eval:acquire_map_context(<<"foo">>, <<"bar">>, ?LANG_BINARY,
+        <<"baz">>, <<"quux">>, [<<"quuz">>]),
+    ?assertEqual({error, foo_error}, Result).
