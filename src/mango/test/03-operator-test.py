1e4be2671153e8eb48b5a1dd7ad8f86d479b3863
hklarner <leevilux@yahoo.de>
2021-05-26 15:57:11 +0200

    adds tests for operator $sizeGte for bad argument and field

--- src/mango/test/03-operator-test.py
+++ src/mango/test/03-operator-test.py
@@ -75,7 +75,7 @@ class OperatorTests:
         docs = self.db.find({"foo": {"$keyMapMatch": {"$eq": "aa"}}})
         self.assertEqual(len(docs), 1)
 
-    def test_size(self):
+    def test_size_success(self):
         amdocs = [
             {"customers": [0]},
             {"customers": [0, 1]},
@@ -89,6 +89,20 @@ class OperatorTests:
         self.assertEqual(len(docs), 3)
         docs = self.db.find({"customers": {"$sizeGte": 3}})
         self.assertEqual(len(docs), 2)
+
+    def test_size_fails_on_bad_argument(self):
+        amdocs = [
+            {"customers": [0, 1, 2]},
+        ]
+        self.db.save_docs(amdocs, w=3)
+        docs = self.db.find({"customers": {"$sizeGte": "text"}})
+
+    def test_size_fails_on_bad_field(self):
+        amdocs = [
+            {"customers": "text"},
+        ]
+        self.db.save_docs(amdocs, w=3)
+        docs = self.db.find({"customers": {"$sizeGte": 5}})
 
     def test_in_operator_array(self):
         docs = self.db.find({"manager": True, "favorites": {"$in": ["Ruby", "Python"]}})
