be326c63faa679b7c69b3b5cf9d48c41aa66130d
Jay Doane <jaydoane@apache.org>
2021-09-03 16:58:17 -0700

    Set structured logger to info level for exunit tests
    Suppress excessive debug logging of the form:
    
    mix test --cover --trace src/chttpd/test/exunit/pagination_test.exs:232
    
      * test Legacy API (10 docs) : _all_docs/queries
    12:40:01.895 [debug] [user: "adm", what: :login_attempt]
    12:40:01.906 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.925 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.934 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.944 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.952 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.960 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.968 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.977 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.985 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:01.995 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:02.004 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:02.013 [debug] [username: 'adm', what: :successful_cookie_auth]
    12:40:02.019 [debug] [username: 'adm', what: :successful_cookie_auth]
      * test Legacy API (10 docs) : _all_docs/queries (152.2ms)

--- src/couch/test/exunit/test_helper.exs
+++ src/couch/test/exunit/test_helper.exs
@@ -1,2 +1,6 @@
 ExUnit.configure(formatters: [JUnitFormatter, ExUnit.CLIFormatter])
+
+Kernel.function_exported?(:logger, :set_primary_config, 2) and
+  :logger.set_primary_config(:level, :info)
+
 ExUnit.start()
