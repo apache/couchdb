29640aa456897da539287f8bf6f66b7b66e48207
Nick Vatamaniuc <vatamane@gmail.com>
2021-11-17 15:35:56 -0500

    Port erlfmt formatting to 3.x
    From PR: #3568
    
    A few changes to the formatting logic compared to `main`:
    
      * rebar3 and `erlfmt` don't work with Erlang 20, so added an OTP version
        check to skip formatting for Erlang 20
    
      * `make erlfmt-*` commands are faster. Instead of spawning an `erlfmt`
        process per-file, spawn one per `directory/*.erl` pattern.
    
      * Remove the non-0 return code work-around. `erlfmt` was returning an error
        on `main` because the line width option used for formatting was different
        than the one used during format checking. Making both the default removed
        all the spurious non-0 exits with the current version of `erlfmt`. Thanks
        to Adam Kocoloski for pointing out the issue.

---
+++ dev/format_check.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+"""Erlang formatter for CouchDB
+Warning: this file needs to run from the CouchDB repo root.
+USAGE: ERLFMT_PATH=<path_to_erlfmt> python3 dev/format_check.py
+"""
+
+import os
+import subprocess
+import sys
+
+from format_lib import get_source_paths, get_erlang_version
+
+
+if __name__ == "__main__":
+    if get_erlang_version() < 21:
+        print("Erlang version is < 21. Skipping format check")
+        sys.exit(0)
+
+    exit_code = 0
+
+    for path in get_source_paths():
+        run_result = subprocess.run(
+            [os.environ["ERLFMT_PATH"], "-c", path],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        rc = run_result.returncode
+        if rc != 0:
+            print("\n %s error for %s" % (rc, path))
+            stderr_lines = run_result.stderr.decode("utf-8").split("\n")
+            for line in stderr_lines:
+                print("  > %s" % line, file=sys.stderr)
+            exit_code = 1
+
+    sys.exit(exit_code)
---
+++ dev/format_lib.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+"""Erlang formatter lib for CouchDB
+Warning: this file is not meant to be executed manually
+"""
+
+import pathlib
+import subprocess
+
+
+def get_erlang_version():
+    args = [
+        "erl",
+        "-eval",
+        "io:put_chars(erlang:system_info(otp_release)), halt().",
+        "-noshell",
+    ]
+    res = subprocess.run(args, stdout=subprocess.PIPE, check=True)
+    str_version = res.stdout.decode("utf-8").strip().strip('"')
+    return int(str_version)
+
+
+# Generate source paths as "directory/*.erl" wildcard patterns
+# those can be directly consumed by erlfmt and processed in parallel
+#
+def get_source_paths():
+    curdir = None
+    for item in (
+        subprocess.run(
+            ["git", "ls-files", "--", "*.erl"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        .stdout.decode("utf-8")
+        .split("\n")
+    ):
+        path = pathlib.Path(item)
+        if path.parent != curdir:
+            yield str(path.parent.joinpath("*.erl"))
+            curdir = path.parent
+    if curdir is not None:
+        yield str(curdir.joinpath("*.erl"))
