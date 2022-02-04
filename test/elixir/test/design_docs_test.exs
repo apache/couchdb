3d70fb35d4f8c6dc978894b88c00176e6f01d8dd
Nick Vatamaniuc <vatamane@apache.org>
2022-02-04 00:59:41 -0500

    View collation upgrade tests
    Checking a few scenarios:
     * _design/*/_info returns collation versions
     * A 2.x view can upgraded to have a view info map
     * A 3.2.1 view can be upgraded to have a view info map
     * A view with multiple collator versions:
       - Can be opened and tagged with the current libicu version
       - Can be queried, written to and then queried again
       - Will be submitted for compaction when open and on updates
       - After compaction the view will have only one collator version
    
    Includes two new fixture views:
     * Version 3.2.1 view without the view info map field
     * A view with a bogus (old) libicu version "1.1.1.1"
    
    The bogus libicu version view is generated by overriding the collation
    version API to return `[1, 1, 1, 1]` [1]. And it can be re-created
    with this snippet [2]
    
    [1]
    ```
    update_collator_versions(#{} = ViewInfo) ->
         Versions = maps:get(ucol_vs, ViewInfo, []),
    -    Ver = tuple_to_list(couch_ejson_compare:get_collator_version()),
    +    % Ver = tuple_to_list(couch_ejson_compare:get_collator_version()),
    +    Ver = [1, 1, 1, 1],
    ```
    
    [2] `http` is the `httpie` command line HTTP client
    ```
    http delete $DB/colltest1 && http put $DB/colltest1'?q=1' && http put $DB/colltest1/_design/colltest1ddoc views:='{"colltest1view":{"map":"function(doc){emit(doc.id, null);}"}}' && http put $DB/colltest1/d1 a=b && http put $DB/colltest1/d2 c=d
    ```

--- test/elixir/test/design_docs_test.exs
+++ test/elixir/test/design_docs_test.exs
@@ -267,9 +267,18 @@ defmodule DesignDocsTest do
     for _x <- 0..1 do
       resp = Couch.get("/#{db_name}/_design/test/_info")
       assert resp.body["name"] == "test"
-      assert resp.body["view_index"]["sizes"]["file"] == prev_view_size
-      assert resp.body["view_index"]["compact_running"] == false
-      assert resp.body["view_index"]["signature"] == prev_view_sig
+      assert is_map(resp.body["view_index"])
+      view_index = resp.body["view_index"]
+      assert view_index["sizes"]["file"] == prev_view_size
+      assert view_index["compact_running"] == false
+      assert view_index["signature"] == prev_view_sig
+
+      # check collator_versions result
+      assert is_list(view_index["collator_versions"])
+      collator_versions = view_index["collator_versions"]
+      assert length(collator_versions) == 1
+      version = hd(collator_versions)
+      assert is_binary(version)
     end
   end
 
