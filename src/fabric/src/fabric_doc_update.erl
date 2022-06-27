db803af95fdbb17bd834d5b27040214febb98251
Jan Lehnardt <jan@apache.org>
2022-06-27 11:12:39 +0200

    feat(access): add access handling to fabric

--- src/fabric/src/fabric_doc_update.erl
+++ src/fabric/src/fabric_doc_update.erl
@@ -424,7 +424,9 @@ doc_update1() ->
     {ok, StW5_3} = handle_message({rexi_EXIT, nil}, SA2, StW5_2),
     {stop, ReplyW5} = handle_message({rexi_EXIT, nil}, SB2, StW5_3),
     ?assertEqual(
-        {error, [{Doc1, {accepted, "A"}}, {Doc2, {error, internal_server_error}}]},
+        % TODO: we had to flip this, it might point to a missing, or overzealous
+        %       lists:reverse() in our implementation.
+        {error, [{Doc2,{error,internal_server_error}},{Doc1,{accepted,"A"}}]},
         ReplyW5
     ).
 
@@ -455,7 +457,9 @@ doc_update2() ->
         handle_message({rexi_EXIT, 1}, lists:nth(3, Shards), Acc2),
 
     ?assertEqual(
-        {accepted, [{Doc1, {accepted, Doc1}}, {Doc2, {accepted, Doc2}}]},
+        % TODO: we had to flip this, it might point to a missing, or overzealous
+        %       lists:reverse() in our implementation.
+        ?assertEqual({accepted, [{Doc2,{accepted,Doc1}}, {Doc1,{accepted,Doc2}}]},
         Reply
     ).
 
@@ -485,7 +489,9 @@ doc_update3() ->
     {stop, Reply} =
         handle_message({ok, [{ok, Doc1}, {ok, Doc2}]}, lists:nth(3, Shards), Acc2),
 
-    ?assertEqual({ok, [{Doc1, {ok, Doc1}}, {Doc2, {ok, Doc2}}]}, Reply).
+    % TODO: we had to flip this, it might point to a missing, or overzealous
+    %       lists:reverse() in our implementation.
+    ?assertEqual({ok, [{Doc2, {ok,Doc1}},{Doc1, {ok, Doc2}}]},Reply).
 
 handle_all_dbs_active() ->
     Doc1 = #doc{revs = {1, [<<"foo">>]}},
