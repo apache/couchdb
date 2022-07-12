76dd66f409ef4b34f9cc809228e0c7d1e6a50554
Noah Shaw <noahshaw11@yahoo.com>
2022-07-12 16:08:17 -0500

    Remove view compaction jobs recovery

--- src/smoosh/src/smoosh_channel.erl
+++ src/smoosh/src/smoosh_channel.erl
@@ -17,7 +17,7 @@
 
 % public api.
 -export([start_link/1, close/1, suspend/1, resume/1, activate/1, get_status/1]).
--export([enqueue/3, last_updated/2, flush/1, is_key/2, is_activated/1, persist/1]).
+-export([enqueue/3, last_updated/2, flush/1, is_key/2, is_activated/1]).
 
 % gen_server api.
 -export([
@@ -37,6 +37,7 @@
 -else.
 -define(START_DELAY_IN_MSEC, 0).
 -define(ACTIVATE_DELAY_IN_MSEC, 0).
+-export([persist/1]).
 -endif.
 
 % records.
@@ -94,6 +95,7 @@ is_key(ServerRef, Key) ->
 is_activated(ServerRef) ->
     gen_server:call(ServerRef, is_activated).
 
+% Only exported to force persistence in tests
 persist(ServerRef) ->
     gen_server:call(ServerRef, persist).
 
@@ -103,8 +105,15 @@ init(Name) ->
     erlang:send_after(60 * 1000, self(), check_window),
     process_flag(trap_exit, true),
     Waiting = smoosh_priority_queue:new(Name),
-    State = #state{name = Name, waiting = Waiting, paused = true, activated = false},
-    erlang:send_after(?START_DELAY_IN_MSEC, self(), start_recovery),
+    State =
+        case smoosh_utils:is_view_channel(Name) of
+            true ->
+                schedule_unpause(),
+                #state{name = Name, waiting = Waiting, paused = true, activated = true};
+            false ->
+                erlang:send_after(?START_DELAY_IN_MSEC, self(), start_recovery),
+                #state{name = Name, waiting = Waiting, paused = true, activated = false}
+        end,
     {ok, State}.
 
 handle_call({last_updated, Object}, _From, State) ->
