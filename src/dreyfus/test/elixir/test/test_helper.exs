1fdf444fad2bb475bdcd1e3fc74b3b7e5fbc718d
Jay Doane <jaydoane@apache.org>
2023-02-14 14:51:20 -0800

    Move elixir search tests to be with other elixir tests
    Now that dreyfus is integrated into CouchDB, its elixir tests can be
    moved to be with the other elixir integration tests. As a bonus,
    remove some unnecessary configuration.

--- src/dreyfus/test/elixir/test/test_helper.exs
+++
@@ -1,6 +0,0 @@
-Code.require_file "../../../../../test/elixir/lib/couch.ex", __DIR__
-Code.require_file "../../../../../test/elixir/lib/couch/dbtest.ex", __DIR__
-Code.require_file "../../../../../test/elixir/test/support/couch_test_case.ex", __DIR__
-Code.require_file "../../../../../test/elixir/lib/suite.ex", __DIR__
-Code.require_file "../../../../../test/elixir/test/test_helper.exs", __DIR__
-Couch.Test.Suite.start()
