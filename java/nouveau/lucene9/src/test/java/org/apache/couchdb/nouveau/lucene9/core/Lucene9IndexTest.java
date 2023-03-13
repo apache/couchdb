bd43d26660d4df0da12fc907e4aaf8587a7dc5ad
Robert Newson <rnewson@apache.org>
2023-03-13 14:43:13 +0000

    index testing framework

---
+++ java/nouveau/lucene9/src/test/java/org/apache/couchdb/nouveau/lucene9/core/Lucene9IndexTest.java
@@ -0,0 +1,42 @@
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.couchdb.nouveau.lucene9.core;
+
+import org.apache.couchdb.nouveau.core.BaseIndexTest;
+import org.apache.couchdb.nouveau.core.IndexLoader;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.misc.store.DirectIODirectory;
+import org.apache.lucene.search.SearcherManager;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class Lucene9IndexTest extends BaseIndexTest<IndexableField> {
+
+    @Override
+    protected IndexLoader<IndexableField> indexLoader() {
+        return (path, indexDefinition) -> {
+            final Analyzer analyzer = Lucene9AnalyzerFactory.fromDefinition(indexDefinition);
+            final Directory dir = new DirectIODirectory(FSDirectory.open(path));
+            final IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            config.setUseCompoundFile(false);
+            final IndexWriter writer = new IndexWriter(dir, config);
+            final SearcherManager searcherManager = new SearcherManager(writer, null);
+            return new Lucene9Index(analyzer, writer, 0L, searcherManager);
+        };
+    }
+
+}
