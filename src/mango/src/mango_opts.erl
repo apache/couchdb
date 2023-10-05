21d6e7b7228240d6483c9b9cd6118c576d0e1dad
Gabor Pali <gabor.pali@ibm.com>
2023-10-05 16:54:39 +0200

    mango: fix validation of `use_index`
    The `binary:split/2` function will split the input only at the
    first occurrence of the pattern which is not suitable for
    identifying each part of the index name, therefore validating it.
    Add the `global` option to fully break the index name into parts
    and let the related validation logic work as expected.

--- src/mango/src/mango_opts.erl
+++ src/mango/src/mango_opts.erl
@@ -262,7 +262,7 @@ validate_bulk_docs(Else) ->
     ?MANGO_ERROR({invalid_bulk_docs, Else}).
 
 validate_use_index(IndexName) when is_binary(IndexName) ->
-    case binary:split(IndexName, <<"/">>) of
+    case binary:split(IndexName, <<"/">>, [global]) of
         [DesignId] ->
             {ok, [DesignId]};
         [<<"_design">>, DesignId] ->
