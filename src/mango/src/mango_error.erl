e21d0e983001d09cdeb7e4208be6c25cda695733
Gabor Pali <gabor.pali@ibm.com>
2023-10-05 16:53:34 +0200

    mango: add `allow_fallback` for user-specified indexes on `_find`
    It is not always beneficial for the performance if the Mango query
    planner tries to assign an index to the selector.  User-specified
    indexes may save the day, but since they are only hints for the
    planner, fallbacks may still happen.
    
    Introduce the `allow_fallback` flag which can be used to tell if
    falling back to other indexes is acceptable when an index is
    explicitly specified by the user.  When set to `false`, give up
    on planning and return an HTTP 400 response right away.  This way
    the user has the chance to learn about the requested but missing
    index, optionally create it and try again.
    
    By default, fallbacks are allowed to maintain backwards
    compatibility.  It is possible to set `allow_fallback` to `true`
    but currently it coincides with the default behavior hence becomes
    a no-op in practice.
    
    Fixes #4511

--- src/mango/src/mango_error.erl
+++ src/mango/src/mango_error.erl
@@ -48,6 +48,28 @@ info(mango_json_bookmark, {invalid_bookmark, BadBookmark}) ->
         <<"invalid_bookmark">>,
         fmt("Invalid bookmark value: ~s", [?JSON_ENCODE(BadBookmark)])
     };
+info(mango_cursor, {invalid_index, []}) ->
+    {
+        400,
+        <<"invalid_index">>,
+        <<"You must specify an index with the `use_index` parameter.">>
+    };
+info(mango_cursor, {invalid_index, [DDocName]}) ->
+    {
+        400,
+        <<"invalid_index">>,
+        fmt("_design/~s specified by `use_index` could not be found or it is not suitable.", [
+            DDocName
+        ])
+    };
+info(mango_cursor, {invalid_index, [DDocName, ViewName]}) ->
+    {
+        400,
+        <<"invalid_index">>,
+        fmt("_design/~s, ~s specified by `use_index` could not be found or it is not suitable.", [
+            DDocName, ViewName
+        ])
+    };
 info(mango_cursor_text, {invalid_bookmark, BadBookmark}) ->
     {
         400,
