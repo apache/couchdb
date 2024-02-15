76a7fcc8d735b4e34c561cae5fadefa6097c77a5
Russell Branca <chewbranca@apache.org>
2024-02-14 16:47:43 -0800

    Handle atoms in type conversion

--- src/couch_stats/src/couch_stats_resource_tracker.erl
+++ src/couch_stats/src/couch_stats_resource_tracker.erl
@@ -594,6 +594,10 @@ convert_mfa(undefined) ->
 
 convert_type(Atom) when is_atom(Atom) ->
     atom_to_binary(Atom);
+convert_type({coordinator, Verb0, Atom0}) when is_atom(Atom0) ->
+    Verb = atom_to_binary(Verb0),
+    Atom = list_to_binary(Atom0),
+    <<"coordinator:", Verb/binary, ":", Atom/binary>>;
 convert_type({coordinator, Verb0, Path0}) ->
     Verb = atom_to_binary(Verb0),
     Path = list_to_binary(Path0),
