05183462681c79a6033b9fc5d8458a762d2da599
Robert Newson <rnewson@apache.org>
2024-04-17 16:33:10 +0100

    Optional account lockout facility on repeated auth failure

---
+++ test/elixir/test/auth_lockout_test.exs
@@ -0,0 +1,65 @@
+defmodule AuthLockoutTest do
+  use CouchTestCase
+
+  @moduletag :authentication
+
+  @moduletag config: [
+               {
+                 "admins",
+                 "couch_auth_lockout",
+                 "bar"
+               }
+             ]
+
+  test "lockout after multiple failed authentications", _context do
+    server_config = [
+      %{
+        :section => "couch_auth_lockout",
+        :key => "mode",
+        :value => "enforce"
+      }
+    ]
+
+    run_on_modified_server(
+      server_config,
+      fn -> test_couch_auth_lockout_enforcement() end
+    )
+  end
+
+  defp test_couch_auth_lockout_enforcement do
+    # exceed the lockout threshold
+    for _n <- 1..5 do
+      resp = Couch.get("/_all_dbs",
+        no_auth: true,
+        headers:  [authorization: "Basic #{:base64.encode("couch_auth_lockout:baz")}"]
+        )
+      assert resp.status_code == 401
+    end
+
+    # locked out?
+    resp = Couch.get("/_all_dbs",
+      no_auth: true,
+      headers:  [authorization: "Basic #{:base64.encode("couch_auth_lockout:baz")}"]
+    )
+    assert resp.status_code == 403
+    assert resp.body["reason"] == "Account is temporarily locked due to multiple authentication failures"
+  end
+
+  defp test_couch_auth_lockout_warning do
+    # exceed the lockout threshold
+    for _n <- 1..5 do
+      resp = Couch.get("/_all_dbs",
+        no_auth: true,
+        headers:  [authorization: "Basic #{:base64.encode("couch_auth_lockout:baz")}"]
+        )
+      assert resp.status_code == 401
+    end
+
+    # warning?
+    resp = Couch.get("/_all_dbs",
+      no_auth: true,
+      headers:  [authorization: "Basic #{:base64.encode("couch_auth_lockout:baz")}"]
+    )
+  end
+
+end
