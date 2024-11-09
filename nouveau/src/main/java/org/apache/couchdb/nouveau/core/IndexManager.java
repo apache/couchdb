4f184bcf537dcc477e481e9b53ede0506faa8641
Robert Newson <rnewson@apache.org>
2024-11-09 19:47:18 +0000

    Remove "9" from package and class names

--- nouveau/src/main/java/org/apache/couchdb/nouveau/core/IndexManager.java
+++ nouveau/src/main/java/org/apache/couchdb/nouveau/core/IndexManager.java
@@ -34,8 +34,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.couchdb.nouveau.api.IndexDefinition;
-import org.apache.couchdb.nouveau.lucene9.Lucene9AnalyzerFactory;
-import org.apache.couchdb.nouveau.lucene9.Lucene9Index;
+import org.apache.couchdb.nouveau.lucene.LuceneAnalyzerFactory;
+import org.apache.couchdb.nouveau.lucene.LuceneIndex;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -392,7 +392,7 @@ public final class IndexManager implements Managed {
         LOGGER.info("opening {}", name);
         final Path path = indexPath(name);
         final IndexDefinition indexDefinition = loadIndexDefinition(name);
-        final Analyzer analyzer = Lucene9AnalyzerFactory.fromDefinition(indexDefinition);
+        final Analyzer analyzer = LuceneAnalyzerFactory.fromDefinition(indexDefinition);
         final Directory dir = new DirectIODirectory(
                 FSDirectory.open(path.resolve(Integer.toString(indexDefinition.getLuceneVersion()))));
         final IndexWriterConfig config = new IndexWriterConfig(analyzer);
@@ -401,7 +401,7 @@ public final class IndexManager implements Managed {
         final long updateSeq = getSeq(writer, "update_seq");
         final long purgeSeq = getSeq(writer, "purge_seq");
         final SearcherManager searcherManager = new SearcherManager(writer, searcherFactory);
-        return new Lucene9Index(analyzer, writer, updateSeq, purgeSeq, searcherManager);
+        return new LuceneIndex(analyzer, writer, updateSeq, purgeSeq, searcherManager);
     }
 
     private long getSeq(final IndexWriter writer, final String key) throws IOException {
