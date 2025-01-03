8ade7dcb96aae808d19a6e7d7805c02afddb78cc
Ronny Berndt <ronny@apache.org>
2025-01-03 13:31:49 +0100

    chore(docs): Remove deprecated function call
    Fixing this message:
    ```
    WARNING: Calling get_html_theme_path is deprecated. If you are calling it to define html_theme_path, you are safe to remove that code.
    ```

--- src/docs/src/conf.py
+++ src/docs/src/conf.py
@@ -51,7 +51,6 @@ primary_domain = "http"
 pygments_style = "sphinx"
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_theme_options = {"canonical_url": "https://docs.couchdb.org/en/stable/"}
 
