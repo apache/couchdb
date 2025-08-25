8f0158126a0774d2213454fb51e5d93d11e3b41b
Nick Vatamaniuc <vatamane@gmail.com>
2025-08-25 15:39:02 -0400

    QuickJS Update
     * TypedArray.prototype.subarray: fixed the step at which '[[ByteOffset]]' is
     read
     https://github.com/bellard/quickjs/commit/c942978927a2a806517598a22a07c50766b3e125
     * Fixed buffer overflow in `js_bigint_from_string()`
     https://github.com/bellard/quickjs/commit/e1c18befb8ef661d9ee8a34ebc54e545a0214df9
     * Fixed crash in OP_add_loc if the variable is modified in
     `JS_ToPrimitiveFree()`
     https://github.com/bellard/quickjs/commit/1168c215d1daf2765a24d950079b1d5538305417
     * Fixed buffer overflow in `js_bigint_to_string1()`
     https://github.com/bellard/quickjs/commit/9ce544289fe86acdb8fb33e6a425da151438be05
     * Fixed buffer overflow in `TypedArray.prototype.lastIndexOf()`
     https://github.com/bellard/quickjs/commit/c927eca49a326326181a5db12627ffb48f191fe2
     * Avoid side effects in `JS_PrintValue()` which may lead to crashes in
     `print()` and `js_std_promise_rejection_check()`
     https://github.com/bellard/quickjs/commit/4e0d0b7f807816097cd4e15996c5296984dc531b
     * Limit function and regexp bytecode to 1G to avoid buffer overflows (the
     bytecode generators assume that bytecode offsets can fit a 32 bit signed
     integer
     https://github.com/bellard/quickjs/commit/d9ec8f102eb211f0b230a5ec3bf5da59f333e697
     * Adjust lastIndex to leading surrogate when inside a surrogate pair in
     unicode RegExp
     https://github.com/bellard/quickjs/commit/a4ac84d993128ec4cfe2cadcbb0893a2c2565988

--- src/couch_quickjs/quickjs/quickjs-libc.c
+++ src/couch_quickjs/quickjs/quickjs-libc.c
@@ -4230,17 +4230,15 @@ static void js_std_promise_rejection_check(JSContext *ctx)
 /* main loop which calls the user JS callbacks */
 void js_std_loop(JSContext *ctx)
 {
-    JSContext *ctx1;
     int err;
 
     for(;;) {
         /* execute the pending jobs */
         for(;;) {
-            err = JS_ExecutePendingJob(JS_GetRuntime(ctx), &ctx1);
+            err = JS_ExecutePendingJob(JS_GetRuntime(ctx), NULL);
             if (err <= 0) {
-                if (err < 0) {
-                    js_std_dump_error(ctx1);
-                }
+                if (err < 0)
+                    js_std_dump_error(ctx);
                 break;
             }
         }
@@ -4271,11 +4269,10 @@ JSValue js_std_await(JSContext *ctx, JSValue obj)
             JS_FreeValue(ctx, obj);
             break;
         } else if (state == JS_PROMISE_PENDING) {
-            JSContext *ctx1;
             int err;
-            err = JS_ExecutePendingJob(JS_GetRuntime(ctx), &ctx1);
+            err = JS_ExecutePendingJob(JS_GetRuntime(ctx), NULL);
             if (err < 0) {
-                js_std_dump_error(ctx1);
+                js_std_dump_error(ctx);
             }
             if (err == 0) {
                 js_std_promise_rejection_check(ctx);
@@ -4303,6 +4300,7 @@ void js_std_eval_binary(JSContext *ctx, const uint8_t *buf, size_t buf_len,
         if (JS_VALUE_GET_TAG(obj) == JS_TAG_MODULE) {
             js_module_set_import_meta(ctx, obj, FALSE, FALSE);
         }
+        JS_FreeValue(ctx, obj);
     } else {
         if (JS_VALUE_GET_TAG(obj) == JS_TAG_MODULE) {
             if (JS_ResolveModule(ctx, obj) < 0) {
