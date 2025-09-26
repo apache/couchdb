ffa80ff759e7d25d7b9e0c8cf61fc80f730ee144
Robert Newson <rnewson@apache.org>
2025-09-26 09:44:43 +0100

    nouveau welcome message for version negotiation

---
+++ nouveau/src/main/java/org/apache/couchdb/nouveau/resources/WelcomeResource.java
@@ -0,0 +1,30 @@
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.couchdb.nouveau.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.couchdb.nouveau.api.WelcomeResponse;
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+public final class WelcomeResource {
+
+    @GET
+    public WelcomeResponse welcome() {
+        return WelcomeResponse.INSTANCE;
+    }
+}
