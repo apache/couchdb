c26fc0e44a78287e655a8d1372692edf09b0e015
Nick Vatamaniuc <vatamane@gmail.com>
2025-09-30 11:13:06 -0400

    Update QuickJS
    Upstream merged test262 PR so drop patches related to that:
     https://github.com/bellard/quickjs/pull/408
    
    Other improvements
     * Add set methods (union, difference, intersection, ...)
     https://github.com/bellard/quickjs/commit/a1e073e44487312df617552e39de47160f5c0bab
     * Add iterators
     https://github.com/bellard/quickjs/commit/982b7aa14f923755d03a210d06ea9d289b60c96b
     https://github.com/bellard/quickjs/commit/e924173c0f414f83caa4bc5cf7c3d2b49c76d029
     https://github.com/bellard/quickjs/commit/b2ed2e91f5b9a7839c8290e29157ea2c3fba5c19
     https://github.com/bellard/quickjs/commit/cf0e179263093bd77fcce6c05da25d5969b9aa52
     https://github.com/bellard/quickjs/commit/2d99c323a27ca9013230cfd6fb56ab37f2a0f1d4
     * Add ArrayBuffer.prototype.transfer
     https://github.com/bellard/quickjs/commit/1e19893c82fe150db60f2464e4eeab2b663499d4
     * Add resizable array buffers
     https://github.com/bellard/quickjs/commit/f021d7714fc03400bf9772280cd21707ff13cae1
     * Add Map and WeakMap upsert
     https://github.com/bellard/quickjs/commit/456e016f7c0543c505b2f3df422a56f7558fe109
     * Add Map.sumPrecise()
     https://github.com/bellard/quickjs/commit/bc753c6a151885d058ccdbc34c07384e4ed31be5
     https://github.com/bellard/quickjs/commit/c3e5ae20087aba2a1bf2d96776f44d3a72c128c4
     * Fix date parsing
     https://github.com/bellard/quickjs/commit/00608769df4aa48a250cfef2efa71004742b388d

--- src/couch_quickjs/update_patches.sh
+++ src/couch_quickjs/update_patches.sh
@@ -14,12 +14,15 @@ rm -rf master.zip quickjs-master
 wget -q ${URL}
 echo " * unzip master.zip to quickjs-master"
 unzip -q -o master.zip
-echo " * updating 01-spidermonkey-185-mode.patch"
+
 set +e
+
+echo " * updating 01-spidermonkey-185-mode.patch"
 diff -u quickjs-master/quickjs.c quickjs/quickjs.c > patches/01-spidermonkey-185-mode.patch
-diff -u quickjs-master/Makefile quickjs/Makefile > patches/02-test262-makefile.patch
-diff -u quickjs-master/tests/test262.patch quickjs/tests/test262.patch > patches/03-test262-yield.patch
-diff -u quickjs-master/test262_errors.txt quickjs/test262_errors.txt > patches/04-test262-errors.patch
+
+echo " * updating 02-test262-errors.patch"
+diff -u quickjs-master/test262_errors.txt quickjs/test262_errors.txt> patches/02-test262-errors.patch
 set -e
+
 echo " * cleaning up"
 rm -rf master.zip quickjs-master
