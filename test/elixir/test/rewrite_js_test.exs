8bb25e7ff3acd96cc71a981f69a8298a161d0dc9
Jiahui Li <lijiahui702@gmail.com>
2025-12-11 11:45:49 -0600

    Update Elixir tests
    - Elixir unit tests need to end with `_test.exs` to run.
      - large_docs_test.exs
      - reshard_changes_feed_test.exs
      - disk_monitor_test.exs
      - 04_key_test.exs
      - 25_beginswith_test.exs
    
    - The `Enum.each()` method cannot run tests because it iterates
      through the data at runtime, while the test macro is defined at
      compile time. Therefore, replace it with a for loop.
      - rewrite_js_test.exs
      - rewrite_test.exs
    
    - Update outdated tests.
      - cluster_with_quorum_test.exs
      - cluster_without_quorum_test.exs
      - cookie_auth_test.exs

--- test/elixir/test/rewrite_js_test.exs
+++ test/elixir/test/rewrite_js_test.exs
@@ -209,203 +209,200 @@ defmodule RewriteJSTest do
     }
   }
 
-  Enum.each(
-    ["test_rewrite_suite_db", "test_rewrite_suite_db%2Fwith_slashes"],
-    fn db_name ->
-      @tag with_random_db: db_name
-      test "Test basic js rewrites on #{db_name}", context do
-        db_name = context[:db_name]
+  for db_name <- ["test_rewrite_suite_db", "test_rewrite_suite_db%2Fwith_slashes"] do
+    @tag with_random_db: db_name
+    test "basic js rewrites on #{db_name}", context do
+      db_name = context[:db_name]
 
-        create_doc(db_name, @ddoc)
+      create_doc(db_name, @ddoc)
 
-        docs1 = make_docs(0..9)
-        bulk_save(db_name, docs1)
+      docs1 = make_docs(0..9)
+      bulk_save(db_name, docs1)
 
-        docs2 = [
-          %{"a" => 1, "b" => 1, "string" => "doc 1", "type" => "complex"},
-          %{"a" => 1, "b" => 2, "string" => "doc 2", "type" => "complex"},
-          %{"a" => "test", "b" => %{}, "string" => "doc 3", "type" => "complex"},
-          %{
-            "a" => "test",
-            "b" => ["test", "essai"],
-            "string" => "doc 4",
-            "type" => "complex"
-          },
-          %{"a" => %{"c" => 1}, "b" => "", "string" => "doc 5", "type" => "complex"}
-        ]
+      docs2 = [
+        %{"a" => 1, "b" => 1, "string" => "doc 1", "type" => "complex"},
+        %{"a" => 1, "b" => 2, "string" => "doc 2", "type" => "complex"},
+        %{"a" => "test", "b" => %{}, "string" => "doc 3", "type" => "complex"},
+        %{
+          "a" => "test",
+          "b" => ["test", "essai"],
+          "string" => "doc 4",
+          "type" => "complex"
+        },
+        %{"a" => %{"c" => 1}, "b" => "", "string" => "doc 5", "type" => "complex"}
+      ]
 
-        bulk_save(db_name, docs2)
+      bulk_save(db_name, docs2)
 
-        # Test simple rewriting
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/foo")
-        assert resp.body == "This is a base64 encoded text"
-        assert resp.headers["Content-Type"] == "text/plain"
+      # Test simple rewriting
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/foo")
+      assert resp.body == "This is a base64 encoded text"
+      assert resp.headers["Content-Type"] == "text/plain"
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/foo2")
-        assert resp.body == "This is a base64 encoded text"
-        assert resp.headers["Content-Type"] == "text/plain"
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/foo2")
+      assert resp.body == "This is a base64 encoded text"
+      assert resp.headers["Content-Type"] == "text/plain"
 
-        # Test POST, hello update world
-        resp =
-          Couch.post("/#{db_name}", body: %{"word" => "plankton", "name" => "Rusty"}).body
+      # Test POST, hello update world
+      resp =
+        Couch.post("/#{db_name}", body: %{"word" => "plankton", "name" => "Rusty"}).body
 
-        assert resp["ok"]
-        doc_id = resp["id"]
-        assert doc_id
+      assert resp["ok"]
+      doc_id = resp["id"]
+      assert doc_id
 
-        resp = Couch.put("/#{db_name}/_design/test/_rewrite/hello/#{doc_id}")
-        assert resp.status_code in [201, 202]
-        assert resp.body == "hello doc"
-        assert String.match?(resp.headers["Content-Type"], ~r/charset=utf-8/)
+      resp = Couch.put("/#{db_name}/_design/test/_rewrite/hello/#{doc_id}")
+      assert resp.status_code in [201, 202]
+      assert resp.body == "hello doc"
+      assert String.match?(resp.headers["Content-Type"], ~r/charset=utf-8/)
 
-        assert Couch.get("/#{db_name}/#{doc_id}").body["world"] == "hello"
+      assert Couch.get("/#{db_name}/#{doc_id}").body["world"] == "hello"
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome?name=user")
-        assert resp.body == "Welcome user"
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome?name=user")
+      assert resp.body == "Welcome user"
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome/user")
-        assert resp.body == "Welcome user"
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome/user")
+      assert resp.body == "Welcome user"
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome2")
-        assert resp.body == "Welcome user"
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome2")
+      assert resp.body == "Welcome user"
 
-        resp = Couch.put("/#{db_name}/_design/test/_rewrite/welcome3/test")
-        assert resp.status_code in [201, 202]
-        assert resp.body == "New World"
-        assert String.match?(resp.headers["Content-Type"], ~r/charset=utf-8/)
+      resp = Couch.put("/#{db_name}/_design/test/_rewrite/welcome3/test")
+      assert resp.status_code in [201, 202]
+      assert resp.body == "New World"
+      assert String.match?(resp.headers["Content-Type"], ~r/charset=utf-8/)
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome3/test")
-        assert resp.body == "Welcome test"
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome3/test")
+      assert resp.body == "Welcome test"
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome4/user")
-        assert resp.body == "Welcome user"
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome4/user")
+      assert resp.body == "Welcome user"
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome5/welcome3")
-        assert resp.body == "Welcome welcome3"
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/welcome5/welcome3")
+      assert resp.body == "Welcome welcome3"
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/basicView")
-        assert resp.status_code == 200
-        assert resp.body["total_rows"] == 9
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/basicView")
+      assert resp.status_code == 200
+      assert resp.body["total_rows"] == 9
 
-        resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/complexView")
-        assert resp.status_code == 200
-        assert String.match?(resp.body, ~r/FirstKey: [1, 2]/)
+      resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/complexView")
+      assert resp.status_code == 200
+      assert String.match?(resp.body, ~r/FirstKey: [1, 2]/)
 
-        resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/complexView2")
-        assert resp.status_code == 200
-        assert String.match?(resp.body, ~r/Value: doc 3/)
+      resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/complexView2")
+      assert resp.status_code == 200
+      assert String.match?(resp.body, ~r/Value: doc 3/)
 
-        resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/complexView3")
-        assert resp.status_code == 200
-        assert String.match?(resp.body, ~r/Value: doc 4/)
+      resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/complexView3")
+      assert resp.status_code == 200
+      assert String.match?(resp.body, ~r/Value: doc 4/)
 
-        resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/complexView4")
-        assert resp.status_code == 200
-        assert String.match?(resp.body, ~r/Value: doc 5/)
+      resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/complexView4")
+      assert resp.status_code == 200
+      assert String.match?(resp.body, ~r/Value: doc 5/)
 
-        # COUCHDB-1612 - send body rewriting get to post
-        resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/sendBody1")
-        assert resp.status_code == 200
-        assert String.match?(resp.body, ~r/Value: doc 5 LineNo: 1/)
+      # COUCHDB-1612 - send body rewriting get to post
+      resp = Rawresp.get("/#{db_name}/_design/test/_rewrite/simpleForm/sendBody1")
+      assert resp.status_code == 200
+      assert String.match?(resp.body, ~r/Value: doc 5 LineNo: 1/)
 
-        resp = Couch.get("/#{db_name}/_design/test/_rewrite/db/_design/test?meta=true")
-        assert resp.status_code == 200
-        assert resp.body["_id"] == "_design/test"
-        assert Map.has_key?(resp.body, "_revs_info")
-      end
-
-      @tag with_random_db: db_name
-      test "early response on #{db_name}", context do
-        db_name = context[:db_name]
-
-        ddoc = %{
-          _id: "_design/response",
-          rewrites: """
-           function(req){
-            status = parseInt(req.query.status);
-            return {code: status,
-                    body: JSON.stringify({"status": status}),
-                    headers: {'x-foo': 'bar', 'Content-Type': 'application/json'}};
-          }
-          """
-        }
-
-        create_doc(db_name, ddoc)
-
-        resp = Couch.get("/#{db_name}/_design/response/_rewrite?status=200")
-        assert resp.status_code == 200
-        assert resp.headers["x-foo"] == "bar"
-        assert resp.body["status"] == 200
-
-        resp = Couch.get("/#{db_name}/_design/response/_rewrite?status=451")
-        assert resp.status_code == 451
-        assert resp.headers["Content-Type"] == "application/json"
-
-        resp = Couch.get("/#{db_name}/_design/response/_rewrite?status=500")
-        assert resp.status_code == 500
-      end
-
-      @tag with_random_db: db_name
-      test "path relative to server on #{db_name}", context do
-        db_name = context[:db_name]
-
-        ddoc = %{
-          _id: "_design/relative",
-          rewrites: """
-           function(req){
-            return '../../../_uuids'
-          }
-          """
-        }
-
-        create_doc(db_name, ddoc)
-        resp = Couch.get("/#{db_name}/_design/relative/_rewrite/uuids")
-        assert resp.status_code == 200
-        assert length(resp.body["uuids"]) == 1
-      end
-
-      @tag with_random_db: db_name
-      test "loop on #{db_name}", context do
-        db_name = context[:db_name]
-
-        ddoc_loop = %{
-          _id: "_design/loop",
-          rewrites: """
-          function(req) {
-            return '_rewrite/loop';
-          }
-          """
-        }
-
-        create_doc(db_name, ddoc_loop)
-        resp = Couch.get("/#{db_name}/_design/loop/_rewrite/loop")
-        assert resp.status_code == 400
-      end
-
-      @tag with_random_db: db_name
-      test "requests with body preserve the query string rewrite on #{db_name}",
-           context do
-        db_name = context[:db_name]
-
-        ddoc_qs = %{
-          _id: "_design/qs",
-          rewrites:
-            "function (r) { return {path: '../../_changes', query: {'filter': '_doc_ids'}};};"
-        }
-
-        create_doc(db_name, ddoc_qs)
-        create_doc(db_name, %{_id: "qs1"})
-        create_doc(db_name, %{_id: "qs2"})
-
-        resp =
-          Couch.post("/#{db_name}/_design/qs/_rewrite",
-            body: %{doc_ids: ["qs2"]}
-          )
-
-        assert resp.status_code == 200
-        assert length(resp.body["results"]) == 1
-        assert Enum.at(resp.body["results"], 0)["id"] == "qs2"
-      end
+      resp = Couch.get("/#{db_name}/_design/test/_rewrite/db/_design/test?meta=true")
+      assert resp.status_code == 200
+      assert resp.body["_id"] == "_design/test"
+      assert Map.has_key?(resp.body, "_revs_info")
     end
-  )
+
+    @tag with_random_db: db_name
+    test "early response on #{db_name}", context do
+      db_name = context[:db_name]
+
+      ddoc = %{
+        _id: "_design/response",
+        rewrites: """
+         function(req){
+          status = parseInt(req.query.status);
+          return {code: status,
+                  body: JSON.stringify({"status": status}),
+                  headers: {'x-foo': 'bar', 'Content-Type': 'application/json'}};
+        }
+        """
+      }
+
+      create_doc(db_name, ddoc)
+
+      resp = Couch.get("/#{db_name}/_design/response/_rewrite?status=200")
+      assert resp.status_code == 200
+      assert resp.headers["x-foo"] == "bar"
+      assert resp.body["status"] == 200
+
+      resp = Couch.get("/#{db_name}/_design/response/_rewrite?status=451")
+      assert resp.status_code == 451
+      assert resp.headers["Content-Type"] == "application/json"
+
+      resp = Couch.get("/#{db_name}/_design/response/_rewrite?status=500")
+      assert resp.status_code == 500
+    end
+
+    @tag with_random_db: db_name
+    test "path relative to server on #{db_name}", context do
+      db_name = context[:db_name]
+
+      ddoc = %{
+        _id: "_design/relative",
+        rewrites: """
+         function(req){
+          return '../../../_uuids'
+        }
+        """
+      }
+
+      create_doc(db_name, ddoc)
+      resp = Couch.get("/#{db_name}/_design/relative/_rewrite/uuids")
+      assert resp.status_code == 200
+      assert length(resp.body["uuids"]) == 1
+    end
+
+    @tag with_random_db: db_name
+    test "loop on #{db_name}", context do
+      db_name = context[:db_name]
+
+      ddoc_loop = %{
+        _id: "_design/loop",
+        rewrites: """
+        function(req) {
+          return '_rewrite/loop';
+        }
+        """
+      }
+
+      create_doc(db_name, ddoc_loop)
+      resp = Couch.get("/#{db_name}/_design/loop/_rewrite/loop")
+      assert resp.status_code == 400
+    end
+
+    @tag with_random_db: db_name
+    test "requests with body preserve the query string rewrite on #{db_name}",
+         context do
+      db_name = context[:db_name]
+
+      ddoc_qs = %{
+        _id: "_design/qs",
+        rewrites:
+          "function (r) { return {path: '../../_changes', query: {'filter': '_doc_ids'}};};"
+      }
+
+      create_doc(db_name, ddoc_qs)
+      create_doc(db_name, %{_id: "qs1"})
+      create_doc(db_name, %{_id: "qs2"})
+
+      resp =
+        Couch.post("/#{db_name}/_design/qs/_rewrite",
+          body: %{doc_ids: ["qs2"]}
+        )
+
+      assert resp.status_code == 200
+      assert length(resp.body["results"]) == 1
+      assert Enum.at(resp.body["results"], 0)["id"] == "qs2"
+    end
+  end
 end
