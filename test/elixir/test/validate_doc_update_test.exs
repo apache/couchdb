166335b0e8761f845ef0227d6fecd3e65783bc92
James Coglan <james@neighbourhood.ie>
2026-01-09 14:05:43 +0000

    feat: Add the ability for VDUs to be written as Mango selectors

--- test/elixir/test/validate_doc_update_test.exs
+++ test/elixir/test/validate_doc_update_test.exs
@@ -76,4 +76,137 @@ defmodule ValidateDocUpdateTest do
 
     assert resp.status_code == 403
   end
+
+  @mango_type_check %{
+    language: "query",
+
+    validate_doc_update: %{
+      "newDoc" => %{"type" => %{"$exists" => true}}
+    }
+  }
+
+  @tag :with_db
+  test "Mango VDU accepts a valid document", context do
+    db = context[:db_name]
+    resp = Couch.put("/#{db}/_design/mango-test", body: @mango_type_check)
+    assert resp.status_code == 201
+
+    resp = Couch.put("/#{db}/doc", body: %{"type" => "movie"})
+    assert resp.status_code == 201
+    assert resp.body["ok"] == true
+  end
+
+  @tag :with_db
+  test "Mango VDU rejects an invalid document", context do
+    db = context[:db_name]
+    resp = Couch.put("/#{db}/_design/mango-test", body: @mango_type_check)
+    assert resp.status_code == 201
+
+    resp = Couch.put("/#{db}/doc", body: %{"no" => "type"})
+    assert resp.status_code == 403
+    assert resp.body["error"] == "forbidden"
+  end
+
+  @tag :with_db
+  test "updating a Mango VDU updates its effects", context do
+    db = context[:db_name]
+
+    resp = Couch.put("/#{db}/_design/mango-test", body: @mango_type_check)
+    assert resp.status_code == 201
+
+    ddoc = %{
+      language: "query",
+
+      validate_doc_update: %{
+        "newDoc" => %{
+          "type" => %{"$type" => "string"},
+          "year" => %{"$lt" => 2026}
+        }
+      }
+    }
+    resp = Couch.put("/#{db}/_design/mango-test", body: ddoc, query: %{rev: resp.body["rev"]})
+    assert resp.status_code == 201
+
+    resp = Couch.put("/#{db}/doc1", body: %{"type" => "movie", "year" => 1994})
+    assert resp.status_code == 201
+
+    resp = Couch.put("/#{db}/doc2", body: %{"type" => 42, "year" => 1994})
+    assert resp.status_code == 403
+    assert resp.body["error"] == "forbidden"
+
+    resp = Couch.put("/#{db}/doc3", body: %{"type" => "movie", "year" => 2094})
+    assert resp.status_code == 403
+    assert resp.body["error"] == "forbidden"
+  end
+
+  @tag :with_db
+  test "converting a Mango VDU to JavaScript updates its effects", context do
+    db = context[:db_name]
+
+    resp = Couch.put("/#{db}/_design/mango-test", body: @mango_type_check)
+    assert resp.status_code == 201
+
+    ddoc = %{
+      language: "javascript",
+
+      validate_doc_update: ~s"""
+        function (newDoc) {
+          if (typeof newDoc.year !== 'number') {
+            throw {forbidden: 'Documents must have a valid year field'};
+          }
+        }
+      """
+    }
+    resp = Couch.put("/#{db}/_design/mango-test", body: ddoc, query: %{rev: resp.body["rev"]})
+    assert resp.status_code == 201
+
+    resp = Couch.put("/#{db}/doc1", body: %{"year" => 1994})
+    assert resp.status_code == 201
+
+    resp = Couch.put("/#{db}/doc2", body: %{"year" => "1994"})
+    assert resp.status_code == 403
+    assert resp.body["error"] == "forbidden"
+  end
+
+  @tag :with_db
+  test "deleting a Mango VDU removes its effects", context do
+    db = context[:db_name]
+
+    resp = Couch.put("/#{db}/_design/mango-test", body: @mango_type_check)
+    assert resp.status_code == 201
+
+    resp = Couch.delete("/#{db}/_design/mango-test", query: %{rev: resp.body["rev"]})
+    assert resp.status_code == 200
+
+    resp = Couch.put("/#{db}/doc", body: %{"no" => "type"})
+    assert resp.status_code == 201
+  end
+
+  @tag :with_db
+  test "Mango VDU rejects a doc if any existing ddoc fails to match", context do
+    db = context[:db_name]
+    resp = Couch.put("/#{db}/_design/mango-test", body: @mango_type_check)
+    assert resp.status_code == 201
+
+    ddoc = %{
+      language: "query",
+
+      validate_doc_update: %{
+        "newDoc" => %{"year" => %{"$lt" => 2026}}
+      }
+    }
+    resp = Couch.put("/#{db}/_design/mango-test-2", body: ddoc)
+    assert resp.status_code == 201
+
+    resp = Couch.put("/#{db}/doc1", body: %{"type" => "movie", "year" => 1994})
+    assert resp.status_code == 201
+
+    resp = Couch.put("/#{db}/doc2", body: %{"year" => 1994})
+    assert resp.status_code == 403
+    assert resp.body["error"] == "forbidden"
+
+    resp = Couch.put("/#{db}/doc3", body: %{"type" => "movie", "year" => 2094})
+    assert resp.status_code == 403
+    assert resp.body["error"] == "forbidden"
+  end
 end
